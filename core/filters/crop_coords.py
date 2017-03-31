7c019527c01f1a7b0fafda5be843c66db7a44f8c
Dimitar Tasev <dimtasev@gmail.com>
2017-03-31 11:20:43 +0100

    Rewrote crop coords and test

--- core/filters/crop_coords.py
+++ core/filters/crop_coords.py
@@ -4,6 +4,10 @@ import helper as h
 
 
 def cli_register(parser):
+    """
+    This function registers the filter with the command line interface dynamically.
+    It's intended to be called only from core.filters.cli_registrator.
+    """
     parser.add_argument(
         "-R",
         "--region-of-interest",
@@ -21,137 +25,57 @@ def gui_register(par):
     raise NotImplementedError("GUI doesn't exist yet")
 
 
-def execute(sample, region_of_interest, flat, dark):
+def execute(sample, region_of_interest, flat=None, dark=None):
     """
-    Execute the Circular Mask filter.
+    Execute the Crop Coordinates by Region of Interest filter. 
+    This does NOT do any checks if the Region of interest is out of bounds!
+    If the region of interest is out of bounds, the crop will fail at runtime.
+    If the region of interest is in bounds, but has overlapping coordinates the crop will fail at runtime.
 
     :param data: The sample image data as a 3D numpy.ndarray
-    :param region_of_interest: The region of interest that will be cropped
+    :param region_of_interest: Crop original images using these coordinates. 
+                               The selection is a rectangle and expected order is - Left Top Right Bottom.
     :param flat: The average flat image to be cropped
     :param dark: The average dark image to be cropped
 
     :return: the data after being processed with the filter
-    """
-    if sample is not None and flat is None and dark is not None:
-        return execute_volume(sample, region_of_interest), None, None
-    else:
-        return execute_volume(sample, region_of_interest), \
-               execute_image(flat, region_of_interest), \
-               execute_image(dark, region_of_interest)
 
+    Example command line:
 
-def _crop_coords_sanity_checks(coords, data_image, expected_data_shape=3):
-    # if nothing is provided make the user aware
-    if not isinstance(coords, list) or 4 != len(coords):
-        raise ValueError(
-            "Error in crop (region of interest) parameter (expecting a list with four integers) "
-            "[left, top, right, bottom]. Got: {0} with {1}".format(
-                coords, type(coords)))
-
-    elif not all(isinstance(crd, int) for crd in coords):
-        raise ValueError(
-            "Cannot use non-integer coordinates to crop images. Got "
-            "these coordinates: {0}".format(coords))
-
-    elif not isinstance(data_image, np.ndarray) or expected_data_shape != len(
-            data_image.shape):
-        raise ValueError(
-            "Wrong data volume when trying to crop: {0}".format(data_image))
-
-
-def execute_image(data, region_of_interest):
+    python main.py -i /some/data -R 134 203 170 250
+    python main.py -i /some/data --region-of-interest 134 203 170 250
     """
 
-    :param data :: image as a 2d numpy array
-    :param region_of_interest: coordinates which will be cropped and returned for further processing
+    assert isinstance(
+        sample, np.ndarray
+    ), "This filter only works with numpy ndarrays. The provided input array doesn't match the np.ndarray type."
 
-    :returns :: cropped data (stack of images)
+    # execute only for sample, if no flat and dark images are provided
+    if sample is not None and (flat is None or dark is None):
+        return _execute(sample, region_of_interest), None, None
+    else:  # crop all
+        return _execute(sample, region_of_interest), \
+               _execute(flat, region_of_interest), \
+               _execute(dark, region_of_interest)
 
-    """
 
+def _execute(data, region_of_interest):
     if region_of_interest:
-        h.pstart("Starting image cropping with coordinates: {0}. ...".format(
+        h.pstart("Starting cropping with coordinates: {0}. ...".format(
             region_of_interest))
 
-        data = _crop_image(data, region_of_interest)
+        left = region_of_interest[0]
+        top = region_of_interest[1]
+        right = region_of_interest[2]
+        bottom = region_of_interest[3]
+
+        if data.ndim == 2:
+            data = data[top:bottom, left:right]
+        elif data.ndim == 3:
+            data = data[:, top:bottom, left:right]
 
         h.pstop(
             "Finished image cropping with pixel data type: {0}, resulting shape: {1}.".
             format(data.dtype, data.shape))
 
     return data
-
-
-def _crop_image(data_image, coords):
-    """
-    Crops a data volume by a rectangle defined by two corner
-    coordinates. Crops along the z axis (outermost numpy array index)
-
-    :param data_image :: image data to be cropped
-    :param coords :: coordinates of the corners that define a rectangle box (crop to this
-    box, as when cropping to the regions of interest).
-    :returns :: cropped image
-    """
-    _crop_coords_sanity_checks(coords, data_image, expected_data_shape=2)
-    left = coords[0]
-    top = coords[1]
-    right = coords[2]
-    bottom = coords[3]
-
-    if not any(coords) or top > bottom or left > right:
-        # skip if for example: 0, 0, 0, 0 (empty selection)
-        return data_image
-    else:
-        return data_image[top:bottom, left:right]
-
-
-def execute_volume(data, crop_coords):
-    """
-    Crop stack of images to a region (region of interest or similar), image by image
-
-    :param data :: stack of images as a 3d numpy array
-    :param crop_coords: coordinates which will be cropped and returned for further processing
-
-    :returns :: cropped data (stack of images)
-    """
-
-    h.check_data_stack(data)
-
-    # list with left, top, right, bottom
-    if crop_coords:
-        h.pstart("Starting data volume cropping with coordinates: {0}. ...".
-                 format(crop_coords))
-
-        data = _crop_volume(data, crop_coords)
-
-        h.pstop(
-            "Finished data volume cropping with pixel data type: {0}, resulting shape: {1}.".
-            format(data.dtype, data.shape))
-
-    h.check_data_stack(data)
-
-    return data
-
-
-def _crop_volume(data_vol, coords):
-    """
-    Crops a data volume by a rectangle defined by two corner
-    coordinates. Crops along the z axis (outermost numpy array index)
-
-    @param data_vol :: 3D data volume
-    @param coords :: coordinates of the corners that define a rectangle box (crop to this
-    box, as when cropping to the regions of interest).
-    Returns :: cropped data volume
-    """
-    _crop_coords_sanity_checks(coords, data_vol)
-
-    left = coords[0]
-    top = coords[1]
-    right = coords[2]
-    bottom = coords[3]
-
-    if not any(coords) or top > bottom or left > right:
-        # skip if for example: 0, 0, 0, 0 (empty selection)
-        return data_vol
-    else:
-        return data_vol[:, top:bottom, left:right]
