e1d6845f6c9047667254bfb3b02037a99ae46750
Dimitar Tasev <dimtasev@gmail.com>
2017-03-31 14:04:33 +0100

    Improvements for rebin

--- core/filters/rebin.py
+++ core/filters/rebin.py
@@ -1,5 +1,8 @@
 from __future__ import (absolute_import, division, print_function)
 import helper as h
+import scipy.misc
+from core.parallel import exclusive_mem as pem
+from core.parallel import utility as pu
 
 
 def cli_register(parser):
@@ -40,19 +43,20 @@ def execute(data, rebin_param, mode, cores=None, chunksize=None):
                         int - Percentage of current size.
                         float - Fraction of current size.
                         tuple - Size of the output image.
-
     :param mode: Interpolation to use for re-sizing ('nearest', 'lanczos', 'bilinear', 'bicubic' or 'cubic').
+    :param cores: The number of cores that will be used to process the data.
+    :param chunksize: The number of chunks that each worker will receive.
 
     :return: the data after being processed with the filter
 
     Full reference:
     https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.misc.imresize.html
 
+    Example command line:
+    python main.py -i /some/data --rebin 0.5 --rebin-mode 'nearest'
     """
     h.check_data_stack(data)
-
     if rebin_param and 0 < rebin_param:
-        from core.parallel import utility as pu
         if pu.multiprocessing_available():
             data = _execute_par(data, rebin_param, mode, cores, chunksize)
         else:
@@ -63,15 +67,11 @@ def execute(data, rebin_param, mode, cores=None, chunksize=None):
 
 
 def _execute_par(data, rebin_param, mode, cores=None, chunksize=None):
-    import scipy.misc
-    from core.parallel import exclusive_mem as pem
-
     resized_data = _create_reshaped_array(data.shape, rebin_param)
 
     h.pstart("Starting PARALLEL image rebinning.")
 
     f = pem.create_partial(scipy.misc.imresize, size=rebin_param, interp=mode)
-
     resized_data = pem.execute(
         data, f, cores, chunksize, "Rebinning", output_data=resized_data)
 
@@ -82,7 +82,6 @@ def _execute_par(data, rebin_param, mode, cores=None, chunksize=None):
 
 
 def _execute_seq(data, rebin_param, mode):
-    import scipy.misc
     h.pstart("Starting image rebinning.")
     resized_data = _create_reshaped_array(data.shape, rebin_param)
     num_images = resized_data.shape[0]
@@ -94,7 +93,6 @@ def _execute_seq(data, rebin_param, mode):
         h.prog_update()
 
     h.prog_close()
-
     h.pstop(
         "Finished image rebinning. New shape: {0}".format(resized_data.shape))
 
@@ -102,10 +100,8 @@ def _execute_seq(data, rebin_param, mode):
 
 
 def _create_reshaped_array(old_shape, rebin_param):
-    from core.parallel import utility as pu
 
     num_images = old_shape[0]
-
     # use SciPy's calculation to find the expected dimensions
     # int to avoid visible deprecation warning
     expected_dimy = int(rebin_param * old_shape[1])
@@ -113,48 +109,3 @@ def _create_reshaped_array(old_shape, rebin_param):
 
     # allocate memory for images with new dimensions
     return pu.create_shared_array((num_images, expected_dimy, expected_dimx))
-
-
-# def _execute_custom(data, config):
-#     """
-#     Downscale to for example shrink 1Kx1K images to 512x512
-
-#     @param data_vol :: 3d volume to downscale
-#     @param block_size :: make block_size X block_size blocks to downscale
-#     @param method :: either 'average' (default) or 'sum' to calculate average or sum of blocks
-
-#     Returns :: downscaled volume, with the dimensions implied by block_size, and the
-#     same data type as the input data volume.
-#     """
-#     raise NotImplementedError("This function's implementation is disabled.")
-
-#     if 0 != np.mod(data_vol.shape[1], block_size) or 0 != np.mod(
-#             data_vol.shape[2], block_size):
-#         raise ValueError(
-#             "The block size ({0}) must be an exact integer divisor of the sizes of the "
-#             "x and y dimensions ({1} and {2} of the input data volume".format(
-#                 data_vol.shape[2], data_vol.shape[1], block_size))
-
-#     supported_methods = ['average', 'sum']
-#     if method.lower() not in supported_methods:
-#         raise ValueError(
-#             "The method to combine pixels in blocks must be one of {0}. Got unknown "
-#             "value: {1}".format(supported_methods, method))
-
-#     rescaled_vol = np.zeros(
-#         (data_vol.shape[0], data_vol.shape[1] // block_size,
-#          data_vol.shape[2] // block_size),
-#         dtype=data_vol.dtype)
-#     # for block averages in every slice/image along the vertical/z axis
-#     tmp_shape = rescaled_vol.shape[1], block_size, rescaled_vol.shape[
-#         2], block_size
-#     for vert_slice in range(len(rescaled_vol)):
-#         vsl = data_vol[vert_slice, :, :]
-#         if 'average' == method:
-#             rescaled_vol[vert_slice, :, :] = vsl.reshape(tmp_shape).mean(
-#                 -1).mean(1)
-#         elif 'sum' == method:
-#             rescaled_vol[vert_slice, :, :] = vsl.reshape(tmp_shape).mean(
-#                 -1).mean(1)
-
-#     return rescaled_vol
--- tests/rebin_test.py
+++ tests/rebin_test.py
@@ -11,50 +11,49 @@ class RebinTest(unittest.TestCase):
         from core.filters import rebin
         self.alg = rebin
 
-    def test_not_executed(self):
+    def test_not_executed_rebin_none(self):
         images, control = th.gen_img_shared_array_and_copy()
-
-        # bad params
         rebin = None
         mode = 'nearest'
         result = self.alg.execute(images, rebin, mode)
         npt.assert_equal(result, control)
 
+    def test_not_executed_rebin_negative(self):
+        images, control = th.gen_img_shared_array_and_copy()
+        mode = 'nearest'
         rebin = -1
         result = self.alg.execute(images, rebin, mode)
         npt.assert_equal(result, control)
 
+    def test_not_executed_rebin_zero(self):
+        images, control = th.gen_img_shared_array_and_copy()
+        mode = 'nearest'
         rebin = 0
         result = self.alg.execute(images, rebin, mode)
         npt.assert_equal(result, control)
 
-        rebin = -0
-        result = self.alg.execute(images, rebin, mode)
-        npt.assert_equal(result, control)
+    def test_executed_par_2(self):
+        self.do_execute(2.)
 
-    def test_executed_par(self):
-        self.do_execute()
+    def test_executed_par_5(self):
+        self.do_execute(5.)
 
-    def test_executed_seq(self):
+    def test_executed_seq_2(self):
         th.switch_mp_off()
-        self.do_execute()
+        self.do_execute(2.)
         th.switch_mp_on()
 
-    def do_execute(self):
+    def test_executed_seq_5(self):
+        th.switch_mp_off()
+        self.do_execute(5.)
+        th.switch_mp_on()
+
+    def do_execute(self, rebin=2.):
         images = th.gen_img_shared_array()
-
-        rebin = 2.  # twice the size
-        expected_x = int(images.shape[1] * rebin)
-        expected_y = int(images.shape[2] * rebin)
         mode = 'nearest'
-        result = self.alg.execute(images, rebin, mode)
-        npt.assert_equal(result.shape[1], expected_x)
-        npt.assert_equal(result.shape[2], expected_y)
 
-        rebin = 5.  # five times the size
         expected_x = int(images.shape[1] * rebin)
         expected_y = int(images.shape[2] * rebin)
-
         result = self.alg.execute(images, rebin, mode)
         npt.assert_equal(result.shape[1], expected_x)
         npt.assert_equal(result.shape[2], expected_y)
