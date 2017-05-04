3c9fc1ff8d9c25fc61134e7668759317f533c3f4
Dimitar Tasev <dimtasev@gmail.com>
2017-05-04 14:29:47 +0100

    Added more generic imgpy.sh

--- imgpy.sh
+++ imgpy.sh
@@ -1,2 +1,11 @@
-#!/bin/sh
-/home/matt/anaconda2/bin/python /home/matt/Documents/imgpy/main.py "$@"
+# set default to system python
+python_exec="/usr/bin/python"
+
+anaconda_dir="$HOME/anaconda2"
+
+if [ -d "$anaconda_dir" ]; then
+	# update python executable to anaconda python
+	python_exec="$anaconda_dir/bin/python"
+fi
+
+$python_exec $PWD/isis_imaging/main.py "$@"
