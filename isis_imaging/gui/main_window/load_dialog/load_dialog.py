58469c1c4bf1f702c0291c16ddfc56b1d389ddac
Dimitar Tasev <dimtasev@gmail.com>
2017-05-16 09:31:23 +0100

    save commit

--- isis_imaging/gui/main_window/load_dialog/load_dialog.py
+++ isis_imaging/gui/main_window/load_dialog/load_dialog.py
@@ -4,7 +4,7 @@ from PyQt4 import QtGui
 
 from core.algorithms import gui_compile_ui
 
-from core.imgdata.loader import get_file_names
+from core.imgdata.loader import get_file_names, get_file_extension
 
 import os
 
@@ -20,6 +20,7 @@ def select_file(field):
 
 
 class MWLoadDialog(QtGui.QDialog):
+
     def __init__(self, parent):
         super(MWLoadDialog, self).__init__(parent)
         gui_compile_ui.execute('gui/ui/load.ui', self)
@@ -33,46 +34,18 @@ class MWLoadDialog(QtGui.QDialog):
         self.darkButton.clicked.connect(lambda: select_file(self.darkPath))
 
         # if accepted load the stack
-        self.accepted.connect(parent.load_stack)
+        self.accepted.connect(parent.execute_load)
 
     def update_indices(self, select_file_result):
         """
-<<<<<<< Updated upstream
-        :param select_file_result: Will be None so discard
-||||||| merged common ancestors
-        :param select_file_result: Will be None, because it contains the return of the nested function self.select_file
-=======
         :param select_file_result: Will be None, because it contains the return of the nested function select_file
->>>>>>> Stashed changes
         """
-        str_path = str(self.samplePath)
-        dot_index = str_path.rfind('.')
-        file_extension = str_path[dot_index:]
-        print(file_extension)
+        file_extension = get_file_extension(str(self.samplePath.text()))
         get_file_names(self.load_path(), file_extension)
         self.index_end = len(get_file_names)
 
-<<<<<<< Updated upstream
-    def select_file(self, field):
-        assert isinstance(
-            field, QtGui.QLineEdit
-        ), "The passed object is of type {0}. This function only works with QLineEdit".format(
-            type(field))
-
-        # open file dialogue and set the text if file is selected
-        field.setText(QtGui.QFileDialog.getOpenFileName())
-
     def load_path(self):
         return os.path.basename(str(self.samplePath.text()))
-||||||| merged common ancestors
-    def select_file(self, field):
-        assert isinstance(
-            field, QtGui.QLineEdit
-        ), "The passed object is of type {0}. This function only works with QLineEdit".format(
-            type(field))
-
-        # open file dialogue and set the text if file is selected
-        field.setText(QtGui.QFileDialog.getOpenFileName())
 
     def sample_path(self):
         """
@@ -101,33 +74,3 @@ class MWLoadDialog(QtGui.QDialog):
     def indices(self):
         return self.index_start.value(), self.index_end.value(
         ), self.index_step.value()
-=======
-    def sample_path(self):
-        """
-        :return: The directory of the path as a Python string
-        """
-        return os.path.dirname(str(self.samplePath.text()))
-
-    def flat_path(self):
-        """
-        :return: The directory of the path as a Python string
-        """
-        return os.path.dirname(str(self.flatPath.text()))
-
-    def dark_path(self):
-        """
-        :return: The directory of the path as a Python string
-        """
-        return os.path.dirname(str(self.darkPath.text()))
-
-    def parallel_load(self):
-        """
-        :return: True if load should be in parallel, else False
-        """
-        return self.parallelLoad.isChecked()
-
-    def indices(self):
-        return self.index_start.value(), self.index_end.value(
-        ), self.index_step.value()
-|
->>>>>>> Stashed changes
--- isis_imaging/gui/main_window/mw_view.py
+++ isis_imaging/gui/main_window/mw_view.py
@@ -1,19 +1,19 @@
 from __future__ import absolute_import, division, print_function
 
-import os
 
 from PyQt4 import QtCore, QtGui
 from PyQt4.QtCore import Qt as Qt_
 
 from core.algorithms import gui_compile_ui
-from core.imgdata import loader
 from gui.main_window.mw_presenter import ImgpyMainWindowPresenter
+from gui.main_window.mw_presenter import Notification as PresNotification
 from gui.stack_visualiser.sv_view import ImgpyStackVisualiserView
 from gui.main_window.load_dialog.load_dialog import MWLoadDialog
 from gui.main_window.save_dialog.save_dialog import MWSaveDialog
 
 
 class ImgpyMainWindowView(QtGui.QMainWindow):
+
     def __init__(self, config):
         super(ImgpyMainWindowView, self).__init__()
         gui_compile_ui.execute('gui/ui/main_window.ui', self)
@@ -23,7 +23,7 @@ class ImgpyMainWindowView(QtGui.QMainWindow):
         self.actionLoad.triggered.connect(self.show_load_dialogue)
 
         self.actionSave.setShortcut('Ctrl+S')
-        self.actionLoad.triggered.connect(self.show_save_dialogue)
+        self.actionSave.triggered.connect(self.show_save_dialogue)
         # setting of shortcuts TODO move to func
         self.actionExit.triggered.connect(QtGui.qApp.quit)
         self.actionExit.setShortcut('Ctrl+Q')
@@ -40,30 +40,15 @@ class ImgpyMainWindowView(QtGui.QMainWindow):
         # actually show the dialogue
         self.load_dialogue.show()
 
-<<<<<<< Updated upstream
-    def load_stack(self):
-        # TODO actually notify presenter that it was loaded
-        # then presenter will load it and set it in the model
-        load_path = str(self.load_dialogue.load_path())
-        if not load_path:
-            return
+    def execute_save(self):
+        self.presenter.notify(PresNotification.SAVE)
 
-        # dirname removes the file name from the path
-        stack = loader.load(os.path.dirname(load_path))
-        self.add_stack_dock(stack)
-||||||| merged common ancestors
-    def execute_load_stack(self):
-        # TODO THIS DOES TOO MUCH, VIEW IS NOT SUPPOSED TO
-        self.presenter.notify(PresNotification.LOAD_STACK)
-=======
+    def execute_load(self):
+        self.presenter.notify(PresNotification.LOAD)
+
     def show_save_dialogue(self):
         self.save_dialogue = MWSaveDialog(self)
         self.save_dialogue.show()
-
-    def execute_load_stack(self):
-        # TODO THIS DOES TOO MUCH, VIEW IS NOT SUPPOSED TO
-        self.presenter.notify(PresNotification.LOAD_STACK)
->>>>>>> Stashed changes
 
     def add_stack_dock(self,
                        stack,
@@ -74,9 +59,6 @@ class ImgpyMainWindowView(QtGui.QMainWindow):
         self.stackvis = ImgpyStackVisualiserView(self, stack)
         self.dock_widget.setWidget(self.stackvis)
         self.dock_widget.setFloating(floating)
-
-    def set_value(self, value=5):
-        pass
 
     def median_filter_clicked(self):
         self.presenter.notify(
