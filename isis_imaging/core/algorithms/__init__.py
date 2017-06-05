6dd928d115218e44d463e4e7ca682267279aca21
Dimitar Tasev <dimtasev@gmail.com>
2017-06-05 15:41:42 +0100

    Algorithms and filters import their subpackages correctly

--- isis_imaging/core/algorithms/__init__.py
+++ isis_imaging/core/algorithms/__init__.py
@@ -22,4 +22,14 @@ from __future__ import (absolute_import, division, print_function)
 # File change history is stored at: <https://github.com/mantidproject/mantid>.
 # Code Documentation is available at: <http://doxygen.mantidproject.org>
 
+from isis_imaging.core.algorithms.registrator import all_modules
+from isis_imaging.core.algorithms.find_package import find_package
+
+# read in all of the available modules
+__all__ = all_modules(find_package(__file__))
+
+# delete the reference
+del all_modules, absolute_import, division, print_function
+
+# actually do the imports
 from . import *  # noqa: F401, F403
