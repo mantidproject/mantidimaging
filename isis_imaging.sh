bccce85eb1f8572e69beb5dd284a2efd871d31c1
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2017-06-20 15:34:10 +0100

    Renamed imgpy to isis_imaging

---
+++ isis_imaging.sh
@@ -0,0 +1,14 @@
+imgpy_dir=$HOME/isis_imaging
+
+# set default to system python
+python_exec="/usr/bin/python"
+
+anaconda_dir="$HOME/anaconda2"
+
+# if anaconda is installed in default directory
+if [ -d "$anaconda_dir" ]; then
+	# update python executable to anaconda python
+	python_exec="$anaconda_dir/bin/python"
+fi
+
+PYTHONPATH="$imgpy_dir:$PYTHONPATH" $python_exec $imgpy_dir/isis_imaging/main.py "$@"
