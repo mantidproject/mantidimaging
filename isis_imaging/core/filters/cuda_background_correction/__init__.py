675381853eb327c6cd12b0585e6d3a5d586351c1
Dimitar Borislavov Tasev <DTasev@users.noreply.github.com>
2017-06-21 11:47:16 +0100

    Revert "Cuda background correction"

--- isis_imaging/core/filters/cuda_background_correction/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import (absolute_import, division, print_function)
-
-from isis_imaging.core.filters.cuda_background_correction.cuda_background_correction import execute, _cli_register, _gui_register  # noqa:F401
-
-del absolute_import, division, print_function  # noqa:F821
