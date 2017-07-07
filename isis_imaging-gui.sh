d988d79fc2f38027300bcbfc6e192cb213fba56c
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2017-07-07 16:25:04 +0100

    Changed . to sh for no reason whatsoever

--- isis_imaging-gui.sh
+++ isis_imaging-gui.sh
@@ -4,6 +4,6 @@ imgpy_dir=${HOME}/isis_imaging
 # this will be used in the env script
 python_exec="python"
 
-. isis_imaging-env.sh
+sh isis_imaging-env.sh
 
 PYTHONPATH="$imgpy_dir:$PYTHONPATH" ${python_location} $imgpy_dir/isis_imaging/main.py --gui "$@"
--- isis_imaging-ipython.sh
+++ isis_imaging-ipython.sh
@@ -4,6 +4,6 @@ imgpy_dir=$HOME/isis_imaging
 # this will be used in the env script
 python_exec="ipython"
 
-. isis_imaging-env.sh
+sh isis_imaging-env.sh
 
 PYTHONPATH="$imgpy_dir:$PYTHONPATH" ${python_location} -i $imgpy_dir/isis_imaging/ipython.py "$@"
