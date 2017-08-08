836418149f297ef64bcba6a2e1741d9e150da75e
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2017-08-08 11:29:45 +0100

    Removed __getitem__ access to Images class as it is confusing

--- isis_imaging/core/io/loader/images.py
+++ isis_imaging/core/io/loader/images.py
@@ -3,24 +3,13 @@ from __future__ import (absolute_import, division, print_function)
 import numpy as np
 from isis_imaging import helper as h
 
+
 class Images(object):
     def __init__(self, sample, flat=None, dark=None, filenames=None):
         self.sample = sample
         self.flat = flat
         self.dark = dark
         self.filenames = filenames
-
-    def __getitem__(self, item):
-        if item == 0:
-            return self.sample
-        elif item == 1:
-            return self.flat
-        elif item == 2:
-            return self.dark
-        else:
-            raise IndexError(
-                "Please use index 0 to access Sample images, index 1 to access Flat images and index 2 to access Dark "
-                "images. Any other indices will raise a ValueError")
 
     def get_sample(self):
         return self.sample
