1809b13594dbb6f949c8af6cbbf3131ff204dce2
Dan Nixon <dan@dan-nixon.com>
2017-09-12 14:34:20 +0100

    Add COR file saving and loading functions

--- mantidimaging/core/imopr/helper.py
+++ mantidimaging/core/imopr/helper.py
@@ -1,5 +1,7 @@
 from __future__ import (absolute_import, division, print_function)
 
+import numpy as np
+
 # TODO should change the name of this file so its not confused with
 # the other helper.py that's not in imopr
 
@@ -24,3 +26,15 @@ def handle_indices(indices, retstep=False):
             return indices[0], indices[1]
         else:
             return indices[0], indices[1], indices[2]
+
+
+def new_cor_array(num_cors):
+    return np.ndarray(shape=(num_cors), dtype='int, float')
+
+
+def save_cors_to_file(filename, cors):
+    np.savetxt(filename, cors, fmt='%i %f', header='slice_index cor')
+
+
+def load_cors_from_file(filename):
+    return np.loadtxt(filename, dtype='int, float')
---
+++ mantidimaging/tests/core_test/imopr_test.py
@@ -0,0 +1,64 @@
+from __future__ import absolute_import, division, print_function
+
+import os
+import tempfile
+import unittest
+import numpy.testing as npt
+
+from mantidimaging.core.imopr.helper import (
+        new_cor_array, save_cors_to_file, load_cors_from_file)
+
+
+class ImoprTest(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(ImoprTest, self).__init__(*args, **kwargs)
+
+    def test_save_cor_file(self):
+        expected = (
+                '# slice_index cor\n' +
+                '0 0.000000\n' +
+                '2 5.500000\n' +
+                '4 11.000000\n' +
+                '6 16.500000\n' +
+                '8 22.000000\n')
+
+        cors = new_cor_array(5)
+        for i in range(5):
+            cors[i] = (i * 2, i * 5.5)
+
+        with tempfile.NamedTemporaryFile() as f:
+            output_path = os.path.dirname(f.name)
+            filename = os.path.join(output_path, 'cors.txt')
+
+            save_cors_to_file(filename, cors)
+
+            with open(filename) as tf:
+                text = tf.read()
+                self.assertEqual(text, expected)
+
+    def test_load_cor_file(self):
+        text_data = (
+                '0 0.000000\n' +
+                '2 5.500000\n' +
+                '4 11.000000\n' +
+                '6 16.500000\n' +
+                '8 22.000000\n')
+
+        expected = new_cor_array(5)
+        for i in range(5):
+            expected[i] = (i * 2, i * 5.5)
+
+        with tempfile.NamedTemporaryFile() as f:
+            output_path = os.path.dirname(f.name)
+            filename = os.path.join(output_path, 'cors.txt')
+
+            with open(filename, 'w') as tf:
+                tf.write(text_data)
+
+            cors = load_cors_from_file(filename)
+
+            npt.assert_array_equal(cors, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
