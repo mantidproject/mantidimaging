acbfe7ad3dfd0af7ca2d21ad6b5c978ec90e5671
Dan Nixon <dan@dan-nixon.com>
2017-09-14 11:17:08 +0100

    flake8 formatting

--- mantidimaging/core/io/loader/loader.py
+++ mantidimaging/core/io/loader/loader.py
@@ -4,7 +4,8 @@ from logging import getLogger
 
 import numpy as np
 
-from mantidimaging.core.io.utility import get_file_names, DEFAULT_IO_FILE_FORMAT
+from mantidimaging.core.io.utility import (
+        get_file_names, DEFAULT_IO_FILE_FORMAT)
 
 from mantidimaging.core.io.loader import img_loader, stack_loader
 
@@ -41,7 +42,8 @@ def _imread(filename):
 
 
 def supported_formats():
-    # ignore errors for unused import/variable, we are only checking availability
+    # ignore errors for unused import/variable, we are only checking
+    # availability
     try:
         import h5py  # noqa: F401
         h5nxs_available = True
@@ -68,7 +70,11 @@ def supported_formats():
     return avail_list
 
 
-def read_in_shape(input_path, in_format=DEFAULT_IO_FILE_FORMAT, data_dtype=np.float32, cores=None, chunksize=None):
+def read_in_shape(input_path,
+                  in_format=DEFAULT_IO_FILE_FORMAT,
+                  data_dtype=np.float32,
+                  cores=None,
+                  chunksize=None):
     input_file_names = get_file_names(input_path, in_format)
     images = load(input_path, None, None, in_format,
                   data_dtype, cores, chunksize, indices=[0, 1, 1])
@@ -81,15 +87,19 @@ def read_in_shape_from_config(config):
     """
     This function is intended for internal usage.
 
-    Read in ONLY the first image in the specified directory, and return the total shape
-    that all the images in that folder will have after loaded. This is determined by the
-    number of images, and the loaded image's width and height.
+    Read in ONLY the first image in the specified directory, and return the
+    total shape that all the images in that folder will have after loaded. This
+    is determined by the number of images, and the loaded image's width and
+    height.
 
-    It is assumed all images are the same. If they are not the loader will fail on runtime.
+    It is assumed all images are the same. If they are not the loader will fail
+    on runtime.
 
-    :param config: The reconstruction config from which the parameters are read.
+    :param config: The reconstruction config from which the parameters are
+                   read.
 
-    :returns: The full shape of the images in the specified directory in a tuple of (Length, X, Y)
+    :returns: The full shape of the images in the specified directory in a
+              tuple of (Length, X, Y)
     """
     input_path = config.func.input_path
     in_format = config.func.in_format
@@ -155,20 +165,24 @@ def load(input_path=None,
 
     :param chunksize: Default:None (auto calculated), chunk of work per worker
 
-    :param parallel_load: Default: False, if set to true the loading of the data
-                          will be done in parallel.
+    :param parallel_load: Default: False, if set to true the loading of the
+                          data will be done in parallel.
                           This could be faster depending on the IO system.
-                          For local runs (with HDD) recommended setting is False
+                          For local runs (with HDD) recommended setting is
+                          False
 
     :param file_names: Use provided file names for loading
 
     :param indices: Specify which indices are loaded from the found files.
-                    This **DOES NOT** check for the number in the image filename,
-                    but removes all indices from the filenames list that are not selected
+                    This **DOES NOT** check for the number in the image
+                    filename, but removes all indices from the filenames list
+                    that are not selected
 
-    :param construct_sinograms: The loaded images will be used to construct the sinograms during loading
+    :param construct_sinograms: The loaded images will be used to construct the
+                                sinograms during loading
 
-    :return: a tuple with shape 3: (sample, flat, dark), if no flat and dark were loaded, they will be None
+    :return: a tuple with shape 3: (sample, flat, dark), if no flat and dark
+             were loaded, they will be None
     """
     if in_format not in supported_formats():
         raise ValueError("Image format {0} not supported!".format(in_format))
@@ -194,8 +208,9 @@ def load(input_path=None,
             load_func = _imread
 
         images = img_loader.execute(
-            load_func, input_file_names, input_path_flat, input_path_dark, in_format, dtype, cores, chunksize,
-            parallel_load, indices, construct_sinograms)
+            load_func, input_file_names, input_path_flat, input_path_dark,
+            in_format, dtype, cores, chunksize, parallel_load, indices,
+            construct_sinograms)
 
     images.check_data_stack(images)
 
@@ -204,14 +219,19 @@ def load(input_path=None,
 
 def load_sinogram(input_path=None, sinogram_number=0, in_format=DEFAULT_IO_FILE_FORMAT, dtype=np.float32):
     """
-    This function is not be exposed to the CLI. It can only be accessed internally and through IPython interface.
-    The reason is because this function will have a lot slower performance than the normal load.
+    This function is not be exposed to the CLI.
+
+    It can only be accessed internally and through IPython interface.
+
+    The reason is because this function will have a lot slower performance than
+    the normal load.
     """
     if in_format not in supported_formats():
         raise ValueError("Image format {0} not supported!".format(in_format))
 
     if in_format == 'nxs':
-        raise NotImplementedError("This functionality is not yet implemented for NXS files")
+        raise NotImplementedError(
+                "This functionality is not yet implemented for NXS files")
 
     input_file_names = get_file_names(input_path, in_format)
 
@@ -224,7 +244,8 @@ def load_sinogram(input_path=None, sinogram_number=0, in_format=DEFAULT_IO_FILE_
     from mantidimaging.core.parallel import utility as pu
 
     # allocate memory for a single sinogram
-    output_data = pu.create_shared_array((1, num_images, img_shape[1]), dtype=dtype)
+    output_data = pu.create_shared_array(
+            (1, num_images, img_shape[1]), dtype=dtype)
 
     logging.getLogger(__name__).info("Output data shape: {}".format(output_data.shape))
 
@@ -233,7 +254,8 @@ def load_sinogram(input_path=None, sinogram_number=0, in_format=DEFAULT_IO_FILE_
     h.prog_init(num_images, "Loading sinograms")
 
     for idx, input_file in enumerate(input_file_names):
-        # read a single row from each projection, very wasteful but can quickly create a sinogram
+        # read a single row from each projection, very wasteful but can quickly
+        # create a sinogram
         loaded_image = _imread(input_file)
         output_data[0, idx, :] = loaded_image[:, sinogram_number]
         h.prog_update()
