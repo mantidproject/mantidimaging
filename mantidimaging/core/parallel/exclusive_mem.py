fa7075c1a6b27a4c888dc26353a38da75f59afde
Dan Nixon <dan@dan-nixon.com>
2017-09-29 15:23:38 +0100

    Start converting use of prog_init, pstart, pend, etc.

--- mantidimaging/core/parallel/exclusive_mem.py
+++ mantidimaging/core/parallel/exclusive_mem.py
@@ -2,15 +2,18 @@ from __future__ import (absolute_import, division, print_function)
 
 from multiprocessing import Pool
 
-from mantidimaging import helper as h
+from mantidimaging.core.algorithms.progress_reporting import Progress
 from mantidimaging.core.parallel import utility as pu
 
 
 def create_partial(func, **kwargs):
     """
-    Create a partial using functools.partial, to forward the kwargs to the parallel execution of imap.
+    Create a partial using functools.partial, to forward the kwargs to the
+    parallel execution of imap.
 
-    This version does not have a forwarding function, because one is not necessary.
+    This version does not have a forwarding function, because one is not
+    necessary.
+
     The data is copied regardless of it being forwarded or not.
 
     :param func: Function that will be executed
@@ -27,11 +30,14 @@ def execute(data=None,
             chunksize=None,
             name="Progress",
             output_data=None,
-            show_timer=True):
+            progress=None):
     """
-    Executes a function in parallel, but does not share the memory between processes.
-    Every process will copy-on-read/write the data to its own virtual memory region, perform the calculation
-    and return the result to the main process, where it will be moved (not copied) to the original container.
+    Executes a function in parallel, but does not share the memory between
+    processes.
+
+    Every process will copy-on-read/write the data to its own virtual memory
+    region, perform the calculation and return the result to the main process,
+    where it will be moved (not copied) to the original container.
 
     - imap_unordered gives the images back in random order!
     - map and map_async cannot replace the data in place and end up
@@ -44,16 +50,17 @@ def execute(data=None,
         output_data[i] = res_data[:]
     TypeError: 'NoneType' object has no attribute '__getitem__'
 
-    It means that the forwarded function is not returning anything and it should!
+    It means that the forwarded function is not returning anything and it
+    should!
 
     :param data: the data array that will be processed in parallel
-    :param partial_func: a function constructed using partial to pass the correct arguments
+    :param partial_func: a function constructed using partial to pass the
+                         correct arguments
     :param cores: number of cores that the processing will use
     :param chunksize: chunk of work per process(worker)
-    :param name: the string that will be appended in front of the progress bar
+    :param name: name of the task used in progress reporting
     :param output_data: the output array in which the results will be stored
-    :param show_timer: Whether to show the loading bar or be completely silent
-
+    :param progress: Progress instance to use for progress reporting (optional)
     :return: The processed output data
     """
     if not cores:
@@ -70,19 +77,21 @@ def execute(data=None,
 
     pool = Pool(cores)
     img_num = output_data.shape[0]
-    if show_timer:
-        h.prog_init(img_num,
-                    name + " " + str(cores) + "c " + str(chunksize) + "chs")
+
+    task_name = name + " " + str(cores) + "c " + str(chunksize) + "chs"
+    progress = Progress.ensure_instance(progress, num_steps=img_num,
+                                        task_name=task_name)
 
     # passing the data triggers a copy-on-write in the child process, even if
     # it only reads the data
     for i, res_data in enumerate(
             pool.imap(partial_func, data, chunksize=chunksize)):
         output_data[i] = res_data[:]
-        h.prog_update()
+        progress.update()
 
     pool.close()
     pool.join()
-    h.prog_close()
+
+    progress.mark_complete()
 
     return output_data
