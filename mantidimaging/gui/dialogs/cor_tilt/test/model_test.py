804c36b0b11977452d3e0e3feb8a9d136cae7d82
Dan Nixon <dan@dan-nixon.com>
2017-11-24 12:14:27 +0000

    Async tasks presenter tests
    Re #194

---
+++ mantidimaging/gui/dialogs/cor_tilt/test/model_test.py
@@ -0,0 +1,9 @@
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
+
+from mantidimaging.gui.dialogs.cor_tilt import CORTiltDialogModel
+
+
+class CORTiltDialogModelTest(unittest.TestCase):
+    pass
