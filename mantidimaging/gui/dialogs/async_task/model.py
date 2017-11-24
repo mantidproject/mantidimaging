304730b29f2ee2837ae3bc25c673272e37ad077b
Dan Nixon <dan@dan-nixon.com>
2017-11-24 10:06:13 +0000

    Async tasks dialog test
    Re #194

--- mantidimaging/gui/dialogs/async_task/model.py
+++ mantidimaging/gui/dialogs/async_task/model.py
@@ -4,62 +4,7 @@ from logging import getLogger
 
 from PyQt5 import Qt, QtCore
 
-from mantidimaging.core.utility.func_call import call_with_known_parameters
-
-
-class TaskWorkerThread(Qt.QThread):
-    """
-    Thread for running tasks asynchronously to the GUI.
-
-    Intended for internal use within this module only.
-
-    Usage:
-        t = TaskWorkerThread(parent)
-        t.task_function = ...
-        t.finished.connect(...)
-        t.start()
-
-    On completion inspect:
-        t.result
-        t.error
-    """
-
-    def __init__(self, parent=None):
-        super(TaskWorkerThread, self).__init__(parent)
-
-        self.task_function = None
-
-        self.args = []
-        self.kwargs = {}
-
-        self.result = None
-        self.error = None
-
-    def run(self):
-        log = getLogger(__name__)
-
-        try:
-            if self.task_function is None:
-                raise ValueError("No task function provided")
-
-            self.result = call_with_known_parameters(
-                    self.task_function, *self.args, **self.kwargs)
-
-        except Exception as e:
-            log.exception("Failed to execute task")
-            self.result = None
-            self.error = e
-
-    def was_successful(self):
-        """
-        Inspects the result and error values of the async task.
-
-        For a task to be "successful" the result must have a value and the
-        error must be None.
-
-        :return: True if task finished successfully
-        """
-        return self.result is not None and self.error is None
+from .task import TaskWorkerThread
 
 
 class AsyncTaskDialogModel(Qt.QObject):
---
+++ mantidimaging/gui/dialogs/async_task/test/task_test.py
@@ -0,0 +1,43 @@
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
+
+from mantidimaging.gui.dialogs.async_task.task import TaskWorkerThread
+
+
+class TaskWorkerThreadTest(unittest.TestCase):
+
+    def test_basic_happy_case(self):
+        def f(a, b):
+            return a + b
+
+        t = TaskWorkerThread()
+        t.task_function = f
+        t.kwargs = {'a': 5, 'b': 4}
+
+        t.run()
+
+        t.wait()
+
+        self.assertTrue(t.was_successful())
+        self.assertIsNone(t.error)
+        self.assertEquals(t.result, 9)
+
+    def test_failure(self):
+        def f(a, b):
+            raise RuntimeError('nope')
+
+        t = TaskWorkerThread()
+        t.task_function = f
+        t.kwargs = {'a': 5, 'b': 4}
+
+        t.run()
+
+        t.wait()
+
+        self.assertFalse(t.was_successful())
+        self.assertIsNotNone(t.error)
+        self.assertIsNone(t.result)
+
+        self.assertTrue(isinstance(t.error, RuntimeError))
+        self.assertEquals(t.error.args, ('nope', ))
