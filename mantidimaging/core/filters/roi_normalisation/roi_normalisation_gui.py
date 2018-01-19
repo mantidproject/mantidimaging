22fa8dc8bdc86cd593809c5769b03c971fca8d44
Dan Nixon <dan@dan-nixon.com>
2018-01-19 10:03:06 +0000

    Avoid GUI imports in core module

--- mantidimaging/core/filters/roi_normalisation/roi_normalisation_gui.py
+++ mantidimaging/core/filters/roi_normalisation/roi_normalisation_gui.py
@@ -1,11 +1,10 @@
 from functools import partial
 
-from mantidimaging.gui.windows.stack_visualiser import Parameters
-
 from . import execute
 
 
 def _gui_register(form, on_change):
+    from mantidimaging.gui.windows.stack_visualiser import Parameters
     from mantidimaging.gui.utility import add_property_to_form
 
     add_property_to_form(
