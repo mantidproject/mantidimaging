cb94fea02c5767fc8cd2573c2652e9f8ec777e67
Dan Nixon <dan@dan-nixon.com>
2018-03-01 14:27:50 +0000

    Add background correction with normalisation filter
    Adds a filter that performs background correction with sample and flat
    image normalisation by integrated open beam area (defined by a region of
    interest).
    
    F = ΣNfIf / |If|
    D = ΣId / |Id|
    T = (NsIs - D) / (F - D)
    
    Where Ns and Nf are arrays of normalisation factors for sample and flat
    field images, in this case obtained through integration (and
    normalisation) of an area of open beam.
    
    F and D are the average flat and dark field images used to uniformally
    correct each sample projection image.

---
+++ mantidimaging/core/filters/background_correction_and_normalisation/background_correction_and_normalisation_gui.py
@@ -0,0 +1,57 @@
+import os
+from functools import partial
+
+from mantidimaging.core import io
+
+from . import execute
+
+
+def _gui_register(form, on_change):
+    from mantidimaging.gui.windows.stack_visualiser import Parameters
+    from mantidimaging.gui.utility import add_property_to_form
+
+    flatPath, _ = add_property_to_form(
+            'Flat', 'file',
+            form=form, on_change=on_change)
+
+    darkPath, _ = add_property_to_form(
+            'Dark', 'file',
+            form=form, on_change=on_change)
+
+    add_property_to_form(
+            'Select ROI on stack visualiser.', 'label',
+            form=form, on_change=on_change)
+
+    params = {
+        'air_region': Parameters.ROI
+    }
+
+    def custom_execute():
+        flat_path = str(flatPath.text())
+        dark_path = str(darkPath.text())
+        flat_extension = io.utility.get_file_extension(flat_path)
+        dark_extension = io.utility.get_file_extension(dark_path)
+
+        flat_dir = os.path.dirname(flat_path)
+        dark_dir = os.path.dirname(dark_path)
+
+        images_flat_only = io.loader.load(flat_dir, in_format=flat_extension)
+
+        # this will be put in the 'sample' attribute, because we load a single
+        # volume
+        flat = images_flat_only.sample.mean(axis=0)
+
+        images_dark_only = io.loader.load(dark_dir, in_format=dark_extension)
+
+        # this will be put in the 'sample' attribute, because we load a single
+        # volume
+        dark = images_dark_only.sample.mean(axis=0)
+
+        par = partial(execute, flat=flat, dark=dark)
+
+        return par
+
+    return (params,
+            None,
+            custom_execute,
+            None)
