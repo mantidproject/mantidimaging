1d2fbd49744ba4ab8f0f872babb636bf71d694bd
Will Taylor <william.h.taylor@stfc.ac.uk>
2020-01-13 16:31:53 +0000

    Removed preview pane from savu filter window

--- mantidimaging/gui/windows/savu_filters/presenter.py
+++ mantidimaging/gui/windows/savu_filters/presenter.py
@@ -1,17 +1,12 @@
-from enum import Enum
+from enum import Enum, auto
 from logging import getLogger
 from typing import List, TYPE_CHECKING
 from uuid import UUID
 
-import numpy as np
 from PyQt5.QtWidgets import QWidget
 from requests import Response
 
-from mantidimaging.core.data import Images
-from mantidimaging.core.utility.histogram import generate_histogram_from_image
-from mantidimaging.core.utility.progress_reporting import Progress
 from mantidimaging.gui.mvp_base import BasePresenter
-from mantidimaging.gui.utility import BlockQtSignals, get_parameters_from_stack
 from mantidimaging.gui.utility import add_property_to_form
 from mantidimaging.gui.windows.savu_filters.job_run_response import JobRunResponseContent
 from mantidimaging.gui.windows.savu_filters.model import SavuFiltersWindowModel, CurrentFilterData
@@ -24,11 +19,8 @@ if TYPE_CHECKING:
 
 
 class Notification(Enum):
-    REGISTER_ACTIVE_FILTER = 1
-    APPLY_FILTER = 2
-    UPDATE_PREVIEWS = 3
-    SCROLL_PREVIEW_UP = 4
-    SCROLL_PREVIEW_DOWN = 5
+    REGISTER_ACTIVE_FILTER = auto()
+    APPLY_FILTER = auto()
 
 
 class SavuFiltersWindowPresenter(BasePresenter):
@@ -49,20 +41,10 @@ class SavuFiltersWindowPresenter(BasePresenter):
                 self.do_register_active_filter()
             elif signal == Notification.APPLY_FILTER:
                 self.do_apply_filter()
-            elif signal == Notification.UPDATE_PREVIEWS:
-                self.do_update_previews()
-            elif signal == Notification.SCROLL_PREVIEW_UP:
-                self.do_scroll_preview(1)
-            elif signal == Notification.SCROLL_PREVIEW_DOWN:
-                self.do_scroll_preview(-1)
 
         except Exception as e:
             self.show_error(e)
             getLogger(__name__).exception("Notification handler failed")
-
-    @property
-    def max_preview_image_idx(self):
-        return max(self.model.num_images_in_stack - 1, 0)
 
     def set_stack_uuid(self, uuid: UUID):
         self.set_stack(self.main_window.get_stack_visualiser(uuid) if uuid is not None else None)
@@ -78,31 +60,10 @@ class SavuFiltersWindowPresenter(BasePresenter):
 
         self.model.stack = stack
 
-        # Update the preview image index
-        with BlockQtSignals([self.view]):
-            self.set_preview_image_index(0)
-            self.view.previewImageIndex.setMaximum(self.max_preview_image_idx)
-
-        self.do_update_previews(False)
-
     def handle_roi_selection(self, roi):
         if roi:
             # TODO used to check  and self.filter_uses_auto_property(SVParameters.ROI): but disabled for now
             self.view.auto_update_triggered.emit()
-
-    def set_preview_image_index(self, image_idx):
-        """
-        Sets the current preview image index.
-        """
-        self.model.preview_image_idx = image_idx
-
-        # Set preview index spin box to new index
-        preview_idx_spin = self.view.previewImageIndex
-        with BlockQtSignals([preview_idx_spin]):
-            preview_idx_spin.setValue(self.model.preview_image_idx)
-
-        # Trigger preview updating
-        self.view.auto_update_triggered.emit()
 
     def do_register_active_filter(self):
         # clear the fields of the previous filter
@@ -141,90 +102,6 @@ class SavuFiltersWindowPresenter(BasePresenter):
     def do_apply_filter(self):
         self.view.clear_output_text()
         self.model.do_apply_filter(self.current_filter)
-
-    def do_update_previews(self, maintain_axes=True):
-        log = getLogger(__name__)
-
-        progress = Progress.ensure_instance()
-        progress.task_name = "Filter preview"
-        progress.add_estimated_steps(1)
-
-        with progress:
-            progress.update(msg="Getting stack")
-            stack = self.model.stack_presenter
-
-            # If there is no stack then clear the preview area
-            if stack is None:
-                self.view.clear_preview_plots()
-
-            else:
-                # Add the remaining steps for calculating the preview
-                progress.add_estimated_steps(8)
-
-                before_image_data = stack.get_image(self.model.preview_image_idx)
-
-                if maintain_axes:
-                    # Record the image axis range from the existing preview
-                    # image
-                    image_axis_ranges = ((self.view.preview_image_before.get_xlim(),
-                                          self.view.preview_image_before.get_ylim())
-                                         if self.view.preview_image_before.images else None)
-
-                # Update image before
-                self._update_preview_image(before_image_data, self.view.preview_image_before,
-                                           self.view.preview_histogram_before, progress)
-
-                # Generate sub-stack and run filter
-                progress.update(msg="Running preview filter")
-                exec_kwargs = get_parameters_from_stack(stack, self.model.parameters_from_stack)
-
-                filtered_image_data = None
-                try:
-                    sub_images = Images(np.asarray([before_image_data]))
-                    self.model.apply_filter(sub_images, exec_kwargs)
-                    filtered_image_data = sub_images.sample[0]
-                except Exception as e:
-                    log.error("Error applying filter for preview: {}".format(e))
-
-                # Update image after
-                if filtered_image_data is not None:
-                    self._update_preview_image(filtered_image_data, self.view.preview_image_after,
-                                               self.view.preview_histogram_after, progress)
-
-                if maintain_axes:
-                    # Set the axis range on the newly created image to keep
-                    # same zoom level/pan region
-                    if image_axis_ranges is not None:
-                        self.view.preview_image_before.set_xlim(image_axis_ranges[0])
-                        self.view.preview_image_before.set_ylim(image_axis_ranges[1])
-
-            # Redraw
-            progress.update(msg="Redraw canvas")
-            self.view.canvas.draw()
-
-    def _update_preview_image(self, image_data, image, histogram, progress):
-        # Generate histogram data
-        progress.update(msg="Generating histogram")
-        center, hist, _ = generate_histogram_from_image(image_data)
-
-        # Update image
-        progress.update(msg="Updating image")
-        # TODO: ideally this should update the data without replotting but a
-        # valid image must exist to start with (which may not always happen)
-        # and this only works as long as the extents do not change.
-        image.cla()
-        image.imshow(image_data, cmap=self.view.cmap)
-
-        # Update histogram
-        progress.update(msg="Updating histogram")
-        histogram.lines[0].set_data(center, hist)
-        histogram.relim()
-        histogram.autoscale()
-
-    def do_scroll_preview(self, offset):
-        idx = self.model.preview_image_idx + offset
-        idx = max(min(idx, self.max_preview_image_idx), 0)
-        self.set_preview_image_index(idx)
 
     def do_job_submission_success(self, response_content: JobRunResponseContent):
         self.remote_presenter.do_job_submission_success(response_content)
--- mantidimaging/gui/windows/savu_filters/view.py
+++ mantidimaging/gui/windows/savu_filters/view.py
@@ -3,13 +3,10 @@ from typing import TYPE_CHECKING
 
 from PyQt5 import Qt
 from PyQt5.QtWidgets import QLabel, QMainWindow, QTextEdit
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
-from matplotlib.figure import Figure
 
 from mantidimaging.core.configs.savu_backend_docker import RemoteConfig, RemoteConstants
 from mantidimaging.gui.mvp_base import BaseMainWindowView
 from mantidimaging.gui.utility import delete_all_widgets_from_layout
-from mantidimaging.gui.windows.filters.navigation_toolbar import FiltersWindowNavigationToolbar
 from mantidimaging.gui.windows.savu_filters.presenter import Notification as PresNotification
 from mantidimaging.gui.windows.savu_filters.presenter import SavuFiltersWindowPresenter
 from mantidimaging.gui.windows.savu_filters.remote_presenter import SavuFiltersRemotePresenter
@@ -19,13 +16,12 @@ if TYPE_CHECKING:
 
 
 class SavuFiltersWindowView(BaseMainWindowView):
-    auto_update_triggered = Qt.pyqtSignal()
     new_output = Qt.pyqtSignal(str)
     savu_finished = Qt.pyqtSignal(str)
     info: QLabel
     description: QLabel
 
-    def __init__(self, main_window: 'MainWindowView', cmap='Greys_r'):
+    def __init__(self, main_window: 'MainWindowView'):
         """
         TODO add Show plugins directory button
         Use qt/python/mantidqt/utils/show_in_explorer.py from Mantid
@@ -48,57 +44,13 @@ class SavuFiltersWindowView(BaseMainWindowView):
         # Populate list of filters and handle filter selection
         self.filterSelector.addItems(self.presenter.model.filter_names)
         self.filterSelector.currentIndexChanged[int].connect(self.handle_filter_selection)
-        self.handle_filter_selection(0)
+        self.handle_filter_selection()
 
         # Handle stack selection
         self.stackSelector.stack_selected_uuid.connect(self.presenter.set_stack_uuid)
-        self.stackSelector.stack_selected_uuid.connect(self.auto_update_triggered.emit)
 
         # Handle apply filter
         self.applyButton.clicked.connect(lambda: self.presenter.notify(PresNotification.APPLY_FILTER))
-
-        # Preview area
-        self.cmap = cmap
-
-        self.figure = Figure(tight_layout=True)
-
-        self.canvas = FigureCanvasQTAgg(self.figure)
-        self.canvas.setParent(self)
-
-        self.toolbar = FiltersWindowNavigationToolbar(self.canvas, self)
-        self.toolbar.filter_window = self
-
-        self.mplLayout.addWidget(self.toolbar)
-        self.mplLayout.addWidget(self.canvas)
-
-        def add_plot(num, title, **kwargs):
-            plt = self.figure.add_subplot(num, title=title, **kwargs)
-            return plt
-
-        self.preview_image_before = add_plot(221, 'Image Before')
-
-        self.preview_image_after = add_plot(223,
-                                            'Image After',
-                                            sharex=self.preview_image_before,
-                                            sharey=self.preview_image_before)
-
-        self.preview_histogram_before = add_plot(222, 'Histogram Before')
-        self.preview_histogram_before.plot([], [])
-
-        self.preview_histogram_after = add_plot(224,
-                                                'Histogram After',
-                                                sharex=self.preview_histogram_before,
-                                                sharey=self.preview_histogram_before)
-        self.preview_histogram_after.plot([], [])
-
-        self.clear_preview_plots()
-
-        # Handle preview index selection
-        self.previewImageIndex.valueChanged[int].connect(self.presenter.set_preview_image_index)
-
-        # Preview update triggers
-        self.auto_update_triggered.connect(self.on_auto_update_triggered)
-        self.updatePreviewButton.clicked.connect(lambda: self.presenter.notify(PresNotification.UPDATE_PREVIEWS))
 
         self.stackSelector.subscribe_to_main_window(main_window)
 
@@ -108,9 +60,8 @@ class SavuFiltersWindowView(BaseMainWindowView):
 
     def show(self):
         super(SavuFiltersWindowView, self).show()
-        self.auto_update_triggered.emit()
 
-    def handle_filter_selection(self, filter_idx):
+    def handle_filter_selection(self):
         """
         Handle selection of a filter from the drop down list.
         """
@@ -119,28 +70,6 @@ class SavuFiltersWindowView(BaseMainWindowView):
 
         # Do registration of new filter
         self.presenter.notify(PresNotification.REGISTER_ACTIVE_FILTER)
-
-        # Update preview on filter selection (on the off chance the default
-        # options are valid)
-        self.auto_update_triggered.emit()
-
-    def on_auto_update_triggered(self):
-        """
-        Called when the signal indicating the filter, filter properties or data
-        has changed such that the previews are now out of date.
-        """
-        if self.previewAutoUpdate.isChecked() and self.isVisible():
-            self.presenter.notify(PresNotification.UPDATE_PREVIEWS)
-
-    def clear_preview_plots(self):
-        """
-        Clears the plotted data from the preview images and plots.
-        """
-        self.preview_image_before.cla()
-        self.preview_image_after.cla()
-
-        self.preview_histogram_before.lines[0].set_data([], [])
-        self.preview_histogram_after.lines[0].set_data([], [])
 
     def set_description(self, info, desc):
         self.info.setText("\n".join([info, desc]))
