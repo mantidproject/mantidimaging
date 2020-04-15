44f3dd0a5cd686ef767e5666140691dbdc3e26e8
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-04-15 15:01:46 +0100

    variables for skip condition and reason

--- mantidimaging/core/gpu/test/gpu_test.py
+++ mantidimaging/core/gpu/test/gpu_test.py
@@ -8,6 +8,10 @@ from mantidimaging.core.filters.median_filter import MedianFilter, modes
 from mantidimaging.core.gpu import utility as gpu
 
 
+GPU_NOT_AVAIL = not gpu.gpu_available()
+GPU_SKIP_REASON = "Skip GPU tests if cupy isn't installed."
+
+
 class GPUTest(unittest.TestCase):
     """
     Test median filter.
@@ -18,37 +22,37 @@ class GPUTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(GPUTest, self).__init__(*args, **kwargs)
 
-    @unittest.skipIf(
-        not gpu.gpu_available(), reason="Skip GPU tests if cupy isn't installed"
-    )
+    @unittest.skipIf(GPU_NOT_AVAIL, reason=GPU_SKIP_REASON)
     def test_numpy_pad_modes_match_scipy_median_modes(self):
 
+        size = 3
         for mode in modes():
             with self.subTest(mode=mode):
 
                 images = th.gen_img_shared_array_and_copy()[0]
 
-                size = 3
+                gpu_result = MedianFilter.filter_func(images.copy(), size, mode)
                 th.switch_gpu_off()
                 cpu_result = MedianFilter.filter_func(images.copy(), size, mode)
                 th.switch_gpu_on()
-                gpu_result = MedianFilter.filter_func(images.copy(), size, mode)
 
-                npt.assert_equal(gpu_result, cpu_result)
+                npt.assert_almost_equal(gpu_result, cpu_result)
 
+    @unittest.skipIf(GPU_NOT_AVAIL, reason=GPU_SKIP_REASON)
     def test_gpu_result_matches_cpu_result_for_different_filter_sizes(self):
 
+        mode = "reflect"
         for size in [5, 7, 9]:
             with self.subTest(size=size):
+
                 images = th.gen_img_shared_array_and_copy()[0]
 
-                mode = "reflect"
                 th.switch_gpu_off()
                 cpu_result = MedianFilter.filter_func(images.copy(), size, mode)
                 th.switch_gpu_on()
                 gpu_result = MedianFilter.filter_func(images.copy(), size, mode)
 
-                npt.assert_equal(gpu_result, cpu_result)
+                npt.assert_almost_equal(gpu_result, cpu_result)
 
 
 if __name__ == "__main__":
