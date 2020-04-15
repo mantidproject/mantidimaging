5d2ef438a0f8e076e4e5a971fe6a2bea68cf7065
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-04-15 14:33:29 +0100

    filter size test

--- mantidimaging/core/gpu/test/gpu_test.py
+++ mantidimaging/core/gpu/test/gpu_test.py
@@ -4,8 +4,7 @@ from unittest import mock
 import numpy.testing as npt
 
 import mantidimaging.test_helpers.unit_test_helper as th
-from mantidimaging.core.filters.median_filter import MedianFilter
-from mantidimaging.core.filters.median_filter import modes
+from mantidimaging.core.filters.median_filter import MedianFilter, modes
 from mantidimaging.core.gpu import utility as gpu
 
 
@@ -29,6 +28,21 @@ class GPUTest(unittest.TestCase):
             images = th.gen_img_shared_array_and_copy()[0]
 
             size = 3
+            th.switch_gpu_off()
+            cpu_result = MedianFilter.filter_func(images.copy(), size, mode)
+            th.switch_gpu_on()
+            gpu_result = MedianFilter.filter_func(images.copy(), size, mode)
+
+            with self.subTest():
+                npt.assert_equal(gpu_result, cpu_result)
+
+    def test_gpu_result_matches_cpu_result_for_different_filter_sizes(self):
+
+        for size in [5, 7, 9]:
+
+            images = th.gen_img_shared_array_and_copy()[0]
+
+            mode = "reflect"
             th.switch_gpu_off()
             cpu_result = MedianFilter.filter_func(images.copy(), size, mode)
             th.switch_gpu_on()
