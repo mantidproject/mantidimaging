e986f1183dd2c50972196992f7de74ba19af54ed
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-04-21 13:59:18 +0100

    working test for adding arrays

--- mantidimaging/core/gpu/test/import_test.py
+++ mantidimaging/core/gpu/test/import_test.py
@@ -1,21 +1,18 @@
-import sys
 import unittest
 
 from unittest import mock
 
 
 class GPUImportTest(unittest.TestCase):
-    @unittest.skip("Cupy seems to load successfuly regardless")
     def test_gpu_available_returns_false_when_cupy_cant_be_loaded(self):
 
-        sys.modules["cupy"] = None
-        from mantidimaging.core.gpu import utility
-        assert not utility.gpu_available()
+        with mock.patch.dict("sys.modules", cupy=None):
+            from mantidimaging.core.gpu import utility
+            assert not utility.gpu_available()
 
     def test_gpu_available_returns_false_when_cupy_isnt_installed_properly(self):
 
         import cupy
-        with mock.patch("mantidimaging.core.gpu.utility.cp.core.core.ndarray.__mul__",
-                        side_effect=cupy.cuda.compiler.CompileException):
+        with mock.patch("cupy.add", side_effect=cupy.cuda.compiler.CompileException("", "", "", None)):
             from mantidimaging.core.gpu import utility
             assert not utility.gpu_available()
