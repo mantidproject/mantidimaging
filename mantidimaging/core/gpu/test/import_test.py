c8759ca84138e74b64068e255826a1f676f8200b
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-04-22 13:30:54 +0100

    tidying

--- mantidimaging/core/gpu/test/import_test.py
+++ mantidimaging/core/gpu/test/import_test.py
@@ -10,9 +10,8 @@ class GPUImportTest(unittest.TestCase):
     @unittest.skip("I don't know how to mock a failed import.")
     def test_gpu_available_returns_false_when_cupy_cant_be_loaded(self):
         """
-        Test that the GPU utility reports that the GPU is not available if cupy isn't installed.
+        Test that the GPU utility reports the GPU is not available if cupy isn't installed.
         """
-
         with mock.patch("mantidimaging.core.gpu.utility.cp", side_effect=ModuleNotFoundError):
             from mantidimaging.core.gpu import utility
             assert not utility.gpu_available()
@@ -20,7 +19,7 @@ class GPUImportTest(unittest.TestCase):
     @unittest.skipIf(GPU_NOT_AVAIL, reason="Can't run GPU tests without cupy installed.")
     def test_gpu_available_returns_false_when_cupy_isnt_installed_properly(self):
         """
-        Test that the GPU utility reports that the GPU is not available if cupy is installed but not set up properly.
+        Test that the GPU utility reports the GPU is not available if cupy is installed but not set up properly.
         """
         import cupy
         with mock.patch("cupy.add", side_effect=cupy.cuda.compiler.CompileException("", "", "", None)):
