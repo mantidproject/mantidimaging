c19b131fc19ee5eee5d89c7feccb68eb09d9d638
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-04-22 13:04:14 +0100

    extra test comments

--- mantidimaging/core/gpu/test/import_test.py
+++ mantidimaging/core/gpu/test/import_test.py
@@ -9,6 +9,9 @@ GPU_NOT_AVAIL = not gpu.gpu_available()
 class GPUImportTest(unittest.TestCase):
     @unittest.skip("I don't know how to mock a failed import.")
     def test_gpu_available_returns_false_when_cupy_cant_be_loaded(self):
+        """
+        Test that the GPU utility reports that the GPU is not available if cupy isn't installed.
+        """
 
         with mock.patch("mantidimaging.core.gpu.utility.cp", side_effect=ModuleNotFoundError):
             from mantidimaging.core.gpu import utility
@@ -16,7 +19,9 @@ class GPUImportTest(unittest.TestCase):
 
     @unittest.skipIf(GPU_NOT_AVAIL, reason="Can't run GPU tests without cupy installed.")
     def test_gpu_available_returns_false_when_cupy_isnt_installed_properly(self):
-
+        """
+        Test that the GPU utility reports that the GPU is not available if cupy is installed but not set up properly.
+        """
         import cupy
         with mock.patch("cupy.add", side_effect=cupy.cuda.compiler.CompileException("", "", "", None)):
             from mantidimaging.core.gpu import utility
