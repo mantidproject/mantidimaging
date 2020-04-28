3b13cf2c4e1149ad2c4c86019a796fbfa825ce6e
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-04-28 18:10:14 +0100

    gpu check/init for median filter

--- mantidimaging/core/filters/median_filter/median_filter.py
+++ mantidimaging/core/filters/median_filter/median_filter.py
@@ -36,8 +36,10 @@ class MedianFilter(BaseFilter):
         h.check_data_stack(data)
 
         if size and size > 1:
-            if not force_cpu:
+            if not force_cpu and gpu.gpu_available():
                 data = _execute_gpu(data.data, size, mode, progress)
+            elif pu.multiprocessing_available():
+                data = _execute_par(data, size, mode, cores, chunksize, progress)
             else:
                 _execute(data.data, size, mode, cores, chunksize, progress)
 
@@ -57,14 +59,14 @@ class MedianFilter(BaseFilter):
                                             form=form,
                                             on_change=on_change)
 
-        return {'size_field': size_field, 'mode_field': mode_field, 'use_gpu_field': gpu_field}
+        return {'size_field': size_field, 'mode_field': mode_field, 'gpu_field': gpu_field}
 
     @staticmethod
-    def execute_wrapper(size_field=None, mode_field=None, use_gpu_field=None):
+    def execute_wrapper(size_field=None, mode_field=None, gpu_field=None):
         return partial(MedianFilter.filter_func,
                        size=size_field.value(),
                        mode=mode_field.currentText(),
-                       force_cpu=not use_gpu_field.isChecked())
+                       force_cpu=not gpu_field.isChecked())
 
 
 def modes():
