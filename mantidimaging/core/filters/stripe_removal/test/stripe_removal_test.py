3e537d067b2d6a402692399fe4cb5a7ce51c0585
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-06-02 10:32:48 +0100

    Fixed all tests except rebin

--- mantidimaging/core/filters/stripe_removal/test/stripe_removal_test.py
+++ mantidimaging/core/filters/stripe_removal/test/stripe_removal_test.py
@@ -19,121 +19,111 @@ class StripeRemovalTest(unittest.TestCase):
         super(StripeRemovalTest, self).__init__(*args, **kwargs)
 
     def test_not_executed(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = None
-            ti = None
-            sf = None
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = None
+        ti = None
+        sf = None
 
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
 
-            npt.assert_equal(result, control)
-            npt.assert_equal(images, control)
+        npt.assert_equal(result, control)
+        npt.assert_equal(images, control)
 
     def test_executed_wf(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = ["level=1"]
-            ti = None
-            sf = None
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = ["level=1"]
+        ti = None
+        sf = None
 
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
 
-            th.assert_not_equals(result, control)
+        th.assert_not_equals(result, control)
 
     def test_executed_wf_dict(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = {"level": 1}
-            ti = None
-            sf = None
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
-            th.assert_not_equals(result, control)
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = {"level": 1}
+        ti = None
+        sf = None
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        th.assert_not_equals(result, control)
 
     def test_executed_ti(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = None
-            ti = ['nblock=2']
-            sf = None
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = None
+        ti = ['nblock=2']
+        sf = None
 
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
 
-            th.assert_not_equals(result, control)
+        th.assert_not_equals(result, control)
 
     def test_executed_ti_dict(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = None
-            ti = {"nblock": 2}
-            sf = None
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
-            th.assert_not_equals(result, control)
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = None
+        ti = {"nblock": 2}
+        sf = None
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        th.assert_not_equals(result, control)
 
     def test_executed_sf(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = None
-            ti = None
-            sf = ['size=5']
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = None
+        ti = None
+        sf = ['size=5']
 
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
 
-            th.assert_not_equals(result, control)
+        th.assert_not_equals(result, control)
 
     def test_executed_sf_dict(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = None
-            ti = None
-            sf = {"size": 5}
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
-            th.assert_not_equals(result, control)
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = None
+        ti = None
+        sf = {"size": 5}
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        th.assert_not_equals(result, control)
 
     def test_memory_executed_wf(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = ["level=1"]
-            ti = None
-            sf = None
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = ["level=1"]
+        ti = None
+        sf = None
 
-            cached_memory = get_memory_usage_linux(kb=True)[0]
+        cached_memory = get_memory_usage_linux(kb=True)[0]
 
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
 
-            self.assertLess(get_memory_usage_linux(kb=True)[0], cached_memory * 1.1)
+        self.assertLess(get_memory_usage_linux(kb=True)[0], cached_memory * 1.1)
 
-            th.assert_not_equals(result, control)
+        th.assert_not_equals(result, control)
 
     def test_memory_executed_ti(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = None
-            ti = ['nblock=2']
-            sf = None
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = None
+        ti = ['nblock=2']
+        sf = None
 
-            cached_memory = get_memory_usage_linux(kb=True)[0]
+        cached_memory = get_memory_usage_linux(kb=True)[0]
 
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
 
-            self.assertLess(get_memory_usage_linux(kb=True)[0], cached_memory * 1.1)
+        self.assertLess(get_memory_usage_linux(kb=True)[0], cached_memory * 1.1)
 
-            th.assert_not_equals(result, control)
+        th.assert_not_equals(result, control)
 
     def test_memory_executed_sf(self):
-        with th.gen_img_shared_array_and_copy() as generated:
-            images, control = generated
-            wf = None
-            ti = None
-            sf = ['size=5']
+        images, control = th.gen_img_shared_array_and_copy()
+        wf = None
+        ti = None
+        sf = ['size=5']
 
-            cached_memory = get_memory_usage_linux(kb=True)[0]
+        cached_memory = get_memory_usage_linux(kb=True)[0]
 
-            result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
+        result = StripeRemovalFilter.filter_func(images, wf, ti, sf)
 
-            self.assertLess(get_memory_usage_linux(kb=True)[0], cached_memory * 1.1)
+        self.assertLess(get_memory_usage_linux(kb=True)[0], cached_memory * 1.1)
 
-            th.assert_not_equals(result, control)
+        th.assert_not_equals(result, control)
 
     def test_execute_wrapper_return_is_runnable(self):
         """
@@ -143,8 +133,8 @@ class StripeRemovalTest(unittest.TestCase):
         value_filter_type.currentText = mock.Mock(return_value="type")
         execute_func = StripeRemovalFilter.execute_wrapper(value_filter_type)
 
-        with th.gen_img_shared_array() as images:
-            execute_func(images)
+        images = th.gen_img_shared_array()
+        execute_func(images)
 
         self.assertEqual(value_filter_type.currentText.call_count, 1)
 
