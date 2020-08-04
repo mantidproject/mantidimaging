f0c5876b395bcd16e562eaeff5cdca5581654e45
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-08-04 16:48:56 +0100

    YAPF formatting round 2

--- mantidimaging/gui/windows/recon/test/presenter_test.py
+++ mantidimaging/gui/windows/recon/test/presenter_test.py
@@ -97,8 +97,8 @@ class ReconWindowPresenterTest(unittest.TestCase):
         self.presenter.model.preview_slice_idx = 15
 
         self.presenter.notify(PresNotification.ADD_COR)
-        self.view.add_cor_table_row.assert_called_once_with(
-            self.presenter.model.selected_row, self.presenter.model.preview_slice_idx, 15)
+        self.view.add_cor_table_row.assert_called_once_with(self.presenter.model.selected_row,
+                                                            self.presenter.model.preview_slice_idx, 15)
         mock_get_me_a_cor.assert_called_once()
 
     @mock.patch('mantidimaging.gui.windows.recon.model.get_reconstructor_for')
