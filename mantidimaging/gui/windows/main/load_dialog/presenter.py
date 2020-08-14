2dc4f9fbab9e8abb083be19457f5c93aaab31d47
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-08-14 11:45:46 +0100

    Logfile parser test, and cleanup of comments

--- mantidimaging/gui/windows/main/load_dialog/presenter.py
+++ mantidimaging/gui/windows/main/load_dialog/presenter.py
@@ -35,8 +35,6 @@ class LoadPresenter:
             self.do_update_sample()
         elif n == Notification.UPDATE_OTHER:
             self.do_update_other(**baggage)
-        # elif n == Notification.UPDATE_EXPECTED_MEM_USAGE:
-        #     self.do_update_expected_memory_usage()
 
     def do_update_sample(self):
         """
@@ -55,12 +53,12 @@ class LoadPresenter:
         filename = self.view.sample.path_text()
         dirname = self.view.sample.directory()
         try:
-            self.last_shape, sinograms = read_in_shape(dirname,
-                                                       in_prefix=get_prefix(filename),
+            self.last_shape, sinograms = read_in_shape(dirname, in_prefix=get_prefix(filename),
                                                        in_format=self.image_format)
         except Exception as e:
             getLogger(__name__).error(f"Failed to read file {sample_filename} {e}")
-            self.view.show_error("Failed to read this file. See log for details.", traceback.format_exc())
+            self.view.show_error("Failed to read this file. See log for details.",
+                                 traceback.format_exc())
             self.last_shape = (0, 0, 0)
             sinograms = False
 
