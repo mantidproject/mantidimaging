04daf72655747e1a9f03e49df92ccf5deaa70d86
Samuel Jones <samuel.jones@stfc.ac.uk>
2020-09-29 14:31:50 +0100

    Re #505 Ensure remove const args, tests are updated.

--- mantidimaging/core/data/test/images_test.py
+++ mantidimaging/core/data/test/images_test.py
@@ -34,11 +34,6 @@ class ImagesTest(unittest.TestCase):
 
         imgs.record_operation('test_func',
                               'A pretty name',
-                              56,
-                              9002,
-                              np.ndarray((800, 1024, 1024)),
-                              'yes',
-                              False,
                               this=765,
                               that=495.0,
                               roi=(1, 2, 3, 4))
@@ -47,7 +42,6 @@ class ImagesTest(unittest.TestCase):
             'operation_history': [{
                 'name': 'test_func',
                 'display_name': 'A pretty name',
-                'args': [56, 9002, None, 'yes', False],
                 'kwargs': {
                     'this': 765,
                     'that': 495.0,
