6f885dcf446643dc4d5a00bcb0740de1f86c57d7
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-11-05 15:51:14 +0000

    Tests correct sorting of uuids

--- mantidimaging/gui/windows/main/test/save_test.py
+++ mantidimaging/gui/windows/main/test/save_test.py
@@ -1,6 +1,9 @@
 import unittest
+import uuid
 
-from mantidimaging.gui.windows.main.save_dialog import sort_by_tomo_and_recon
+from mantidimaging.gui.windows.main.model import StackId
+from mantidimaging.gui.windows.main.save_dialog import sort_by_tomo_and_recon, MWSaveDialog
+from mantidimaging.test_helpers.start_qapplication import start_qapplication
 
 
 class SaveDialogTest(unittest.TestCase):
@@ -13,3 +16,21 @@ class SaveDialogTest(unittest.TestCase):
 
         self.assertEqual("Recon", new_names[0])
         self.assertEqual("Tomo", new_names[1])
+
+
+@start_qapplication
+class SaveDialogQtTest(unittest.TestCase):
+    def test_init(self):
+        stack_list = [
+            StackId(uuid.uuid4(), "Stack 1"),
+            StackId(uuid.uuid4(), "Stack 2"),
+            StackId(uuid.uuid4(), "Stack 3"),
+            StackId(uuid.uuid4(), "Stack Tomo"),
+            StackId(uuid.uuid4(), "Stack Recon"),
+        ]
+        mwsd = MWSaveDialog(None, stack_list)
+
+        # the Recon stack is top choice
+        self.assertEqual(mwsd.stack_uuids[0], stack_list[4].id)
+        # the Tomo stack is 2nd choice
+        self.assertEqual(mwsd.stack_uuids[1], stack_list[3].id)
