e41c4f223f5f29a508f4a3fdc77588c9033a40fc
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-11-18 11:32:00 +0000

    Version check now takes into account package label

--- mantidimaging/core/utility/version_check.py
+++ mantidimaging/core/utility/version_check.py
@@ -5,47 +5,64 @@ import json
 import os
 import subprocess
 from logging import getLogger
-from typing import Tuple, Callable
+from typing import Callable
+from collections import namedtuple
 
 import requests
 
 LOG = getLogger(__name__)
+ParsedVersion = namedtuple('ParsedVersion', ['version', 'commits'])
 
 
 def find_if_latest_version(action: Callable[[str], None]):
     LOG.info("Finding and comparing mantidimaging versions")
     local_package = subprocess.check_output("conda list mantidimaging | grep '^[^#]' | awk 'END{print $2}'",
-                                                   shell=True,
-                                                   env=os.environ).decode("utf-8").strip()
+                                            shell=True,
+                                            env=os.environ).decode("utf-8").strip()
+
     if local_package == "":
         LOG.info("Running a development build without a local Mantid Imaging package installation.")
         # no local installation, no point sending out API requests
         return
+
+    local_version, local_label = local_package.split()
+    # makes sure this is no longer used by accident
+    del local_package
+
     try:
         response = requests.get("https://api.anaconda.org/package/mantid/mantidimaging")
-        remote_package = json.loads(response.content)["latest_version"]
+        remote_main_version = json.loads(response.content)["latest_version"]
+        remote_unstable_version = json.loads(
+            response.content)["versions"][-1] if len(json.loads(response.content)["versions"]) > 0 else ''
     except Exception:
-        # whatever goes wrong, in the end we don't have the version
-        remote_package = ''
-    if remote_package == "":
+        # if anything goes wrong, in the end we don't have the version
         LOG.info("Could not connect Anaconda remote to get the latest version")
+        return
+
+    parsed_local_version = _parse_version(local_version)
+    del local_version
+
+    if "unstable" in local_label:
+        return _do_version_check(parsed_local_version, _parse_version(remote_unstable_version), action)
+    elif "main" in local_label:
+        return _do_version_check(parsed_local_version, _parse_version(remote_main_version), action)
     else:
-        local_version, local_commits_since_last = _parse_version(local_package)
-        remote_version, remote_commits_since_last = _parse_version(remote_package)
-
-        if local_version < remote_version or local_commits_since_last < remote_commits_since_last:
-            msg = f"Not running the latest Mantid Imaging. Found {local_package}, " \
-                  f"latest: {remote_package}. Please check the terminal for an update command!"
-            LOG.info(msg)
-            LOG.info(
-                "To update your environment please copy and run the following command:\n\n"
-                "source /opt/miniconda/bin/activate /opt/miniconda && "
-                "source <(curl -s https://raw.githubusercontent.com/mantidproject/mantidimaging/master/install.sh)")
-            action(msg)
-            return
-        LOG.info("Running the latest Mantid Imaging")
+        raise RuntimeError(f"Unknown package label found: {local_label}")
 
 
-def _parse_version(package_version_string: str) -> Tuple[Tuple[int, ...], int]:
+def _parse_version(package_version_string: str) -> ParsedVersion:
     local_version, local_commits_since_last = package_version_string.split("_")
-    return tuple(map(int, local_version.split("."))), int(local_commits_since_last)
+    return ParsedVersion(tuple(map(int, local_version.split("."))), int(local_commits_since_last))
+
+
+def _do_version_check(local: ParsedVersion, remote: ParsedVersion, action: Callable[[str], None]):
+    if local.version < remote.version or local.commits < remote.commits:
+        msg = f"Not running the latest Mantid Imaging. Found {local}, " \
+                f"latest: {remote}. Please check the terminal for an update command!"
+        LOG.info(msg)
+        LOG.info("To update your environment please copy and run the following command:\n\n"
+                 "source /opt/miniconda/bin/activate /opt/miniconda && "
+                 "source <(curl -s https://raw.githubusercontent.com/mantidproject/mantidimaging/master/install.sh)")
+        action(msg)
+        return
+    LOG.info("Running the latest Mantid Imaging")
