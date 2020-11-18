84bf0150500ea2f76705d713a383fd1e55baf23c
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-11-18 11:48:39 +0000

    Fix command for unstable build

--- mantidimaging/core/utility/version_check.py
+++ mantidimaging/core/utility/version_check.py
@@ -16,7 +16,7 @@ ParsedVersion = namedtuple('ParsedVersion', ['version', 'commits'])
 
 def find_if_latest_version(action: Callable[[str], None]):
     LOG.info("Finding and comparing mantidimaging versions")
-    local_package = subprocess.check_output("conda list mantidimaging | grep '^[^#]' | awk 'END{print $2}'",
+    local_package = subprocess.check_output("conda list mantidimaging | grep '^[^#]' | awk 'END{print $2,$4}'",
                                             shell=True,
                                             env=os.environ).decode("utf-8").strip()
 
@@ -43,9 +43,9 @@ def find_if_latest_version(action: Callable[[str], None]):
     del local_version
 
     if "unstable" in local_label:
-        return _do_version_check(parsed_local_version, _parse_version(remote_unstable_version), action)
+        return _do_version_check(parsed_local_version, _parse_version(remote_unstable_version), unstable=True, action)
     elif "main" in local_label:
-        return _do_version_check(parsed_local_version, _parse_version(remote_main_version), action)
+        return _do_version_check(parsed_local_version, _parse_version(remote_main_version), unstable=False, action)
     else:
         raise RuntimeError(f"Unknown package label found: {local_label}")
 
@@ -55,14 +55,20 @@ def _parse_version(package_version_string: str) -> ParsedVersion:
     return ParsedVersion(tuple(map(int, local_version.split("."))), int(local_commits_since_last))
 
 
-def _do_version_check(local: ParsedVersion, remote: ParsedVersion, action: Callable[[str], None]):
+def _do_version_check(local: ParsedVersion, remote: ParsedVersion, unstable: bool, action: Callable[[str], None]):
     if local.version < remote.version or local.commits < remote.commits:
         msg = f"Not running the latest Mantid Imaging. Found {local}, " \
                 f"latest: {remote}. Please check the terminal for an update command!"
         LOG.info(msg)
+
+        # for unstable packages these run variables are prepended to the command
+        # for main packages nothing is prepended, and the script's defaults are used
+        command_prefix = "ENVIRONMENT_NAME=mantidimaging_unstable REPO_LABEL=unstable " if unstable else ''
+
         LOG.info("To update your environment please copy and run the following command:\n\n"
                  "source /opt/miniconda/bin/activate /opt/miniconda && "
-                 "source <(curl -s https://raw.githubusercontent.com/mantidproject/mantidimaging/master/install.sh)")
+                 f"{command_prefix}source "
+                 "<(curl -s https://raw.githubusercontent.com/mantidproject/mantidimaging/master/install.sh)")
         action(msg)
         return
     LOG.info("Running the latest Mantid Imaging")
