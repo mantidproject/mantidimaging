705ad8a316bf790e945a58e5638b2c0815382d6e
Samuel Jones <samuel.jones@stfc.ac.uk>
2020-11-26 16:50:28 +0000

    Show the name of the stack being compared as a title on compare window

--- mantidimaging/gui/windows/stack_choice/tests/test_compare_presenter.py
+++ mantidimaging/gui/windows/stack_choice/tests/test_compare_presenter.py
@@ -3,6 +3,7 @@
 
 import unittest
 from unittest import mock
+from unittest.mock import call
 
 import mantidimaging.test_helpers.unit_test_helper as th
 from mantidimaging.gui.windows.stack_choice.compare_presenter import StackComparePresenter
@@ -31,3 +32,16 @@ class StackChoicePresenterTest(unittest.TestCase):
         self.presenter.show()
 
         view.return_value.show.assert_called_once()
+
+    @mock.patch("mantidimaging.gui.windows.stack_choice.compare_presenter.StackChoiceView")
+    def test_titles_set(self, view):
+        stack_name = "stack_name"
+        custom_parent = mock.MagicMock()
+        custom_parent.find_images_stack_title.return_value = stack_name
+
+        self.presenter = StackComparePresenter(stack_one=self.stack_one, stack_two=self.stack_two, parent=custom_parent)
+
+        custom_parent.find_images_stack_title.assert_has_calls([call(self.stack_one), call(self.stack_two)])
+        self.assertEqual(2, custom_parent.find_images_stack_title.call_count)
+        view.return_value.originalStackLabel.setText.assert_called_once_with(stack_name)
+        view.return_value.newStackLabel.setText.assert_called_once_with(stack_name)
