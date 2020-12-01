ee3e1d40e88e926fab5fcdd53d0eb59715fc70c8
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-12-01 13:07:44 +0000

    Setting spin box in initialiser

--- mantidimaging/gui/dialogs/cor_inspection/view.py
+++ mantidimaging/gui/dialogs/cor_inspection/view.py
@@ -27,6 +27,13 @@ class CORInspectionDialogView(BaseDialogView):
     def __init__(self, parent, images: Images, slice_index: int, initial_cor: ScalarCoR,
                  recon_params: ReconstructionParameters, iters_mode: bool):
         super().__init__(parent, 'gui/ui/cor_inspection_dialog.ui')
+        self.iters_mode = iters_mode
+
+        if self.iters_mode:
+            self.spin_box = self.stepIterations
+        else:
+            self.spin_box = self.stepCOR
+
         self.presenter = CORInspectionDialogPresenter(self, images, slice_index, initial_cor, recon_params, iters_mode)
 
         self.stepCOR.editingFinished.connect(lambda: self.presenter.do_update_ui_parameters())
@@ -47,7 +54,6 @@ class CORInspectionDialogView(BaseDialogView):
         self.presenter.do_refresh()
         self.image_canvas.current_hist.imageChanged(autoLevel=True, autoRange=True)
 
-        self.iters_mode = iters_mode
 
     def set_image(self, image_type: ImageType, recon_data: np.ndarray, title: str):
         self.image_canvas.set_image(image_type, recon_data, title)
@@ -60,17 +66,11 @@ class CORInspectionDialogView(BaseDialogView):
 
     @property
     def step_size(self):
-        if self.iters_mode:
-            return self.stepIterations.value()
-        return self.stepCOR.value()
+        return self.spin_box.value()
 
     @step_size.setter
     def step_size(self, value):
-        if self.iters_mode:
-            spin_box = self.stepIterations
-        else:
-            spin_box = self.stepCOR
-        with BlockQtSignals([spin_box]):
+        with BlockQtSignals([self.spin_box]):
             self.spin_box.setValue(value)
 
     @property
