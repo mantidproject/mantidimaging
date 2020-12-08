f892bb9a0eaca5b77e6b703eafa60e4928e5bdf7
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-12-08 16:06:56 +0000

    flake8

--- mantidimaging/core/utility/cuda_check.py
+++ mantidimaging/core/utility/cuda_check.py
@@ -8,4 +8,4 @@ def check_cuda():
     """
     Checks if nvidia-smi is on the system + working, and that the libcuda file can be located.
     """
-    return "Driver Version" in os.popen("nvidia-smi").read() and os.popen("locate libcuda.so").read() is not ""
+    return "Driver Version" in os.popen("nvidia-smi").read() and os.popen("locate libcuda.so").read() != ""
