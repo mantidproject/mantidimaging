f2600858155196f28eec92e4dbe2aec18a2c1140
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-12-09 14:00:59 +0000

    Not found message

--- mantidimaging/core/utility/cuda_check.py
+++ mantidimaging/core/utility/cuda_check.py
@@ -4,8 +4,16 @@
 import os
 
 
-def is_cuda_present():
+def cuda_is_present() -> bool:
     """
     Checks if nvidia-smi is on the system + working, and that the libcuda file can be located.
     """
     return "Driver Version" in os.popen("nvidia-smi").read() and os.popen("locate libcuda.so").read() != ""
+
+
+def not_found_message() -> str:
+    """
+    Generates a message that can be displayed if a working CUDA installation isn't found.
+    """
+    return "Working CUDA installation not found. Will only use gridrec algorithm for reconstruction. Check " \
+           "troubleshooting page for assistance. "
