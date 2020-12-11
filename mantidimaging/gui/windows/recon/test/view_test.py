fa10c85004a0187124a2b1817d8d0c3810559e2d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-12-11 12:28:30 +0000

    Test for clear cor table and set results

--- mantidimaging/gui/windows/recon/test/view_test.py
+++ mantidimaging/gui/windows/recon/test/view_test.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest import mock
 
+from mantidimaging.core.utility.data_containers import ScalarCoR, Degrees, Slope
 from mantidimaging.gui.windows.main import MainWindowView
 from mantidimaging.gui.windows.recon import ReconstructWindowView
 from mantidimaging.test_helpers import start_qapplication
@@ -35,3 +36,25 @@ class ReconstructWindowViewTest(unittest.TestCase):
     def test_remove_selected_cor(self):
         self.view.tableView = table_view_mock = mock.Mock()
         assert self.view.remove_selected_cor() == table_view_mock.removeSelectedRows.return_value
+
+    def test_clear_cor_table(self):
+        self.view.tableView = table_view_mock = mock.Mock()
+        assert self.view.clear_cor_table() == table_view_mock.model.return_value.removeAllRows.return_value
+
+    def test_cor_table_model(self):
+        pass
+
+    def test_set_results(self):
+        cor_val = 20
+        tilt_val = 30
+        slope_val = 40
+        cor = ScalarCoR(cor_val)
+        tilt = Degrees(tilt_val)
+        slope = Slope(slope_val)
+
+        self.view.image_view = image_view_mock = mock.Mock()
+        self.view.set_results(cor, tilt, slope)
+        assert self.view.rotation_centre == cor_val
+        assert self.view.tilt == tilt_val
+        assert self.view.slope == slope_val
+        image_view_mock.set_tilt.assert_called_once_with(tilt)
