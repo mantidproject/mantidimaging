810368b3b9ab0de2e060225b1c95cdad4193caba
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-12-15 11:07:18 +0000

    Replacing os.popen with subprocess

--- mantidimaging/core/utility/cuda_check.py
+++ mantidimaging/core/utility/cuda_check.py
@@ -1,22 +1,25 @@
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
+import subprocess
+from typing import Tuple, List
 
-import os
-from typing import Tuple
 
-
-def _read_from_terminal(command: str) -> str:
+def _read_from_terminal(command_args: List[str]) -> str:
     """
     Runs a terminal command and returns the result.
     """
-    return os.popen(command).read()
+    return subprocess.check_output(command_args).decode("ascii")
 
 
 def cuda_is_present() -> bool:
     """
     Checks if nvidia-smi is on the system + working, and that the libcuda file can be located.
     """
-    return "Driver Version" in _read_from_terminal("nvidia-smi") and _read_from_terminal("locate libcuda.so") != ""
+    try:
+        return "Driver Version" in _read_from_terminal(["nvidia-smi"
+                                                        ]) and _read_from_terminal(["locate", "libcuda.so"]) != ""
+    except (subprocess.CalledProcessError, PermissionError):
+        return False
 
 
 def not_found_message() -> Tuple[str, str]:
--- mantidimaging/core/utility/test/cuda_check_test.py
+++ mantidimaging/core/utility/test/cuda_check_test.py
@@ -1,22 +1,30 @@
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from mantidimaging.core.utility import cuda_check
 
 
 class TestCudaCheck(unittest.TestCase):
-    def test_cuda_is_present(self):
-        with patch("mantidimaging.core.utility.cuda_check.os") as os_mock:
-            os_mock.popen.return_value.read = Mock(side_effect=["Driver Version", "/path/to/libcuda.so"])
-            assert cuda_check.cuda_is_present()
+    @patch("mantidimaging.core.utility.cuda_check.subprocess.check_output")
+    def test_cuda_is_present_returns_true(self, check_output_mock):
+        check_output_mock.side_effect = [b"Driver Version", b"/path/to/libcuda.so\n"]
+        assert cuda_check.cuda_is_present()
 
-            os_mock.popen.return_value.read = Mock(side_effect=["command not found: nvidia-smi", "/path/to/libcuda.so"])
-            assert not cuda_check.cuda_is_present()
+    @patch("mantidimaging.core.utility.cuda_check.subprocess.check_output")
+    def test_cuda_is_present_returns_false(self, check_output_mock):
+        check_output_mock.side_effect = [
+            b"NVIDIA-SMI has failed because it couldn't communicate with the NVIDIA "
+            b"driver. Make sure that the latest NVIDIA driver is installed and "
+            b"running.\n", b"/path/to/libcuda.so\n"
+        ]
+        assert not cuda_check.cuda_is_present()
 
-            os_mock.popen.return_value.read = Mock(side_effect=["Driver Version", ""])
-            assert not cuda_check.cuda_is_present()
+    @patch("mantidimaging.core.utility.cuda_check.subprocess.check_output")
+    def test_cuda_is_present_returns_false_when_subprocess_raises_exception(self, check_output_mock):
+        check_output_mock.side_effect = PermissionError
+        assert not cuda_check.cuda_is_present()
 
     def test_not_found_message(self):
         short_msg, long_msg = cuda_check.not_found_message()
