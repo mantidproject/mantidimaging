f3997c670c5d9bd2023c0ef8b23d12f6e256fa85
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-12-15 11:12:13 +0000

    Extra exception check

--- mantidimaging/core/utility/test/cuda_check_test.py
+++ mantidimaging/core/utility/test/cuda_check_test.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
+import subprocess
 import unittest
 from unittest.mock import patch
 
@@ -24,6 +25,9 @@ class TestCudaCheck(unittest.TestCase):
     @patch("mantidimaging.core.utility.cuda_check.subprocess.check_output")
     def test_cuda_is_present_returns_false_when_subprocess_raises_exception(self, check_output_mock):
         check_output_mock.side_effect = PermissionError
+        assert not cuda_check.cuda_is_present()
+
+        check_output_mock.side_effect = subprocess.CalledProcessError(returncode=2, cmd=["bad"])
         assert not cuda_check.cuda_is_present()
 
     def test_not_found_message(self):
