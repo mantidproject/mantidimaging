92a6be2d83caa6112fe7bdae79ee1289e6a37974
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-12-16 14:16:37 +0000

    Removes outdated vscode debugging fix

--- mantidimaging/core/parallel/__init__.py
+++ mantidimaging/core/parallel/__init__.py
@@ -1,11 +1,2 @@
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-import os
-
-# if debugging using PTVSD (used in VSCode), it is needed
-# to change the multiprocess start method.
-# See https://github.com/Microsoft/ptvsd/issues/1283
-if os.environ.get("PTVSD_DEBUG", False):
-    import multiprocessing
-
-    multiprocessing.set_start_method('spawn', True)
