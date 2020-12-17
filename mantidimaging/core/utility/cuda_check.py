6ad46e319c0344be6178623f3aad84e9ddf0e601
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-12-17 11:07:18 +0000

    Not passing list

--- mantidimaging/core/utility/cuda_check.py
+++ mantidimaging/core/utility/cuda_check.py
@@ -2,19 +2,19 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 import subprocess
 from logging import getLogger
-from typing import Tuple, List
+from typing import Tuple
 
 LOG = getLogger(__name__)
 
 
-def _read_from_terminal(command_args: List[str]) -> str:
+def _read_from_terminal(command: str) -> str:
     """
     Runs a terminal command and returns the result.
     """
     try:
-        return subprocess.check_output(command_args, shell=True).decode("ascii")
+        return subprocess.check_output(command, shell=True).decode("ascii")
     except subprocess.CalledProcessError:
-        LOG.error(f"{command_args[0]} doesn't appear to be installed on your system.")
+        LOG.error(f"{command.split()[0]} doesn't appear to be installed on your system.")
         return ""
 
 
@@ -24,22 +24,22 @@ def cuda_is_present() -> bool:
     """
     nvidia_smi_working = libcuda_files_found = True
     try:
-        nvidia_smi_output = _read_from_terminal(["nvidia-smi"])
+        nvidia_smi_output = _read_from_terminal("nvidia-smi")
         if "Driver Version" not in nvidia_smi_output:
             nvidia_smi_working = False
             LOG.error(nvidia_smi_output)
 
     except (PermissionError, FileNotFoundError) as e:
-        LOG.info(f"Error when attempting to run nvidia-smi: {e}")
+        LOG.error(f"Error when attempting to run nvidia-smi: {e}")
         nvidia_smi_working = False
 
     try:
-        if _read_from_terminal(["locate", "--regex", "'^/usr/(lib|lib64)/(.*?)/libcuda.so'"]) == "":
+        if _read_from_terminal("locate --regex '^/usr/(lib|lib64)/(.*?)/libcuda.so'") == "":
             libcuda_files_found = False
             LOG.error("Search for libcuda files returned no results.")
 
     except (PermissionError, FileNotFoundError) as e:
-        LOG.info(f"Error when attempting to run locate: {e}")
+        LOG.error(f"Error when attempting to run locate: {e}")
         libcuda_files_found = False
 
     return nvidia_smi_working and libcuda_files_found
