6c75a09ffc8747e49b92ab2159512229d1e9357e
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-01-05 17:00:02 +0000

    Initial Testing implementation

--- mantidimaging/core/io/loader/test/loader_test.py
+++ mantidimaging/core/io/loader/test/loader_test.py
@@ -1,14 +1,15 @@
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-
+import os
 import unittest
 from unittest import mock
 
 from mantidimaging.core.io.loader import load_stack
 from mantidimaging.core.io import loader
+from test_helpers import FileOutputtingTestCase
 
 
-class LoaderTest(unittest.TestCase):
+class LoaderTest(FileOutputtingTestCase):
     def test_raise_on_invalid_format(self):
         self.assertRaises(ValueError, loader.load, "/some/path", file_names=["/somefile"], in_format='txt')
 
@@ -26,3 +27,42 @@ class LoaderTest(unittest.TestCase):
         get_file_names_mock.assert_called_once_with(path="/path/to/file/that/is",
                                                     img_format="tif",
                                                     prefix="/path/to/file/that/is/fake.ti")
+
+    def _create_test_sample(self):
+        # Logs
+        with open(os.path.join(self.output_directory, "Tomo_log.txt"), "w") as f:
+            f.write("log")
+        with open(os.path.join(self.output_directory, "Flat_After_log.txt"), "w") as f:
+            f.write("log")
+        with open(os.path.join(self.output_directory, "Flat_Before_log.txt"), "w") as f:
+            f.write("log")
+        with open(os.path.join(self.output_directory, "180deg_log.txt"), "w") as f:
+            f.write("log")
+
+        # Flat
+        for ii in range(0, 10):
+            with open(os.path.join(self.output_directory, "Flat_Before", f"Flat_Before_00{ii}.tif"), "wb") as f:
+                f.write(b"\0")
+
+            with open(os.path.join(self.output_directory, "Flat_After", f"Flat_After_00{ii}.tif"), "wb") as f:
+                f.write(b"\0")
+
+        # Dark
+        for ii in range(0, 10):
+            with open(os.path.join(self.output_directory, "Dark_Before", f"Dark_Before_00{ii}.tif"), "wb") as f:
+                f.write(b"\0")
+
+            with open(os.path.join(self.output_directory, "Dark_After", f"Dark_After_00{ii}.tif"), "wb") as f:
+                f.write(b"\0")
+
+        # Tomo
+        for ii in range (0, 200):
+            with open(os.path.join(self.output_directory, "Tomo", f"Tomo_00{ii}.tif"), "wb") as f:
+                f.write(b"\0")
+
+        # 180 degree projection
+        with open(os.path.join(self.output_directory, "180deg", "180deg_000.tif"), "wb") as f:
+            f.write(b"\0")
+
+    def test_create_loading_parameters_for_file_path(self):
+        self._create_test_sample()
