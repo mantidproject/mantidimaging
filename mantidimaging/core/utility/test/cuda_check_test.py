6101f57ffeb322324914064abbbf6603f20f10f4
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-01-07 12:43:24 +0000

    Renaming function

--- mantidimaging/core/utility/test/cuda_check_test.py
+++ mantidimaging/core/utility/test/cuda_check_test.py
@@ -11,7 +11,7 @@ class TestCudaCheck(unittest.TestCase):
     @patch("mantidimaging.core.utility.cuda_check.subprocess.check_output")
     def test_cuda_is_present_returns_true(self, check_output_mock):
         check_output_mock.side_effect = [b"Driver Version", b"/usr/lib/path/to/libcuda.so\n"]
-        assert cuda_check.cuda_is_present()
+        assert cuda_check._cuda_is_present()
 
     @patch("mantidimaging.core.utility.cuda_check.subprocess.check_output")
     def test_cuda_is_present_returns_false(self, check_output_mock):
@@ -20,24 +20,24 @@ class TestCudaCheck(unittest.TestCase):
         check_output_mock.side_effect = [str.encode(nvidia_error), b"/path/to/libcuda.so\n"]
 
         with self.assertLogs(cuda_check.__name__, level='ERROR') as cuda_check_log:
-            assert not cuda_check.cuda_is_present()
+            assert not cuda_check._cuda_is_present()
         self.assertIn(nvidia_error, cuda_check_log.output[0])
 
     @patch("mantidimaging.core.utility.cuda_check.subprocess.check_output")
     def test_failed_libcuda_search_is_logged(self, check_output_mock):
         check_output_mock.side_effect = [b"Driver Version", b""]
         with self.assertLogs(cuda_check.__name__, level='ERROR') as cuda_check_log:
-            assert not cuda_check.cuda_is_present()
+            assert not cuda_check._cuda_is_present()
         self.assertIn("Search for libcuda files returned no results.", cuda_check_log.output[0])
 
     @patch("mantidimaging.core.utility.cuda_check.subprocess.check_output")
     def test_cuda_is_present_returns_false_when_subprocess_raises_exception(self, check_output_mock):
         with self.assertLogs(cuda_check.__name__, level='ERROR') as cuda_check_log:
             check_output_mock.side_effect = PermissionError
-            assert not cuda_check.cuda_is_present()
+            assert not cuda_check._cuda_is_present()
 
             check_output_mock.side_effect = FileNotFoundError
-            assert not cuda_check.cuda_is_present()
+            assert not cuda_check._cuda_is_present()
 
         nvidia_exception_msg = f"{EXCEPTION_MSG} {NVIDIA_SMI}"
         locate_exception_msg = f"{EXCEPTION_MSG} {LOCATE}"
