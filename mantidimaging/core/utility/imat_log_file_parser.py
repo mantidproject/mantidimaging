ba5bffc68abad02d86afc196be32e9fc89dea18f
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-01-12 10:13:50 +0000

    Refactored and can still read old log files

--- mantidimaging/core/utility/imat_log_file_parser.py
+++ mantidimaging/core/utility/imat_log_file_parser.py
@@ -9,10 +9,6 @@ import numpy
 
 from mantidimaging.core.utility.data_containers import ProjectionAngles, Counts
 
-EXPECTED_HEADER_FOR_IMAT_LOG_FILE = [
-    'TIME STAMP  IMAGE TYPE', 'IMAGE COUNTER', 'COUNTS BM3 before image', 'COUNTS BM3 after image'
-]
-
 
 class IMATLogColumn(Enum):
     TIMESTAMP = auto()
@@ -23,39 +19,80 @@ class IMATLogColumn(Enum):
     COUNTS_AFTER = auto()
 
 
-class IMATLogFile:
-    def __init__(self, data: List[List[str]], source_file: str):
-        self._source_file = source_file
-        self._data: Dict[IMATLogColumn, List] = {
+class TextLogParser:
+    EXPECTED_HEADER_FOR_IMAT_TEXT_LOG_FILE = \
+            ' TIME STAMP  IMAGE TYPE   IMAGE COUNTER   COUNTS BM3 before image   COUNTS BM3 after image\n'
+
+    def __init__(self, data: List[str]) -> None:
+        self.data = [line.strip().split("   ") for line in data]
+
+    def parse(self) -> Dict[IMATLogColumn, List]:
+        parsed_log: Dict[IMATLogColumn, List] = {
             IMATLogColumn.TIMESTAMP: [],
             IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER: [],
             IMATLogColumn.COUNTS_BEFORE: [],
             IMATLogColumn.COUNTS_AFTER: []
         }
 
-        if EXPECTED_HEADER_FOR_IMAT_LOG_FILE != data[0]:
-            raise RuntimeError(
-                "The Log file found for this dataset does not seem to have the correct header for an IMAT log file.\n"
-                "The header is expected to contain the names of the columns:\n"
-                f"{str(EXPECTED_HEADER_FOR_IMAT_LOG_FILE)}")
-
         # ignores the headers (index 0) as they're not the same as the data anyway
         # and index 1 is an empty line
-        for line in data[2:]:
-            self._data[IMATLogColumn.TIMESTAMP].append(line[0])
-            self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER].append(line[1])
-            self._data[IMATLogColumn.COUNTS_BEFORE].append(line[2])
-            self._data[IMATLogColumn.COUNTS_AFTER].append(line[3])
+        for line in self.data[2:]:
+            parsed_log[IMATLogColumn.TIMESTAMP].append(line[0])
+            parsed_log[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER].append(line[1])
+            parsed_log[IMATLogColumn.COUNTS_BEFORE].append(line[2])
+            parsed_log[IMATLogColumn.COUNTS_AFTER].append(line[3])
+
+        return parsed_log
+
+    @staticmethod
+    def validate(file_contents) -> bool:
+
+        if TextLogParser.EXPECTED_HEADER_FOR_IMAT_TEXT_LOG_FILE != file_contents[0]:
+            return False
+        return True
+
+
+class CSVLogParser:
+    EXPECTED_HEADER_FOR_IMAT_CSV_LOG_FILE = \
+        "TIME STAMP,IMAGE TYPE,IMAGE COUNTER,COUNTS BM3 before image,COUNTS BM3 after image"
+
+    def __init__(self, data: List[str]) -> None:
+        pass
+
+    @staticmethod
+    def validate(file_contents) -> bool:
+        if CSVLogParser.EXPECTED_HEADER_FOR_IMAT_CSV_LOG_FILE != file_contents[0]:
+            return False
+        return True
+
+
+class IMATLogFile:
+    def __init__(self, data: List[str], source_file: str):
+        self._source_file = source_file
+
+        self.parser = self.find_parser(data)
+        self._data = self.parser.parse()
+
+    @staticmethod
+    def find_parser(data: List[str]):
+        if TextLogParser.validate(data):
+            return TextLogParser(data)
+        elif CSVLogParser.validate(data):
+            return CSVLogParser(data)
+        else:
+            raise RuntimeError("The format of the log file is not recognised.")
 
     @property
     def source_file(self) -> str:
         return self._source_file
 
     def projection_numbers(self):
-        proj_nums = numpy.zeros(len(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]), dtype=numpy.uint32)
+        proj_nums = numpy.zeros(len(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]),
+                                dtype=numpy.uint32)
         for i, angle_str in enumerate(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]):
             if "angle:" not in angle_str:
-                raise ValueError("Projection angles loaded from logfile do not have the correct formatting!")
+                raise ValueError(
+                    "Projection angles loaded from logfile do not have the correct formatting!")
             proj_nums[i] = int(angle_str[angle_str.find(": ") + 1:angle_str.find("a")])
 
         return proj_nums
@@ -64,16 +101,17 @@ class IMATLogFile:
         angles = numpy.zeros(len(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]))
         for i, angle_str in enumerate(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]):
             if "angle:" not in angle_str:
-                raise ValueError("Projection angles loaded from logfile do not have the correct formatting!")
+                raise ValueError(
+                    "Projection angles loaded from logfile do not have the correct formatting!")
             angles[i] = float(angle_str[angle_str.rfind(": ") + 1:])
 
         return ProjectionAngles(numpy.deg2rad(angles))
 
     def counts(self) -> Counts:
         counts = numpy.zeros(len(self._data[IMATLogColumn.COUNTS_BEFORE]))
-        for i, [before,
-                after] in enumerate(zip(self._data[IMATLogColumn.COUNTS_BEFORE],
-                                        self._data[IMATLogColumn.COUNTS_AFTER])):
+        for i, [before, after] in enumerate(
+                zip(self._data[IMATLogColumn.COUNTS_BEFORE],
+                    self._data[IMATLogColumn.COUNTS_AFTER])):
             # clips the string before the count number
             before = before[before.rfind(":") + 1:]
             after = after[after.rfind(":") + 1:]
