be4ee690a14caec8cf44090650394fbe32df804e
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-01-12 15:48:06 +0000

    Refactors log parser to move parsing into child classes
    Tests didn't need updating as this is an internal change

--- mantidimaging/core/utility/imat_log_file_parser.py
+++ mantidimaging/core/utility/imat_log_file_parser.py
@@ -1,13 +1,22 @@
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 
+import csv
 from enum import Enum, auto
 from itertools import zip_longest
-from typing import List, Dict
+from typing import Dict, List
 
 import numpy
 
-from mantidimaging.core.utility.data_containers import ProjectionAngles, Counts
+from mantidimaging.core.utility.data_containers import Counts, ProjectionAngles
+
+
+def _get_projection_number(s: str) -> int:
+    return int(s[s.find(": ") + 1:s.find("a")])
+
+
+def _get_angle(s: str) -> float:
+    return float(s[s.rfind(": ") + 1:])
 
 
 class IMATLogColumn(Enum):
@@ -15,6 +24,8 @@ class IMATLogColumn(Enum):
     # currently these 2 are the same column because
     # the log file formatting is inconsistent
     IMAGE_TYPE_IMAGE_COUNTER = auto()
+    PROJECTION_NUMBER = auto()
+    PROJECTION_ANGLE = auto()
     COUNTS_BEFORE = auto()
     COUNTS_AFTER = auto()
 
@@ -29,16 +40,17 @@ class TextLogParser:
     def parse(self) -> Dict[IMATLogColumn, List]:
         parsed_log: Dict[IMATLogColumn, List] = {
             IMATLogColumn.TIMESTAMP: [],
-            IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER: [],
+            IMATLogColumn.PROJECTION_NUMBER: [],
+            IMATLogColumn.PROJECTION_ANGLE: [],
             IMATLogColumn.COUNTS_BEFORE: [],
             IMATLogColumn.COUNTS_AFTER: []
         }
-
         # ignores the headers (index 0) as they're not the same as the data anyway
         # and index 1 is an empty line
         for line in self.data[2:]:
             parsed_log[IMATLogColumn.TIMESTAMP].append(line[0])
-            parsed_log[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER].append(line[1])
+            parsed_log[IMATLogColumn.PROJECTION_NUMBER].append(_get_projection_number(line[1]))
+            parsed_log[IMATLogColumn.PROJECTION_ANGLE].append(_get_angle(line[1]))
             parsed_log[IMATLogColumn.COUNTS_BEFORE].append(line[2])
             parsed_log[IMATLogColumn.COUNTS_AFTER].append(line[3])
 
@@ -54,10 +66,38 @@ class TextLogParser:
 
 class CSVLogParser:
     EXPECTED_HEADER_FOR_IMAT_CSV_LOG_FILE = \
-        "TIME STAMP,IMAGE TYPE,IMAGE COUNTER,COUNTS BM3 before image,COUNTS BM3 after image"
+        "TIME STAMP,IMAGE TYPE,IMAGE COUNTER,COUNTS BM3 before image,COUNTS BM3 after image\n"
 
     def __init__(self, data: List[str]) -> None:
-        pass
+        self.data = data
+
+    def parse(self) -> Dict[IMATLogColumn, List]:
+        parsed_log: Dict[IMATLogColumn, List] = {
+            IMATLogColumn.TIMESTAMP: [],
+            IMATLogColumn.PROJECTION_NUMBER: [],
+            IMATLogColumn.PROJECTION_ANGLE: [],
+            IMATLogColumn.COUNTS_BEFORE: [],
+            IMATLogColumn.COUNTS_AFTER: []
+        }
+
+        reader = csv.reader(self.data)
+
+        # skip headings
+        next(reader)
+
+        for row in reader:
+            parsed_log[IMATLogColumn.TIMESTAMP].append(row[0])
+            parsed_log[IMATLogColumn.PROJECTION_NUMBER].append(row[2])
+            angle_raw = row[3]
+            parsed_log[IMATLogColumn.PROJECTION_ANGLE].append(_get_angle(angle_raw))
+
+            counts_before_raw = row[4]
+            parsed_log[IMATLogColumn.COUNTS_BEFORE].append(_get_angle(counts_before_raw))
+
+            counts_after_raw = row[5]
+            parsed_log[IMATLogColumn.COUNTS_AFTER].append(_get_angle(counts_after_raw))
+
+        return parsed_log
 
     @staticmethod
     def validate(file_contents) -> bool:
@@ -87,24 +127,14 @@ class IMATLogFile:
         return self._source_file
 
     def projection_numbers(self):
-        proj_nums = numpy.zeros(len(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]),
+        proj_nums = numpy.zeros(len(self._data[IMATLogColumn.PROJECTION_NUMBER]),
                                 dtype=numpy.uint32)
-        for i, angle_str in enumerate(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]):
-            if "angle:" not in angle_str:
-                raise ValueError(
-                    "Projection angles loaded from logfile do not have the correct formatting!")
-            proj_nums[i] = int(angle_str[angle_str.find(": ") + 1:angle_str.find("a")])
-
+        proj_nums[:] = self._data[IMATLogColumn.PROJECTION_NUMBER]
         return proj_nums
 
     def projection_angles(self) -> ProjectionAngles:
-        angles = numpy.zeros(len(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]))
-        for i, angle_str in enumerate(self._data[IMATLogColumn.IMAGE_TYPE_IMAGE_COUNTER]):
-            if "angle:" not in angle_str:
-                raise ValueError(
-                    "Projection angles loaded from logfile do not have the correct formatting!")
-            angles[i] = float(angle_str[angle_str.rfind(": ") + 1:])
-
+        angles = numpy.zeros(len(self._data[IMATLogColumn.PROJECTION_ANGLE]))
+        angles[:] = self._data[IMATLogColumn.PROJECTION_ANGLE]
         return ProjectionAngles(numpy.deg2rad(angles))
 
     def counts(self) -> Counts:
