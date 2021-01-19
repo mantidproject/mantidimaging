e6c4222bfaa6bab1609f851dffef26b8a39045bd
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-01-19 11:21:06 +0000

    PaletteChangerPresenter implementation

---
+++ mantidimaging/gui/widgets/palette_changer/presenter.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+from typing import List
+
+from pyqtgraph.graphicsItems import HistogramLUTItem
+
+from mantidimaging.gui.mvp_base import BasePresenter
+
+
+class PaletteChangerPresenter(BasePresenter):
+    def __init__(self, view, hists):
+        super(PaletteChangerPresenter, self).__init__(view)
+        self.hists = hists
+
+    def notify(self, signal):
+        pass
+
+    def change_colour_palette(self):
+        # todo: log here
+        preset = self.view.colour_map
+        for hist in self.hists:
+            hist.gradient.loadPreset(preset)
