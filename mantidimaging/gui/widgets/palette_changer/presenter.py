5572d084dc18076fe314c8f15ad0a2ffadd2f65b
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-01-22 17:25:32 +0000

    Working jenks with array subset

--- mantidimaging/gui/widgets/palette_changer/presenter.py
+++ mantidimaging/gui/widgets/palette_changer/presenter.py
@@ -1,15 +1,19 @@
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
+from typing import List
+
+from pyqtgraph import HistogramLUTItem
 
 from mantidimaging.gui.mvp_base import BasePresenter
 from jenkspy import jenks_breaks
+import numpy as np
 
 
 class PaletteChangerPresenter(BasePresenter):
-    def __init__(self, view, hists, images):
+    def __init__(self, view, hists: List[HistogramLUTItem], projection_image: np.ndarray):
         super(PaletteChangerPresenter, self).__init__(view)
         self.hists = hists
-        self.images = images
+        self.projection_image = projection_image
 
     def notify(self, signal):
         pass
@@ -22,17 +26,11 @@ class PaletteChangerPresenter(BasePresenter):
             self._otsu_break()
 
     def _jenks_breaks(self):
-        image = self.images[-1]
         hist = self.hists[-1]
-        breaks = jenks_breaks(image.image[0], self.view.num_materials)
-        levels = hist.getLevels()
-        breaks[0] = levels[0]
-        breaks[-1] = levels[1]
-        norm = 1.0 / abs(levels[1] - levels[0])
-        breaks = [(b + levels[0]) * norm for b in breaks]
+        tick_points = self._generate_jenks_tick_points()
         old_ticks = list(hist.gradient.ticks.keys())
-        for tick_break in breaks:
-            t = hist.gradient.addTick(tick_break, finish=False)
+        for x in tick_points:
+            hist.gradient.addTick(x, finish=False)
         for t in old_ticks:
             hist.gradient.removeTick(t, finish=False)
         hist.gradient.showTicks()
@@ -45,3 +43,13 @@ class PaletteChangerPresenter(BasePresenter):
 
     def _otsu_break(self):
         pass
+
+    def _generate_jenks_tick_points(self):
+        breaks = jenks_breaks(np.random.choice(self.projection_image.flatten(), 15000), self.view.num_materials)
+        min_val = self.projection_image.min()
+        max_val = self.projection_image.max()
+        val_range = abs(max_val - min_val)
+        breaks[0], breaks[-1] = min_val, max_val
+        for i in range(len(breaks)):
+            breaks[i] = (breaks[i] - min_val) / val_range
+        return breaks
