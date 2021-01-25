043377c81edd350a91f4a721324c58a825c6b449
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-01-25 11:49:15 +0000

    Trying otsu

--- mantidimaging/gui/widgets/palette_changer/presenter.py
+++ mantidimaging/gui/widgets/palette_changer/presenter.py
@@ -3,6 +3,7 @@
 from typing import List
 
 from pyqtgraph import HistogramLUTItem
+from skimage import filters
 
 from mantidimaging.gui.mvp_base import BasePresenter
 from jenkspy import jenks_breaks
@@ -29,27 +30,42 @@ class PaletteChangerPresenter(BasePresenter):
         projection_hist = self.hists[-1]
         tick_points = self._generate_jenks_tick_points()
         old_ticks = list(projection_hist.gradient.ticks.keys())
+
+        # Insert new ticks
         for x in tick_points:
             projection_hist.gradient.addTick(x, finish=False)
+        # Remove default ticks
         for t in old_ticks:
             projection_hist.gradient.removeTick(t, finish=False)
+
+        # Inform ticks object to update
         projection_hist.gradient.showTicks()
         projection_hist.gradient.sigGradientChangeFinished.emit(projection_hist.gradient)
 
     def _change_colour_map(self):
+        """
+        Changes the colour map of the images.
+        """
         preset = self.view.colour_map
         for hist in self.hists:
             hist.gradient.loadPreset(preset)
 
     def _otsu_break(self):
-        pass
+        val = filters.threshold_otsu(np.random.choice(self.projection_image.flatten(), 15000), self.view.num_materials)
+        print(val)
 
     def _generate_jenks_tick_points(self):
+        """
+        Generates tick points using the Jenks Breaks algorithm.
+        """
+        # Perform Breaks on a random subset
         breaks = jenks_breaks(np.random.choice(self.projection_image.flatten(), 15000), self.view.num_materials)
         min_val = self.projection_image.min()
         max_val = self.projection_image.max()
         val_range = abs(max_val - min_val)
+        # Replace the first and last breaks, because the random may have missed them
         breaks[0], breaks[-1] = min_val, max_val
+        # Normalise so the values range from 0 to 1
         for i in range(len(breaks)):
             breaks[i] = (breaks[i] - min_val) / val_range
         return breaks
