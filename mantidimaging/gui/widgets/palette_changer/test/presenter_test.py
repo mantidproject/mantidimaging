1800389882834a560cea939f67bddbbf57fe3b13
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-01-26 14:26:30 +0000

    Using mock histograms

--- mantidimaging/gui/widgets/palette_changer/test/presenter_test.py
+++ mantidimaging/gui/widgets/palette_changer/test/presenter_test.py
@@ -1,10 +1,15 @@
 import unittest
 from unittest import mock
 
+import numpy as np
+from pyqtgraph.graphicsItems import HistogramLUTItem
+
 from mantidimaging.gui.widgets.palette_changer.presenter import PaletteChangerPresenter
 
 
 class PaletteChangerPresenterTest(unittest.TestCase):
     def setUp(self) -> None:
         self.view = mock.MagicMock()
-        self.presenter = PaletteChangerPresenter(self.view, None, None)
+        self.histograms = [mock.Mock(spec=HistogramLUTItem) for _ in range(3)]
+        self.projection_image = np.random.random((200, 200))
+        self.presenter = PaletteChangerPresenter(self.view, self.histograms, self.projection_image)
