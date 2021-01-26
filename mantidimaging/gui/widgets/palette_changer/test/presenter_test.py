6e9dde1bfef97f8804dd9fc48a4818ab92a2fec8
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-01-26 16:42:27 +0000

    Few more tests

--- mantidimaging/gui/widgets/palette_changer/test/presenter_test.py
+++ mantidimaging/gui/widgets/palette_changer/test/presenter_test.py
@@ -10,6 +10,7 @@ class PaletteChangerPresenterTest(unittest.TestCase):
     def setUp(self) -> None:
         self.view = mock.MagicMock()
         self.histograms = [mock.Mock() for _ in range(3)]
+        self.projection_histogram = self.histograms[-1]
         self.projection_image = np.random.random((200, 200))
         self.presenter = PaletteChangerPresenter(self.view, self.histograms, self.projection_image)
 
@@ -28,3 +29,32 @@ class PaletteChangerPresenterTest(unittest.TestCase):
 
         for histogram in self.histograms:
             histogram.gradient.loadPreset.assert_called_once_with(colour_map_selection)
+
+    def test_record_old_tick_points(self):
+        old_ticks_list = [mock.Mock() for i in range(2)]
+        self.projection_histogram.gradient.ticks = {old_ticks_list[i]: i * 1.0 for i in range(2)}
+        self.presenter._record_old_tick_points()
+        assert self.presenter.old_ticks == old_ticks_list
+
+    def test_insert_new_ticks(self):
+        pass
+
+    def test_change_colour_palette(self):
+        pass
+
+    def test_get_colours(self):
+        self.projection_histogram.gradient.getColor = get_color_mock = mock.Mock()
+        n_ticks = 5
+        colours = self.presenter._get_colours(n_ticks)
+        get_color_mock.assert_has_calls([mock.call(x) for x in np.linspace(0, 1, n_ticks)])
+        for colour in colours:
+            assert colour == get_color_mock.return_value
+
+    @mock.patch("mantidimaging.gui.widgets.palette_changer.presenter.filters.threshold_otsu")
+    def test_generate_otsu_tick_points(self, threshold_otsu_mock):
+        threshold_otsu_mock.return_value = otsu_value = 0.858
+        norm_otsu = (otsu_value - self.projection_image.min()) / np.ptp(self.projection_image)
+        self.assertListEqual(self.presenter._generate_otsu_tick_points(), [0.0, norm_otsu, 1.0])
+
+    def test_generate_jenks_tick_points(self):
+        pass
