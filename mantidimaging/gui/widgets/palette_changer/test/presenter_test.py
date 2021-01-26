b2d4de2bd40ce345c8da65ee0954d15cf23fb834
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-01-26 14:13:56 +0000

    Basic components of presenter tests

---
+++ mantidimaging/gui/widgets/palette_changer/test/presenter_test.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest import mock
+
+from mantidimaging.gui.widgets.palette_changer.presenter import PaletteChangerPresenter
+
+
+class PaletteChangerPresenterTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.view = mock.MagicMock()
+        self.presenter = PaletteChangerPresenter(view, None, None)
