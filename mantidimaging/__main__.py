9c876be0709a1aaf017952dbd9b3bf8cf417fe36
Sam Tygier <sam.tygier@stfc.ac.uk>
2021-02-09 10:45:38 +0000

    Fix shebangs
    Looks like some '!#' lines got moved when the copyright strings were
    originally added.

--- mantidimaging/__main__.py
+++ mantidimaging/__main__.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-
-# !/usr/bin/env python
 
 if __name__ == '__main__':
     from mantidimaging import main
--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
+#!/usr/bin/env python
 # Copyright (C) 2020 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-
-# !/usr/bin/env python
 
 import fnmatch
 import os
