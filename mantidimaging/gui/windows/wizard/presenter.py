a2c8f498c9a032aa3f92807cd2f2d11775fab3fe
Sam Tygier <sam.tygier@stfc.ac.uk>
2021-03-04 18:35:26 +0000

    Add wizard view and presenter
    Wizard is set up using wizard_steps.yml which specifies steps.
    
    Steps can trigger actions in the rest of the GUI. Applying a filter
    triggers the wizard to recheck the stack history and enable the next
    steps.

---
+++ mantidimaging/gui/windows/wizard/presenter.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+
+from logging import getLogger
+from pathlib import Path
+import yaml
+
+from mantidimaging.gui.mvp_base.presenter import BasePresenter
+from mantidimaging.gui.windows.wizard.view import WizardView
+
+wizard_data_file = Path(__file__).parent / "wizard_steps.yml"
+logger = getLogger(__name__)
+
+
+class WizardPresenter(BasePresenter):
+    def __init__(self, parent=None):
+        self.view = WizardView(parent, self)
+        super().__init__(self.view)
+
+        self.main_window_presenter = parent.presenter
+        self.wizard_data = yaml.safe_load(wizard_data_file.open())
+        self.populate()
+        parent.active_stacks_changed.connect(self.handle_stack_change)
+        parent.filter_applied.connect(self.handle_stack_change)
+        self.handle_stack_change()
+        self.show()
+
+    def show(self):
+        self.view.show()
+
+    def populate(self):
+        for stage in self.wizard_data["stages"]:
+            self.view.add_stage(stage["name"])
+            for step in stage["steps"]:
+                self.view.add_step(stage["name"], step)
+
+    def run_action(self, action: str):
+        if action == "load":
+            self.main_window_presenter.wizard_action_load()
+
+        elif action.startswith("operations"):
+            operation_menu_name = action.partition(":")[2].strip()
+            self.main_window_presenter.wizard_action_show_operation(operation_menu_name)
+
+        elif action.startswith("reconstruction"):
+            self.main_window_presenter.wizard_action_show_reconstruction()
+
+        else:
+            logger.error(f"Unknown action: '{action}'")
+
+    def handle_stack_change(self):
+        stack_history = None
+        stack_list = self.main_window_presenter.stack_list
+        for uuid, name in stack_list:
+            if "Tomo" not in name:
+                continue
+
+            stack_history = self.main_window_presenter.get_stack_history(uuid)
+            break
+
+        for stage in self.view.stages.values():
+            stage.handle_stack_change(stack_history)
---
+++ mantidimaging/gui/windows/wizard/view.py
@@ -0,0 +1,82 @@
+# Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+
+from __future__ import annotations
+from PyQt5.QtWidgets import QLabel, QWidget, QVBoxLayout, QGroupBox, QPushButton
+from typing import List
+
+from mantidimaging.gui.mvp_base import BaseDialogView
+from .model import EnablePredicateFactory
+
+
+class WizardStage(QWidget):
+    def __init__(self, name, parent=None):
+        super().__init__(parent)
+
+        self.layout = QVBoxLayout(self)
+        self.title_label = QLabel("Stage: " + name)
+        self.layout.addWidget(self.title_label)
+        self.steps: List[WizardStep] = []
+
+    def add_step(self, wizard_step: WizardStep):
+        self.steps.append(wizard_step)
+        self.layout.addWidget(wizard_step)
+
+    def handle_stack_change(self, stack_history: dict):
+        for step in self.steps:
+            step.handle_stack_change(stack_history)
+
+
+class WizardStep(QWidget):
+    def __init__(self, step: dict, wizard: WizardView, parent=None):
+        super().__init__(parent)
+
+        self.name = step["name"]
+        self.layout = QVBoxLayout(self)
+        self.title_label = QPushButton("Step: " + self.name)
+
+        self.layout.addWidget(self.title_label)
+
+        self.step_box = QGroupBox()
+        self.step_box_layout = QVBoxLayout(self.step_box)
+        self.description = QLabel(step["description"])
+
+        self.step_box_layout.addWidget(self.description)
+
+        if "action" in step:
+            run_button = QPushButton("Run")
+            self.step_box_layout.addWidget(run_button)
+            run_button.clicked.connect(lambda: wizard.presenter.run_action(step["action"]))
+
+        self.step_box.setVisible(False)
+        self.layout.addWidget(self.step_box)
+
+        self.title_label.clicked.connect(self.toggle_visible)
+
+        self.enable_predicate = EnablePredicateFactory(step.get("enable_if", ""))
+
+    def toggle_visible(self, event):
+        self.step_box.setVisible(not self.step_box.isVisible())
+
+    def handle_stack_change(self, stack_history: dict):
+        enabled = self.enable_predicate(stack_history)
+
+        if not enabled:
+            self.step_box.setVisible(False)
+        self.title_label.setEnabled(enabled)
+
+
+class WizardView(BaseDialogView):
+    def __init__(self, parent, presenter):
+        super().__init__(parent, "gui/ui/wizard.ui")
+        self.stages = {}
+        self.presenter = presenter
+
+    def add_stage(self, stage_name: str):
+        new_stage = WizardStage(stage_name)
+        self.stages[stage_name] = new_stage
+        self.wizard_top_layout.addWidget(new_stage)
+
+    def add_step(self, stage_name: str, step: dict):
+        new_step = WizardStep(step, self)
+        self.stages[stage_name].add_step(new_step)
