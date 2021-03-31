9e4b4b8e1b968ecf210fe74c7e584620adb856e4
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-03-31 15:33:49 +0100

    Set log scale when image is set

--- mantidimaging/gui/windows/stack_visualiser/view.py
+++ mantidimaging/gui/windows/stack_visualiser/view.py
@@ -8,7 +8,6 @@ from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtWidgets import QAction, QDockWidget, QInputDialog, QMenu, QMessageBox, QVBoxLayout, QWidget
 
 from mantidimaging.core.data import Images
-from mantidimaging.core.utility.histogram import _set_histogram_log_scale
 from mantidimaging.core.utility.sensible_roi import SensibleROI
 from mantidimaging.gui.dialogs.op_history_copy.view import OpHistoryCopyDialogView
 from mantidimaging.gui.widgets.mi_image_view.view import MIImageView
@@ -68,12 +67,12 @@ class StackVisualiserView(QDockWidget):
         self.actionCloseStack.triggered.connect(self.close)
         self.actionCloseStack.setShortcut("Ctrl+W")
 
-        _set_histogram_log_scale(self.image_view.ui.histogram)
-
         self.addAction(self.actionCloseStack)
         self.image_view.setImage(self.presenter.images.data)
         self.image_view.roi_changed_callback = self.roi_changed_callback
         self.layout.addWidget(self.image_view)
+
+        self.image_view.set_log_scale()
 
     @property
     def name(self):
@@ -94,6 +93,7 @@ class StackVisualiserView(QDockWidget):
     @image.setter
     def image(self, to_display):
         self.image_view.setImage(to_display)
+        self.image_view.set_log_scale()
 
     @property
     def main_window(self) -> 'MainWindowView':
