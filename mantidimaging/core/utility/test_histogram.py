9613f97ec134128698e797c41fdfea78acec25b5
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-04-15 15:23:35 +0100

    Passing test

---
+++ mantidimaging/core/utility/test_histogram.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+
+import numpy as np
+
+from unittest import mock
+
+from mantidimaging.core.utility.histogram import set_histogram_log_scale
+
+
+def test_set_histogram_log_scale():
+    x_data = np.array([i for i in range(10)])
+    y_data = x_data.copy()
+    histogram = mock.Mock()
+    histogram.plot.getData.return_value = (x_data, y_data)
+    set_histogram_log_scale(histogram)
+    np.testing.assert_array_equal(histogram.plot.setData.call_args_list[0][0][0], x_data)
+    np.testing.assert_array_equal(histogram.plot.setData.call_args_list[0][0][1], np.log(y_data + 1))
