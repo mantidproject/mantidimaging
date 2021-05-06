f131334fa3f4cbca1042aff7da98dcb2ba27ff82
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-05-06 16:37:31 +0100

    Reshaping averaged image to make it 3D and restoring previous MIImageView

--- mantidimaging/gui/windows/operations/view.py
+++ mantidimaging/gui/windows/operations/view.py
@@ -256,7 +256,7 @@ class FiltersWindowView(BaseMainWindowView):
 
         def set_averaged_image():
             averaged_images = np.sum(self.presenter.stack.presenter.images.data, axis=0)
-            self.roi_view.setImage(averaged_images)
+            self.roi_view.setImage(averaged_images.reshape((1, averaged_images.shape[0], averaged_images.shape[1])))
             self.roi_view_averaged = True
 
         def toggle_average_images(images_):
