bc9d1ac1b8c3f9e2601164cb3e04bfc7382f0ec2
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-05-10 12:15:40 +0100

    Subtraction value

--- mantidimaging/core/operations/arithmetic/arithmetic.py
+++ mantidimaging/core/operations/arithmetic/arithmetic.py
@@ -19,13 +19,18 @@ class ArithmeticFilter(BaseFilter):
     link_histograms = True
 
     @staticmethod
-    def filter_func(images: Images, mult_val: float = 1.0, div_val: float = 1.0, add_val: float = 1.0, progress=None) -> Images:
+    def filter_func(images: Images,
+                    mult_val: float = 1.0,
+                    div_val: float = 1.0,
+                    add_val: float = 0.0,
+                    sub_val: float = 0.0,
+                    progress=None) -> Images:
 
         if mult_val != 0:
             images.data *= mult_val
         if div_val != 0:
             images.data /= div_val
-        images.data += add_val
+        images.data = images.data + add_val - sub_val
         return images
 
     @staticmethod
@@ -38,31 +43,40 @@ class ArithmeticFilter(BaseFilter):
                                                     valid_values=(-MAX_SPIN_BOX, MAX_SPIN_BOX),
                                                     tooltip="The multiplication value.")
         _, div_input_widget = add_property_to_form('Divide',
-                                                    Type.FLOAT,
-                                                    form=form,
-                                                    on_change=on_change,
-                                                    default_value=1.0,
-                                                    valid_values=(-MAX_SPIN_BOX, MAX_SPIN_BOX),
-                                                    tooltip="The division value.")
-        _, add_input_widget = add_property_to_form('Add',
                                                    Type.FLOAT,
                                                    form=form,
                                                    on_change=on_change,
                                                    default_value=1.0,
                                                    valid_values=(-MAX_SPIN_BOX, MAX_SPIN_BOX),
+                                                   tooltip="The division value.")
+        _, add_input_widget = add_property_to_form('Add',
+                                                   Type.FLOAT,
+                                                   form=form,
+                                                   on_change=on_change,
+                                                   default_value=0.0,
+                                                   valid_values=(-MAX_SPIN_BOX, MAX_SPIN_BOX),
                                                    tooltip="The add value.")
+        _, sub_input_widget = add_property_to_form('Subtract',
+                                                   Type.FLOAT,
+                                                   form=form,
+                                                   on_change=on_change,
+                                                   default_value=0.0,
+                                                   valid_values=(-MAX_SPIN_BOX, MAX_SPIN_BOX),
+                                                   tooltip="The subtract value.")
 
         return {
             'mult_input_widget': mult_input_widget,
             'div_input_widget': div_input_widget,
             'add_input_widget': add_input_widget,
+            'sub_input_widget': sub_input_widget,
         }
 
     @staticmethod
     def execute_wrapper(  # type: ignore
-            mult_input_widget: QDoubleSpinBox, div_input_widget: QDoubleSpinBox, add_input_widget: QDoubleSpinBox) -> partial:
-        mult_input = mult_input_widget.value()
-        div_input = div_input_widget.value()
-        add_input = add_input_widget.value()
-
-        return partial(ArithmeticFilter.filter_func, mult_val=mult_input, div_val=div_input, add_val=add_input)
+            mult_input_widget: QDoubleSpinBox, div_input_widget: QDoubleSpinBox, add_input_widget: QDoubleSpinBox,
+            sub_input_widget: QDoubleSpinBox) -> partial:
+        return partial(ArithmeticFilter.filter_func,
+                       mult_val=mult_input_widget.value(),
+                       div_val=div_input_widget.value(),
+                       add_val=add_input_widget.value(),
+                       sub_val=sub_input_widget.value())
