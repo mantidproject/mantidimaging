cebdc558513705692bb4b695aabae1676f9d2545
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-05-10 16:30:07 +0100

    More tests

--- mantidimaging/core/operations/arithmetic/test/arithmetic_test.py
+++ mantidimaging/core/operations/arithmetic/test/arithmetic_test.py
@@ -19,3 +19,19 @@ class ArithmeticTest(unittest.TestCase):
         result = ArithmeticFilter().filter_func(images.copy(), div_val=2.0)
 
         npt.assert_array_equal(images.data * 0.5, result.data)
+
+    def test_execute_mult_only(self):
+        images = th.generate_images()
+
+        result = ArithmeticFilter().filter_func(images.copy(), mult_val=2.0)
+
+        npt.assert_array_equal(images.data * 2.0, result.data)
+
+    def test_cant_divide_or_multiply_by_zero(self):
+        images = th.generate_images()
+
+        result = ArithmeticFilter().filter_func(images.copy(), mult_val=0.0)
+        npt.assert_array_equal(images.data, result.data)
+
+        result = ArithmeticFilter().filter_func(images.copy(), div_val=0.0)
+        npt.assert_array_equal(images.data, result.data)
