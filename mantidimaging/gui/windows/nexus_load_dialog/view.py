003ab98a9a6e10f7fd19facfbe20f1cc1f3bebdf
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-06-08 15:53:45 +0100

    choose nexus file and change line edit

--- mantidimaging/gui/windows/nexus_load_dialog/view.py
+++ mantidimaging/gui/windows/nexus_load_dialog/view.py
@@ -1,15 +1,36 @@
 # Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QPushButton, QFileDialog, QLineEdit
 
 from mantidimaging.gui.utility import compile_ui
 from mantidimaging.gui.windows.nexus_load_dialog.presenter import NexusLoadPresenter
 
+NEXUS_CAPTION = "NeXus"
+NEXUS_FILTER = "NeXus (*.nxs *.hd5)"
+
 
 class NexusLoadDialog(QDialog):
+
+    chooseFileButton: QPushButton
+    filePathLineEdit: QLineEdit
+
     def __init__(self, parent):
         super(NexusLoadDialog, self).__init__(parent)
         compile_ui("gui/ui/nexus_load_dialog.ui", self)
 
         self.parent_view = parent
         self.presenter = NexusLoadPresenter(self)
+
+        self.chooseFileButton.clicked.connect(self.choose_nexus_file)
+        self.filePathLineEdit.textChanged.connect(self.scan_nexus_file)
+
+    def choose_nexus_file(self):
+        selected_file, _ = QFileDialog.getOpenFileName(caption=NEXUS_CAPTION,
+                                                       filter=f"{NEXUS_FILTER};;All (*.*)",
+                                                       initialFilter=NEXUS_FILTER)
+
+        if selected_file:
+            self.filePathLineEdit.setText(selected_file)
+
+    def scan_nexus_file(self, file_path: str):
+        print(file_path)
