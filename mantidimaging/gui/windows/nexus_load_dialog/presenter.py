ff2e9acbb73a6247bff16c47d7857c664344fef2
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-06-10 10:49:23 +0100

    moving nexus loader methods

--- mantidimaging/gui/windows/nexus_load_dialog/presenter.py
+++ mantidimaging/gui/windows/nexus_load_dialog/presenter.py
@@ -1,9 +1,34 @@
 # Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-from typing import TYPE_CHECKING
+import traceback
+from enum import auto, Enum
+from logging import getLogger
+from typing import TYPE_CHECKING, Optional, Union
+
+import h5py
 
 if TYPE_CHECKING:
     from mantidimaging.gui.windows.nexus_load_dialog.view import NexusLoadDialog  # pragma: no cover
+
+logger = getLogger(__name__)
+
+
+class Notification(Enum):
+    NEXUS_FILE_SELECTED = auto()
+
+
+TOMO_ENTRY = "tomo_entry"
+DATA_PATH = "instrument/detector/data"
+IMAGE_KEY_PATH = "instrument/detector/image_key"
+
+
+def _missing_data_message(field_name: str) -> str:
+    """
+    Creates a message for logging when a certain field is missing in the NeXus file.
+    :param field_name: The name of the missing field.
+    :return: A string telling the user that the field is missing.
+    """
+    return f"The NeXus file does not contain the required {field_name} field."
 
 
 class NexusLoadPresenter:
@@ -11,3 +36,64 @@ class NexusLoadPresenter:
 
     def __init__(self, view: 'NexusLoadDialog'):
         self.view = view
+        self.nexus_file = None
+        self.tomo_entry = None
+        self.data = None
+        self.tomo_path = ""
+
+    def notify(self, n: Notification, **baggage):
+        try:
+            if n == Notification.NEXUS_FILE_SELECTED:
+                if not baggage:
+                    return
+                self.scan_nexus_file(**baggage)
+        except RuntimeError as err:
+            self.view.show_error(str(err), traceback.format_exc())
+
+    def scan_nexus_file(self, file_path: str):
+        with h5py.File(file_path, "r") as self.nexus_file:
+            self.tomo_entry = self._find_tomo_entry()
+            if self.tomo_entry is None:
+                error_msg = _missing_data_message(TOMO_ENTRY)
+                logger.error(error_msg)
+                self.view.show_error(error_msg)
+                return
+
+            self.data = self._get_tomo_data(DATA_PATH)
+            if self.data is None:
+                error_msg = _missing_data_message(DATA_PATH)
+                logger.error(error_msg)
+                self.view.show_error(error_msg)
+                return
+
+            self.view.set_data_found(1, True, self.tomo_path + DATA_PATH)
+            #
+            # self.title = self._find_data_title()
+            #
+            # self.image_key_dataset = self._get_tomo_data(IMAGE_KEY_PATH)
+            # if self.image_key_dataset is None:
+            #     return self._get_projections()
+            # else:
+            #     return self._get_data_from_image_key()
+
+    def _find_tomo_entry(self) -> Optional[h5py.Group]:
+        """
+        Look for a tomo_entry field in the NeXus file.
+        :return: The first tomo_entry group if one could be found, None otherwise.
+        """
+        for key in self.nexus_file.keys():
+            if TOMO_ENTRY in self.nexus_file[key].keys():
+                self.tomo_path = f"{key}/{TOMO_ENTRY}"
+                return self.nexus_file[key][TOMO_ENTRY]
+        return None
+
+    def _get_tomo_data(self, entry_path: str) -> Optional[Union[h5py.Group, h5py.Dataset]]:
+        """
+        Retrieve data from the tomo entry field.
+        :param entry_path: The path in which the data is found.
+        :return: The Nexus group if it exists, None otherwise.
+        """
+        try:
+            return self.tomo_entry[entry_path]
+        except KeyError:
+            return None
--- mantidimaging/gui/windows/nexus_load_dialog/view.py
+++ mantidimaging/gui/windows/nexus_load_dialog/view.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-from PyQt5.QtWidgets import QDialog, QPushButton, QFileDialog, QLineEdit
+from PyQt5.QtWidgets import QDialog, QPushButton, QFileDialog, QLineEdit, QTreeWidget, QTreeWidgetItem, QLabel
 
 from mantidimaging.gui.utility import compile_ui
-from mantidimaging.gui.windows.nexus_load_dialog.presenter import NexusLoadPresenter
+from mantidimaging.gui.windows.nexus_load_dialog.presenter import NexusLoadPresenter, Notification
 
 NEXUS_CAPTION = "NeXus"
 NEXUS_FILTER = "NeXus (*.nxs *.hd5)"
@@ -11,6 +11,7 @@ NEXUS_FILTER = "NeXus (*.nxs *.hd5)"
 
 class NexusLoadDialog(QDialog):
 
+    tree: QTreeWidget
     chooseFileButton: QPushButton
     filePathLineEdit: QLineEdit
 
@@ -22,7 +23,7 @@ class NexusLoadDialog(QDialog):
         self.presenter = NexusLoadPresenter(self)
 
         self.chooseFileButton.clicked.connect(self.choose_nexus_file)
-        self.filePathLineEdit.textChanged.connect(self.scan_nexus_file)
+        # self.filePathLineEdit.textChanged.connect(self.presenter.notify(Notification.NEXUS_FILE_SELECTED))
 
     def choose_nexus_file(self):
         selected_file, _ = QFileDialog.getOpenFileName(caption=NEXUS_CAPTION,
@@ -31,6 +32,18 @@ class NexusLoadDialog(QDialog):
 
         if selected_file:
             self.filePathLineEdit.setText(selected_file)
+            self.presenter.notify(Notification.NEXUS_FILE_SELECTED)
 
-    def scan_nexus_file(self, file_path: str):
-        print(file_path)
+    def set_data_found(self, position: int, found: bool, path: str):
+        if not found:
+            pass
+
+        section: QTreeWidgetItem = self.tree.topLevelItem(position)
+        found_text = QLabel("True")
+        path_text = QLabel(path)
+
+        self.tree.setItemWidget(section, 2, found_text)
+        self.tree.setItemWidget(section, 3, path_text)
+
+    def show_error(self, msg, traceback):
+        self.parent_view.presenter.show_error(msg, traceback)
