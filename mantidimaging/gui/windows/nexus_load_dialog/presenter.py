30125e9df6f27514b2e7bb6a26a21258aa057702
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-06-21 11:53:16 +0100

    isinstance doesn't accept h5py types

--- mantidimaging/gui/windows/nexus_load_dialog/presenter.py
+++ mantidimaging/gui/windows/nexus_load_dialog/presenter.py
@@ -123,7 +123,7 @@ class NexusLoadPresenter:
         found.
         :return: The first tomo_entry group if one could be found, None otherwise.
         """
-        assert isinstance(self.nexus_file, h5py.File)
+        assert self.nexus_file is not None
         for key in self.nexus_file.keys():
             if TOMO_ENTRY in self.nexus_file[key].keys():
                 self.tomo_path = f"{key}/{TOMO_ENTRY}"
@@ -139,7 +139,7 @@ class NexusLoadPresenter:
         :param entry_path: The path in which the data is found.
         :return: The Nexus Group/Dataset if it exists, None otherwise.
         """
-        assert isinstance(self.tomo_entry, h5py.Group)
+        assert self.tomo_entry is not None
         try:
             return self.tomo_entry[entry_path]
         except KeyError:
@@ -176,8 +176,8 @@ class NexusLoadPresenter:
                        images. Ignored when getting projection images.
         :return: The set of images that correspond with a given image key.
         """
-        assert isinstance(self.image_key_dataset, h5py.Dataset)
-        assert isinstance(self.data, h5py.Dataset)
+        assert self.image_key_dataset is not None
+        assert self.data is not None
         if image_key_number is ImageKeys.Projections:
             indices = self.image_key_dataset[...] == image_key_number.value
         else:
@@ -194,7 +194,7 @@ class NexusLoadPresenter:
         Find the title field in the tomo_entry.
         :return: The title if it was found, "NeXus Data" otherwise.
         """
-        assert isinstance(self.tomo_entry, h5py.Group)
+        assert self.tomo_entry is not None
         try:
             return self.tomo_entry["title"][0].decode("UTF-8")
         except KeyError:
