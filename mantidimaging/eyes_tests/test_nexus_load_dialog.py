73e51de419eabd2d4aa5f4d6ba47cd8f595eb66d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-06-23 17:08:35 +0100

    using set text

--- mantidimaging/eyes_tests/test_nexus_load_dialog.py
+++ mantidimaging/eyes_tests/test_nexus_load_dialog.py
@@ -1,7 +1,5 @@
 # Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-from unittest import mock
-
 from mantidimaging.eyes_tests.base_eyes import BaseEyesTest, NEXUS_SAMPLE
 
 
@@ -13,9 +11,7 @@ class LoadDialogTest(BaseEyesTest):
 
     def test_load_nexus_file_populates_window(self):
         self.imaging.actionLoadNeXusFile.trigger()
-        self.imaging.nexus_load_dialog.filePathLineEdit.text = mock.MagicMock(return_value=NEXUS_SAMPLE)
+        self.imaging.nexus_load_dialog.filePathLineEdit.setText(NEXUS_SAMPLE)
 
         self.imaging.nexus_load_dialog.presenter.scan_nexus_file()
-        # self.imaging.presenter.load_nexus_file()
-
         self.check_target(widget=self.imaging.nexus_load_dialog)
