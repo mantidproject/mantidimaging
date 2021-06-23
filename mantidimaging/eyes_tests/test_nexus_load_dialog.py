5e61a95ec92be2a563aa8a912af9e16090339117
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-06-23 15:44:37 +0100

    trying out applitools test

---
+++ mantidimaging/eyes_tests/test_nexus_load_dialog.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+from unittest import mock
+
+from mantidimaging.eyes_tests.base_eyes import BaseEyesTest, NEXUS_SAMPLE
+
+
+class LoadDialogTest(BaseEyesTest):
+    def test_load_dialog_opens(self):
+        self.imaging.actionLoadNeXusFile.trigger()
+
+        self.check_target(widget=self.imaging.nexus_load_dialog)
+
+    def test_load_dialog_selected_dataset(self):
+        self.imaging.actionLoadDataset.trigger()
+        self.imaging.nexus_load_dialog.view.filePathLineEdit.text = mock.MagicMock(return_value=NEXUS_SAMPLE)
+
+        self.imaging.nexus_load_dialog.scan_nexus_file()
+        self.imaging.presenter.load_nexus_file()
+
+        self.check_target(widget=self.imaging.nexus_load_dialog)
