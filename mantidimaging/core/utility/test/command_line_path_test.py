c5b63bd3c2ab3635f45eb86f983118f88d4800b3
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-09-14 13:29:38 +0100

    clearing instances

--- mantidimaging/core/utility/test/command_line_path_test.py
+++ mantidimaging/core/utility/test/command_line_path_test.py
@@ -5,5 +5,6 @@ from mantidimaging.core.utility.command_line_path import CommandLinePath
 
 class CommandLinePathTest(unittest.TestCase):
     def test_bad_path_calls_exit(self):
+        CommandLinePath._instance = None
         with self.assertRaises(SystemExit):
             CommandLinePath("does/not/exist")
