7b22b1c4c6060d6806ab1bbb16e9599876e82da6
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-09-21 16:52:20 +0100

    conversion

--- mantidimaging/core/operations/loader.py
+++ mantidimaging/core/operations/loader.py
@@ -3,11 +3,12 @@
 import os
 import pkgutil
 import sys
-from typing import List, Iterator
+from typing import List, Iterator, Optional
 
 from mantidimaging.core.operations.base_filter import BaseFilter
 
 OPERATIONS_CLASSES = []
+OPERATIONS_DIR = "mantidimaging.core.operations"
 
 
 def find_package_path(package_str):
@@ -29,12 +30,10 @@ def find_package_path(package_str):
     raise RuntimeError("Cannot find path for package {}".format(package_str))
 
 
-for loader, module_name, is_pkg in pkgutil.walk_packages([find_package_path("mantidimaging.core.operations")]):
-    if is_pkg:
-        OPERATIONS_CLASSES.append(loader.find_module(module_name))
+OPERATIONS_ITER = pkgutil.walk_packages([find_package_path(OPERATIONS_DIR)])
 
 
-def get_package_children(pkgs, packages=False, modules=False, ignore=None) -> Iterator[pkgutil.ModuleInfo]:
+def get_package_children(package_name: str, packages=False, modules=False, ignore=None) -> Iterator[pkgutil.ModuleInfo]:
     """
     Gets a list of names of child packages or modules found under a given
     package.
@@ -46,6 +45,12 @@ def get_package_children(pkgs, packages=False, modules=False, ignore=None) -> It
 
     :return: Iterator over matching modules
     """
+    if package_name == OPERATIONS_DIR:
+        pkgs = OPERATIONS_ITER
+    else:
+        pkgs: Iterator[pkgutil.ModuleInfo] = pkgutil.walk_packages([find_package_path(package_name)],
+                                                                   prefix=package_name + '.')
+
     # Ignore those that do not match the package/module selection criteria
     pkgs = filter(lambda p: p.ispkg and packages or not p.ispkg and modules, pkgs)
 
@@ -54,6 +59,29 @@ def get_package_children(pkgs, packages=False, modules=False, ignore=None) -> It
         pkgs = filter(lambda p: not any([m in p.name for m in ignore]), pkgs)
 
     return pkgs
+
+
+def import_items(packages: Iterator[pkgutil.ModuleInfo], required_attributes: Optional[List[str]] = None):
+    """
+    Imports a list of packages/modules and operations out those that do not have a
+    specified required list of attributes.
+
+    :param packages: List of packages to import.
+    :param required_attributes: Optional list of attributes that must be
+                                present on each individual module
+
+    :return: List of imported packages/modules
+    """
+    imported = []
+    for loader, module_name, _ in packages:
+        imported.append(loader.find_module(module_name).load_module())
+
+    # Filter out those that do not contain all the required attributes
+    if required_attributes:
+        imported = filter(  # type: ignore
+            lambda i: all([hasattr(i, a) for a in required_attributes]), imported)  # type: ignore
+
+    return imported
 
 
 def load_filter_packages(package_name="mantidimaging.core.operations", ignored_packages=None) -> List[BaseFilter]:
@@ -67,14 +95,9 @@ def load_filter_packages(package_name="mantidimaging.core.operations", ignored_p
                          operations
     :param ignored_packages: List of ignore rules
     """
+    filter_packages = get_package_children(package_name, packages=True, ignore=ignored_packages)
 
-    filters = [p.load_module() for p in OPERATIONS_CLASSES]
-    filters = [f for f in filters if hasattr(f, "FILTER_CLASS")]
+    loaded_filters = import_items(filter_packages, required_attributes=['FILTER_CLASS'])
+    loaded_filters = filter(lambda f: f.available() if hasattr(f, 'available') else True, loaded_filters)
 
-    # filter_packages = get_package_children(package_name, packages=True, ignore=ignored_packages)
-    # filter_package_names = [p.name for p in OPERATIONS_CLASSES]
-    #
-    # loaded_filters = import_items(filter_package_names, required_attributes=['FILTER_CLASS'])
-    # loaded_filters = filter(lambda f: f.available() if hasattr(f, 'available') else True, loaded_filters)
-
-    return [f.FILTER_CLASS for f in filters]
+    return [f.FILTER_CLASS for f in loaded_filters]
