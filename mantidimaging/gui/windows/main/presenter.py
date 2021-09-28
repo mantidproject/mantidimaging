dc3bcd8d91a88949d9f702480f67b67c8f1d7595
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-09-28 13:18:48 +0100

    Saving 180 projection as Images object

--- mantidimaging/gui/windows/main/presenter.py
+++ mantidimaging/gui/windows/main/presenter.py
@@ -146,7 +146,7 @@ class MainWindowPresenter(BasePresenter):
                 closest_projection, diff = find_projection_closest_to_180(sample.projections,
                                                                           sample.projection_angles().value)
                 if diff <= THRESHOLD_180 or self.view.ask_to_use_closest_to_180(diff):
-                    container.sample.proj180deg = closest_projection
+                    container.sample.proj180deg = Images(closest_projection)
                     self._add_stack(container.sample.proj180deg, "180", sample_stack_vis)
 
         if len(current_stack_visualisers) > 1:
