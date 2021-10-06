9b99d27c488bf7fb09cdcd8a170317d6c0295755
Sam Tygier <sam.tygier@stfc.ac.uk>
2021-10-06 18:44:55 +0100

    Appease mypy 0.900
    pkgutil and import lib give unions and optionals that upset mypy. Here
    we know what the concrete types are and so can assert them.
    
    Also need to convince mypy that the modules have a FILTER_CLASS
    attribute. This is done by creating a protocol and casting to it. This
    is safe because the previous line has check the attribute exists.

--- mantidimaging/core/operations/loader.py
+++ mantidimaging/core/operations/loader.py
@@ -2,13 +2,23 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 import os
 import pkgutil
-from typing import List
+from typing import List, Protocol, cast
+from importlib.machinery import FileFinder, ModuleSpec
+from importlib.abc import Loader
 
 from mantidimaging.core.operations.base_filter import BaseFilter
 
 MODULES_OPERATIONS = {}
-for loader, module_name, is_pkg in pkgutil.walk_packages([os.path.dirname(__file__)]):
-    MODULES_OPERATIONS[module_name] = loader.find_spec(module_name).loader
+for finder, module_name, is_pkg in pkgutil.walk_packages([os.path.dirname(__file__)]):
+    assert isinstance(finder, FileFinder)
+    spec = finder.find_spec(module_name)
+    assert isinstance(spec, ModuleSpec)
+    assert isinstance(spec.loader, Loader)
+    MODULES_OPERATIONS[module_name] = spec.loader
+
+
+class OperationModule(Protocol):
+    FILTER_CLASS: BaseFilter
 
 
 def load_filter_packages(ignored_packages=None) -> List[BaseFilter]:
@@ -23,9 +33,10 @@ def load_filter_packages(ignored_packages=None) -> List[BaseFilter]:
 
     filters = {name: MODULES_OPERATIONS[name].load_module(name) for name in MODULES_OPERATIONS.keys()}
     filters = {name: filters[name] for name in filters.keys() if hasattr(filters[name], 'FILTER_CLASS')}
+    operation_modules = {name: cast(OperationModule, f) for name, f in filters.items()}
     if not ignored_packages:
-        return [f.FILTER_CLASS for f in filters.values()]
+        return [f.FILTER_CLASS for f in operation_modules.values()]
     return [
-        filters[name].FILTER_CLASS for name in filters.keys()
+        operation_modules[name].FILTER_CLASS for name in operation_modules.keys()
         if not any([ignore in name for ignore in ignored_packages])
     ]
