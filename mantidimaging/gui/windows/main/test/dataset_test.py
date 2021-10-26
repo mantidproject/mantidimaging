87b3a55c817c3035e85c09ea37dd1a3e66bfaead
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-10-26 16:35:27 +0100

    images in dataset not set tests

--- mantidimaging/gui/windows/main/test/dataset_test.py
+++ mantidimaging/gui/windows/main/test/dataset_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+from mantidimaging.core.data import Images
 from mantidimaging.core.data.dataset import Dataset
 from mantidimaging.test_helpers.unit_test_helper import generate_images
 
@@ -24,4 +25,19 @@ class DatasetTest(unittest.TestCase):
     def test_delete_dark_after(self):
         self.images.pop(4)
         self.assertIsNone(self.dataset.dark_after)
-        
+
+    def test_images_returned(self):
+        self.assertIsInstance(self.dataset.sample, Images)
+        self.assertIsInstance(self.dataset.flat_before, Images)
+        self.assertIsInstance(self.dataset.flat_after, Images)
+        self.assertIsInstance(self.dataset.dark_before, Images)
+        self.assertIsInstance(self.dataset.dark_after, Images)
+
+    def test_attribute_not_set_returns_none(self):
+        sample = generate_images()
+        dataset = Dataset(sample)
+
+        self.assertIsNone(dataset.flat_before)
+        self.assertIsNone(dataset.flat_after)
+        self.assertIsNone(dataset.dark_before)
+        self.assertIsNone(dataset.dark_after)
