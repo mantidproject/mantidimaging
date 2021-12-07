15c3a1ab2e6c0c2a22614ce9df13771ecc5cd835
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2021-12-07 12:28:04 +0000

    stackdatasettest

---
+++ mantidimaging/gui/windows/main/test/stackdataset_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from mantidimaging.core.data.dataset import StackDataset
+from mantidimaging.test_helpers.unit_test_helper import generate_images
+
+
+class StackDatasetTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.image_stacks = [generate_images() for _ in range(3)]
+        self.stack_dataset = StackDataset(self.image_stacks)
+
+    def test_all(self):
+        self.assertListEqual(self.stack_dataset.all, self.image_stacks)
+
+    def test_delete_stack(self):
+        prev_stacks = self.image_stacks.copy()
+        self.stack_dataset.delete_stack(self.image_stacks[-1].id)
+        self.assertListEqual(self.stack_dataset.all, prev_stacks[:-1])
