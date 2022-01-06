1fd2f6a59c96a104f0ab55842cdf0cbfb3784720
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-01-06 17:49:13 +0000

    precommit

--- mantidimaging/gui/windows/nexus_load_dialog/presenter.py
+++ mantidimaging/gui/windows/nexus_load_dialog/presenter.py
@@ -244,10 +244,10 @@ class NexusLoadPresenter:
         """
         sample_images = self._create_sample_images()
         ds = Dataset(sample=sample_images,
-                       flat_before=self._create_images_if_required(self.flat_before_array, "Flat Before"),
-                       flat_after=self._create_images_if_required(self.flat_after_array, "Flat After"),
-                       dark_before=self._create_images_if_required(self.dark_before_array, "Dark Before"),
-                       dark_after=self._create_images_if_required(self.dark_after_array, "Dark After"))
+                     flat_before=self._create_images_if_required(self.flat_before_array, "Flat Before"),
+                     flat_after=self._create_images_if_required(self.flat_after_array, "Flat After"),
+                     dark_before=self._create_images_if_required(self.dark_before_array, "Dark Before"),
+                     dark_after=self._create_images_if_required(self.dark_after_array, "Dark After"))
         ds.name = self.title
         return ds, self.title
 
