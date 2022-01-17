a41d9ecd5e7a6019c75b6ea6951978b9f716e9cf
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-01-17 17:05:48 +0000

    Basic test

--- mantidimaging/gui/widgets/dataset_selector/test/test_presenter.py
+++ mantidimaging/gui/widgets/dataset_selector/test/test_presenter.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest import mock
 
-from mantidimaging.gui.widgets.dataset_selector.presenter import DatasetSelectorWidgetPresenter
+from mantidimaging.gui.widgets.dataset_selector.presenter import DatasetSelectorWidgetPresenter, Notification
 from mantidimaging.gui.widgets.dataset_selector.view import DatasetSelectorWidgetView
 
 
@@ -24,3 +24,16 @@ class DatasetSelectorWidgetPresenterTests(unittest.TestCase):
         self.presenter.handle_selection(1)
         self.view.dataset_selected_uuid.emit.assert_called_with(matching_uuid)
         assert matching_uuid == self.presenter.current_dataset
+
+    def test_notify_reload_datasets(self):
+        self.presenter.do_reload_datasets = mock.Mock()
+        self.presenter.notify(Notification.RELOAD_DATASETS)
+        self.presenter.do_reload_datasets.assert_called_once()
+
+    def test_do_reload_datasets(self):
+        self.view.main_window = mock.Mock()
+        self.view.datasets_updated.emit = mock.Mock()
+        self.view.dataset_selected_uuid.emit = mock.Mock()
+        self.view.currentText.return_value = dataset_name = "dataset-name"
+        self.view.main_window.dataset_list = [("dataset-id", dataset_name)]
+        self.presenter.do_reload_datasets()
