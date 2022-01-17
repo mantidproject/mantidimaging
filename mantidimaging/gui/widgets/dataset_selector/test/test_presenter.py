3ab373311e2687081a4cedc2c3b0fdbeba6936a7
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-01-17 12:19:28 +0000

    2nd handle selection test

--- mantidimaging/gui/widgets/dataset_selector/test/test_presenter.py
+++ mantidimaging/gui/widgets/dataset_selector/test/test_presenter.py
@@ -10,8 +10,17 @@ class DatasetSelectorWidgetPresenterTests(unittest.TestCase):
         self.view = mock.create_autospec(DatasetSelectorWidgetView)
         self.presenter = DatasetSelectorWidgetPresenter(self.view)
 
-    def test_handle_selection_no_matching_index(self):
+    def test_handle_selection_no_matching_index_found(self):
         self.view.dataset_selected_uuid.emit = mock.Mock()
         self.presenter.handle_selection(1)
         self.view.dataset_selected_uuid.emit.assert_called_with(None)
         self.assertIsNone(self.presenter.current_dataset)
+
+    def test_handle_selection_matching_index_found(self):
+        matching_uuid = "second-uuid"
+        self.presenter.dataset_uuids = ["first-uuid", matching_uuid]
+        self.view.dataset_selected_uuid.emit = mock.Mock()
+
+        self.presenter.handle_selection(1)
+        self.view.dataset_selected_uuid.emit.assert_called_with(matching_uuid)
+        assert matching_uuid == self.presenter.current_dataset
