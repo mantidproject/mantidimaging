8358194ccd7c26f171c780450e871a800a11b4d2
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-01-18 16:56:19 +0000

    typo

--- mantidimaging/gui/widgets/dataset_selector/test/test_presenter.py
+++ mantidimaging/gui/widgets/dataset_selector/test/test_presenter.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
-# SPDX - License - Identifier: GPL-3.0-or-late
+# SPDX - License - Identifier: GPL-3.0-or-later
 
 import unittest
 from unittest import mock
