256ef4c5b0ecbef14e5cd13d52556bcc24cc8e54
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-01-18 12:18:00 +0000

    tests

--- mantidimaging/gui/widgets/dataset_selector/test/test_view.py
+++ mantidimaging/gui/widgets/dataset_selector/test/test_view.py
@@ -11,12 +11,26 @@ from mantidimaging.test_helpers import start_qapplication
 class DatasetSelectorWidgetViewTest(unittest.TestCase):
     def setUp(self) -> None:
         self.view = DatasetSelectorWidgetView(None)
+        self.view.presenter = self.presenter = mock.Mock()
 
     def test_subscribe_to_main_window(self):
         main_window_mock = mock.create_autospec(MainWindowView)
         main_window_mock.model_changed.connect = mock.Mock()
-        self.view.presenter = presenter_mock = mock.Mock()
 
         self.view.subscribe_to_main_window(main_window_mock)
-        presenter_mock.notify.assert_called_once_with(Notification.RELOAD_DATASETS)
+        self.presenter.notify.assert_called_once_with(Notification.RELOAD_DATASETS)
         main_window_mock.model_changed.connect.assert_called_once_with(self.view._handle_loaded_datasets_changed)
+
+    def test_unsubscribe_from_main_window(self):
+        self.view.main_window = main_window_mock = mock.create_autospec(MainWindowView)
+        main_window_mock.model_changed.disconnect = mock.Mock()
+
+        self.view.unsubscribe_from_main_window()
+        main_window_mock.model_changed.disconnect.assert_called_once_with(self.view._handle_loaded_datasets_changed)
+
+    def test_handle_loaded_datasets_changed(self):
+        self.view._handle_loaded_datasets_changed()
+        self.presenter.notify.assert_called_once_with(Notification.RELOAD_DATASETS)
+
+    def test_current(self):
+        assert self.view.current() is self.presenter.current_dataset
