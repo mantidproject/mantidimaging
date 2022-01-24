3dcc5fb5b6d18d5e103b4e272961de67f37fe0b7
Rachel Baust <rachel.baust@stfc.ac.uk>
2022-01-24 11:21:32 +0000

    Update licence year in all files

--- mantidimaging/core/operations/outliers/__init__.py
+++ mantidimaging/core/operations/outliers/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
+# Copyright (C) 2022 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 
 from .outliers import OutliersFilter, modes  # noqa:F401
--- mantidimaging/gui/__init__.py
+++ mantidimaging/gui/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
+# Copyright (C) 2022 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 
 from .gui import execute, setup_application  # noqa: F401  # noqa:F821
