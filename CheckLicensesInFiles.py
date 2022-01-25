8026236085f43d26776d6e08e02b453cc5896fa9
Rachel Baust <rachel.baust@stfc.ac.uk>
2022-01-25 15:45:55 +0000

    Add pre-commit rule to check licenses

---
+++ CheckLicensesInFiles.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# Copyright (C) 2022 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+
+import json
+import sys
+from typing import List, Dict
+
+
+def load_copyright_header() -> Dict[str, str]:
+    return json.load(open(".licenserc.json"))
+
+
+def find_files_with_incorrect_license_headers(filepaths: List[str], copyright_text: str) -> List[str]:
+    copyright_lines = copyright_text.split("\n")
+    incorrect_files = []
+
+    for filepath in filepaths:
+        lines = open(filepath).readlines()
+
+        if len(lines) > 0:
+            if has_shebang_line(lines):
+                del lines[0]
+
+            if not has_correct_copyright_lines(lines, copyright_lines):
+                incorrect_files.append(filepath)
+
+    return incorrect_files
+
+
+def has_shebang_line(file_lines: List[str]) -> bool:
+    return file_lines[0].startswith("#!")
+
+
+def has_correct_copyright_lines(file_lines: List[str], copyright_lines: List[str]) -> bool:
+    if len(file_lines) < len(copyright_lines):
+        return False
+
+    for i in range(len(copyright_lines)):
+        if file_lines[i].strip() != copyright_lines[i].strip():
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    copyright_strings = load_copyright_header()
+    files_to_check = sys.argv[1:]
+
+    files_in_error = set()
+    for copyright_string in copyright_strings.values():
+        files_in_error.update(find_files_with_incorrect_license_headers(files_to_check, copyright_string))
+
+    if len(files_in_error) > 0:
+        print('The following files contain errors in their licenses:\n')
+        print('\n'.join(files_in_error))
+        exit(1)
+
+    exit(0)
