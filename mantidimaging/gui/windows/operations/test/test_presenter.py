dca2c09a96824cd3f4e52e0d9ec96ee86be2047f
Rachel Baust <rachel.baust@stfc.ac.uk>
2022-02-07 14:44:31 +0000

    Remove obsolete test mock

--- mantidimaging/gui/windows/operations/test/test_presenter.py
+++ mantidimaging/gui/windows/operations/test/test_presenter.py
@@ -24,7 +24,6 @@ from mantidimaging.core.data import Images
 class FiltersWindowPresenterTest(unittest.TestCase):
     def setUp(self) -> None:
         self.main_window = mock.create_autospec(MainWindowView)
-        self.main_window.filter_applied.connect = mock.Mock()
         self.view = mock.MagicMock()
         self.presenter = FiltersWindowPresenter(self.view, self.main_window)
         self.presenter.model.filter_widget_kwargs = {"roi_field": None}
