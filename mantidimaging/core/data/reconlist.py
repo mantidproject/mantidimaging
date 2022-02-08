15c511bb8bd650b621415393cf268a3d8cd1421b
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-02-08 11:42:33 +0000

    ids property

--- mantidimaging/core/data/reconlist.py
+++ mantidimaging/core/data/reconlist.py
@@ -2,9 +2,11 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 
 import uuid
+from collections import UserList
+from typing import List
 
 
-class ReconList(list):
+class ReconList(UserList):
     def __init__(self):
         super().__init__()
         self._id: uuid.UUID = uuid.UUID
@@ -12,3 +14,7 @@ class ReconList(list):
     @property
     def id(self) -> uuid.UUID:
         return self._id
+
+    @property
+    def ids(self) -> List[uuid.UUID]:
+        return [recon.id for recon in self.data]
