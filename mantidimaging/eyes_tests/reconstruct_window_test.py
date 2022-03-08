528ed0e5a041a9ffd3b99a272f52268d78e50259
Rachel Baust <rachel.baust@stfc.ac.uk>
2022-03-08 15:32:58 +0000

    Update eyes test for recon colour palette dialog

--- mantidimaging/eyes_tests/reconstruct_window_test.py
+++ mantidimaging/eyes_tests/reconstruct_window_test.py
@@ -61,7 +61,7 @@ class ReconstructionWindowTest(BaseEyesTest):
         self._load_data_set()
 
         self.imaging.show_recon_window()
-        self.imaging.recon.image_view.recon.image = np.empty((512, 512))
+        self.imaging.recon.image_view.imageview_recon.im.image = np.empty((512, 512))
         self.imaging.recon.changeColourPaletteButton.click()
 
         self.check_target(widget=self.imaging.recon.change_colour_palette_dialog)
