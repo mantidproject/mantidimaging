114e79b9bdbe1767a51484e99500f86bd5a2660f
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-03-29 16:47:47 +0100

    line edit tests

--- mantidimaging/gui/windows/main/test/nexus_save_test.py
+++ mantidimaging/gui/windows/main/test/nexus_save_test.py
@@ -27,3 +27,32 @@ class NexusSaveDialogTest(unittest.TestCase):
         save_path = "a/save/path"
         self.nexus_save_dialog.savePath.text = mock.Mock(return_value=save_path)
         self.assertEqual(save_path, self.nexus_save_dialog.save_path())
+
+    def test_sample_name(self):
+        sample_name = "sample-name"
+        self.nexus_save_dialog.sampleNameLineEdit.text = mock.Mock(return_value=sample_name)
+        self.assertEqual(sample_name, self.nexus_save_dialog.sample_name())
+
+    def test_save_disabled_when_no_save_path(self):
+        self.nexus_save_dialog.savePath.text = mock.Mock(return_value="")
+        self.nexus_save_dialog.sampleNameLineEdit.text = mock.Mock(return_value="sample-name")
+        self.nexus_save_dialog.buttonBox = mock.Mock()
+
+        self.nexus_save_dialog.enable_save()
+        self.nexus_save_dialog.buttonBox.button.return_value.setEnabled.assert_called_once_with(False)
+
+    def test_save_disabled_when_no_sample_name(self):
+        self.nexus_save_dialog.savePath.text = mock.Mock(return_value="save-path")
+        self.nexus_save_dialog.sampleNameLineEdit.text = mock.Mock(return_value="")
+        self.nexus_save_dialog.buttonBox = mock.Mock()
+
+        self.nexus_save_dialog.enable_save()
+        self.nexus_save_dialog.buttonBox.button.return_value.setEnabled.assert_called_once_with(False)
+
+    def test_save_enabled(self):
+        self.nexus_save_dialog.savePath.text = mock.Mock(return_value="save-path")
+        self.nexus_save_dialog.sampleNameLineEdit.text = mock.Mock(return_value="sample-name")
+        self.nexus_save_dialog.buttonBox = mock.Mock()
+
+        self.nexus_save_dialog.enable_save()
+        self.nexus_save_dialog.buttonBox.button.return_value.setEnabled.assert_called_once_with(True)
