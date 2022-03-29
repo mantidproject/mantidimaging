4629aaa412e6157b28b67383e2503515c627b34c
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-03-29 16:38:01 +0100

    save path test

--- mantidimaging/gui/windows/main/test/nexus_save_test.py
+++ mantidimaging/gui/windows/main/test/nexus_save_test.py
@@ -9,14 +9,21 @@ from mantidimaging.test_helpers import start_qapplication
 
 @start_qapplication
 class NexusSaveDialogTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.nexus_save_dialog = NexusSaveDialog(None, [])
+
     def test_save_calls_execute_nexus_save(self):
         parent = mock.Mock()
-        nexus_save_dialog = NexusSaveDialog(None, [])
-        nexus_save_dialog.parent = mock.Mock(return_value=parent)
+        self.nexus_save_dialog.parent = mock.Mock(return_value=parent)
 
         dataset_id = "dataset-id"
-        nexus_save_dialog.dataset_uuids = [dataset_id]
-        nexus_save_dialog.datasetNames.currentIndex = mock.Mock(return_value=0)
+        self.nexus_save_dialog.dataset_uuids = [dataset_id]
+        self.nexus_save_dialog.datasetNames.currentIndex = mock.Mock(return_value=0)
 
-        nexus_save_dialog.save()
+        self.nexus_save_dialog.save()
         parent.execute_nexus_save.assert_called_once()
+
+    def test_save_path(self):
+        save_path = "a/save/path"
+        self.nexus_save_dialog.savePath.text = mock.Mock(return_value=save_path)
+        self.assertEqual(save_path, self.nexus_save_dialog.save_path())
