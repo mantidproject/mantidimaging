201240e2e59f63c3d9b72e69bd7c535445d2eaf7
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-04-04 16:05:29 +0100

    eyes test

---
+++ mantidimaging/eyes_tests/nexus_save_dialog_test.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2022 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+
+from collections import namedtuple
+from unittest import mock
+
+from mantidimaging.eyes_tests.base_eyes import BaseEyesTest
+
+
+class NexusSaveDialogTest(BaseEyesTest):
+    def test_save_dialog_opens_with_dataset(self):
+        TestTuple = namedtuple('TestTuple', ['id', 'name'])
+        dataset_list = [TestTuple('', 'Test Dataset')]
+        with mock.patch("mantidimaging.gui.windows.main.MainWindowView.strict_dataset_list",
+                        new_callable=mock.PropertyMock) as mock_dataset_list:
+            mock_dataset_list.return_value = dataset_list
+            self.imaging.actionSaveNexus.trigger()
+
+        self.check_target(widget=self.imaging.image_save_dialog)
