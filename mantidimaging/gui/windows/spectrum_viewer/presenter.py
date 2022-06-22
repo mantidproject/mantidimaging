eb8a0ebbeec1fa260f5e3351e7aec5a2a2737692
Sam Tygier <sam.tygier@stfc.ac.uk>
2022-06-22 14:59:56 +0100

    SpectrumViewerWindow: Show selected sample
    First go at showing the sample and spectrum

--- mantidimaging/gui/windows/spectrum_viewer/presenter.py
+++ mantidimaging/gui/windows/spectrum_viewer/presenter.py
@@ -36,5 +36,17 @@ class SpectrumViewerWindowPresenter(BasePresenter):
                 elif new_dataset.flat_after is not None:
                     self.view.try_to_select_relevant_normalise_stack(new_dataset.flat_after.name)
 
+        if uuid is not None:
+            self.show_new_sample(uuid)
+
     def get_dataset_id_for_stack(self, stack_id: Optional['UUID']) -> Optional['UUID']:
         return None if stack_id is None else self.main_window.get_dataset_id_from_stack_uuid(stack_id)
+
+    def show_new_sample(self, uuid: 'UUID'):
+        stack = self.main_window.get_stack(uuid)
+
+        stack_averaged = stack.data.mean(axis=0)
+        stack_spectrum = stack.data.mean(axis=(1, 2))
+
+        self.view.spectrum.image.setImage(stack_averaged)
+        self.view.spectrum.spectrum.plot(stack_spectrum, clear=True)
