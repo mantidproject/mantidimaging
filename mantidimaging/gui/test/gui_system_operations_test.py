65a0b3401789b42c71d487c8923e5a8cba918fa1
Rachel Baust <rachel.baust@stfc.ac.uk>
2022-10-10 09:17:51 +0100

    Add test for ROI buttons disabling after click

--- mantidimaging/gui/test/gui_system_operations_test.py
+++ mantidimaging/gui/test/gui_system_operations_test.py
@@ -8,7 +8,7 @@ from uuid import UUID
 from parameterized import parameterized
 from PyQt5.QtTest import QTest
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtWidgets import QFormLayout, QLabel, QWidget
+from PyQt5.QtWidgets import QFormLayout, QLabel, QWidget, QPushButton
 
 from mantidimaging.gui.windows.stack_choice.presenter import StackChoicePresenter
 from mantidimaging.core.data import ImageStack
@@ -73,6 +73,18 @@ class TestGuiSystemOperations(GuiSystemBase):
                     return widget_item.widget()
 
         raise ValueError(f"Could not find '{param_name}' in form")
+
+    @staticmethod
+    def _get_operation_button_widget(form: QFormLayout, button_name: str) -> QWidget:
+        for i in range(form.rowCount()):
+            widget_item = form.itemAt(i * 2)
+
+            if widget_item is not None:
+                button = widget_item.widget()
+                if isinstance(button, QPushButton) and button.text() == button_name:
+                    return button
+
+        raise ValueError(f"Could not find '{button_name}' in form")
 
     @parameterized.expand(OP_LIST)
     def test_run_operation_stack(self, op_name, params):
@@ -173,6 +185,22 @@ class TestGuiSystemOperations(GuiSystemBase):
         QTest.mouseClick(self.op_window.applyButton, Qt.MouseButton.LeftButton)
         QTest.qWait(SHORT_DELAY)
         wait_until(lambda: self.op_window.presenter.filter_is_running is False, max_retry=600)
+
+        self.main_window.filters.close()
+        QTest.qWait(SHOW_DELAY)
+
+    @parameterized.expand([(OP_LIST[3][0], "Select ROI"), (OP_LIST[13][0], "Select Air Region")])
+    def test_opening_roi_selector_window_disables_button(self, op_name, button_name):
+        QTest.qWait(SHOW_DELAY)
+        index = self.op_window.filterSelector.findText(op_name)
+        self.op_window.filterSelector.setCurrentIndex(index)
+        QTest.qWait(SHOW_DELAY)
+
+        roi_button = self._get_operation_button_widget(self.op_window.filterPropertiesLayout, button_name)
+        self.assertTrue(roi_button.isEnabled())
+        QTest.mouseClick(roi_button, Qt.MouseButton.LeftButton)
+        QTest.qWait(SHOW_DELAY)
+        self.assertFalse(roi_button.isEnabled())
 
         self.main_window.filters.close()
         QTest.qWait(SHOW_DELAY)
