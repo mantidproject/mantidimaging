9ff13d8795b88562c2b8f050756197d8aca8b4a0
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-10-24 11:59:00 +0100

    disabling combo box based on dataset type

--- mantidimaging/gui/windows/add_images_to_dataset_dialog/view.py
+++ mantidimaging/gui/windows/add_images_to_dataset_dialog/view.py
@@ -1,9 +1,14 @@
 # Copyright (C) 2022 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
+from PyQt5.QtWidgets import QComboBox
 
 from mantidimaging.gui.mvp_base import BaseDialogView
 
 
 class AddImagesToDatasetDialog(BaseDialogView):
+    imageTypeComboBox: QComboBox
+
     def __init__(self, parent, strict_dataset: bool):
         super().__init__(parent, 'gui/ui/add_to_dataset.ui')
+
+        self.imageTypeComboBox.setEnabled(strict_dataset)
