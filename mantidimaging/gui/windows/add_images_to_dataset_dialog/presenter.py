a6f9c92c2e741136e9ab2607aee6c1cf2b400e9b
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-10-25 15:58:26 +0100

    calling load stack

--- mantidimaging/gui/windows/add_images_to_dataset_dialog/presenter.py
+++ mantidimaging/gui/windows/add_images_to_dataset_dialog/presenter.py
@@ -1,8 +1,9 @@
 # Copyright (C) 2022 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-import os.path
 from enum import Enum, auto
 from typing import TYPE_CHECKING
+
+from mantidimaging.core.io.loader import load_stack
 
 if TYPE_CHECKING:
     from mantidimaging.gui.windows.add_images_to_dataset_dialog.view import AddImagesToDatasetDialog
@@ -18,8 +19,6 @@ class AddImagesToDatasetPresenter:
     def __init__(self, view: 'AddImagesToDatasetDialog'):
         self.view = view
 
-    def find_images(self):
+    def load_images(self):
         image_path = self.view.path
-        directory = os.path.dirname(image_path)
-        print(image_path)
-        print(directory)
+        return load_stack(image_path)
