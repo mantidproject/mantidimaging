319ed03a04b605a6cd308049c36eaa39681ab5a2
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-11-22 15:56:17 +0000

    connecting dataset selection and available types

--- mantidimaging/gui/windows/move_stack_dialog/view.py
+++ mantidimaging/gui/windows/move_stack_dialog/view.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2022 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 import uuid
-from typing import Dict, NamedTuple
+from typing import Dict
 
 from PyQt5.QtWidgets import QLabel, QComboBox
 
@@ -18,33 +18,36 @@ class MoveStackDialog(BaseDialogView):
     destinationTypeComboBox: QComboBox
 
     def __init__(self, parent, origin_dataset_id: uuid.UUID, origin_dataset_name: str, origin_data_type: str,
-                 destination_datasets: Dict[NamedTuple]):
+                 dataset_is_strict: Dict[bool]):
         super().__init__(parent, 'gui/ui/move_stack_dialog.ui')
 
         self.originDatasetName.setText(origin_dataset_name)
         self.originDataType.setText(origin_data_type)
         self._origin_dataset_id = origin_dataset_id
-        self.destination_dataset_info = destination_datasets
+        self.destination_dataset_info = dataset_is_strict
 
         self.parent_view = parent
         self.presenter = MoveStackPresenter(self)
 
-        self.destinationNameComboBox.addItems([item.name for item in destination_datasets.values()])
-        self._destination_dataset_is_strict = destination_datasets[self.destinationTypeComboBox.currentText()].is_strict
+        self.destinationNameComboBox.addItems([name for name in dataset_is_strict.keys()])
+        self._destination_dataset_is_strict = dataset_is_strict[self.destinationNameComboBox.currentText()]
+        self._create_destination_type_options(self._destination_dataset_is_strict)
 
-    def _on_accepted(self):
-        self.presenter.notify(Notification.ACCEPTED)
-
-    def _on_destination_dataset_change(self):
-        selected_dataset_is_strict = self.destination_dataset_info[self.destinationTypeComboBox.currentText()].is_strict
-        if selected_dataset_is_strict == self._destination_dataset_is_strict:
-            return
-
-        self.destinationTypeComboBox.clear()
-        if selected_dataset_is_strict:
+    def _create_destination_type_options(self, destination_dataset_is_strict: bool):
+        if destination_dataset_is_strict:
             self.destinationTypeComboBox.addItems(
                 ["Sample", "Flat Before", "Flat After", "Dark Before", "Dark After", "Recon"])
         else:
             self.destinationTypeComboBox.addItems(["Images", "Recon"])
 
+    def _on_accepted(self):
+        self.presenter.notify(Notification.ACCEPTED)
+
+    def _on_destination_dataset_change(self):
+        selected_dataset_is_strict = self.destination_dataset_info[self.destinationNameComboBox.currentText()]
+        if selected_dataset_is_strict == self._destination_dataset_is_strict:
+            return
+
+        self.destinationTypeComboBox.clear()
+        self._create_destination_type_options(selected_dataset_is_strict)
         self._destination_dataset_is_strict = selected_dataset_is_strict
