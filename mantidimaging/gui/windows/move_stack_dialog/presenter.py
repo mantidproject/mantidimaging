2c2cd862b9e5004292dbd9f9fe1167cfae52153d
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-11-23 14:29:35 +0000

    on accepted

--- mantidimaging/gui/windows/move_stack_dialog/presenter.py
+++ mantidimaging/gui/windows/move_stack_dialog/presenter.py
@@ -21,6 +21,14 @@ class MoveStackPresenter(BasePresenter):
     def notify(self, n: Notification):
         try:
             if n == Notification.ACCEPTED:
-                pass
+                self._on_accepted()
         except RuntimeError as err:
             self.view.show_exception(str(err), traceback.format_exc())
+
+    def _on_accepted(self):
+        origin_dataset_id = self.view.origin_dataset_id
+        stack_id = self.view.stack_id
+        destination_data_type = self.view.destination_data_type
+        destination_dataset_name = self.view.destination_dataset_name
+        self.view.parent_view.presenter.execute_move_stack(origin_dataset_id, stack_id, destination_data_type,
+                                                           destination_dataset_name)
