f3476737afe26b4a0a3c8143bac28ec283ee53d9
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2022-12-12 17:10:23 +0000

    refactoring view and tests

--- mantidimaging/gui/windows/move_stack_dialog/test/view_test.py
+++ mantidimaging/gui/windows/move_stack_dialog/test/view_test.py
@@ -7,7 +7,7 @@ from unittest import mock
 from mantidimaging.gui.windows.main import MainWindowView
 from mantidimaging.gui.windows.move_stack_dialog.presenter import Notification
 
-from mantidimaging.gui.windows.move_stack_dialog.view import MoveStackDialog, STRICT_DATASET_ATTRS
+from mantidimaging.gui.windows.move_stack_dialog.view import MoveStackDialog
 from mantidimaging.test_helpers import start_qapplication
 
 
@@ -30,52 +30,13 @@ class MoveStackDialogTest(unittest.TestCase):
             self.origin_dataset_name: True
         }
         self.view = MoveStackDialog(self.main_window, self.origin_dataset_id, self.stack_id, self.origin_dataset_name,
-                                    self.origin_data_type, self.is_dataset_strict)
+                                    self.origin_data_type)
         self.view.presenter = self.presenter = mock.Mock()
+        self.view.dataset_selector = self.dataset_selector = mock.Mock()
 
     def test_origin_stack_information_matched(self):
         assert self.origin_dataset_name == self.view.originDatasetName.text()
         assert self.origin_data_type == self.view.originDataType.text()
-
-    def test_combo_box_contains_dataset_names(self):
-        assert self.view.destinationNameComboBox.count() == len(self.is_dataset_strict)
-        index = 0
-        for key in self.is_dataset_strict.keys():
-            assert key == self.view.destinationNameComboBox.itemText(index)
-            index += 1
-
-    def test_destination_dataset_strict_options(self):
-        """
-        When the destination dataset is strict, the user should be able to have a stack moved to it become a Flat
-        Before, Flat After, etc
-        """
-        self.view.destinationNameComboBox.setCurrentText(self.strict_dataset_name)
-        destination_stack_type_options = [
-            self.view.destinationTypeComboBox.itemText(i) for i in range(self.view.destinationTypeComboBox.count())
-        ]
-        self.assertListEqual(destination_stack_type_options, STRICT_DATASET_ATTRS)
-
-    def test_destination_dataset_mixed_options(self):
-        """
-        When the dataset is mixed, the user should be able to have a stack moved to it become an Images stack or a recon
-        only
-        """
-        self.view.destinationNameComboBox.setCurrentText(self.mixed_dataset_name)
-        destination_stack_type_options = [
-            self.view.destinationTypeComboBox.itemText(i) for i in range(self.view.destinationTypeComboBox.count())
-        ]
-        self.assertListEqual(destination_stack_type_options, ["Images", "Recon"])
-
-    def test_moved_stack_within_dataset_options(self):
-        """
-        When a stack is moved to the same dataset, it should not be possible to move it to the same data type
-        (i.e. a Flat Before can become a Recon but moving it to Flat Before should not be possible)
-        """
-        self.view.destinationNameComboBox.setCurrentText(self.origin_dataset_name)
-        destination_stack_type_options = [
-            self.view.destinationTypeComboBox.itemText(i) for i in range(self.view.destinationTypeComboBox.count())
-        ]
-        self.assertNotIn(self.origin_data_type, destination_stack_type_options)
 
     def test_accept(self):
         self.view.close = mock.Mock()
@@ -83,12 +44,12 @@ class MoveStackDialogTest(unittest.TestCase):
         self.presenter.notify.assert_called_once_with(Notification.ACCEPTED)
         self.view.close.assert_called_once()
 
+    def test_on_destination_dataset_changed(self):
+        self.view._on_destination_dataset_changed()
+        self.presenter.notify.assert_called_once_with(Notification.DATASET_CHANGED)
+
     def test_origin_dataset_id(self):
         assert self.view.origin_dataset_id == self.origin_dataset_id
 
     def test_stack_id(self):
         assert self.view.stack_id == self.stack_id
-
-    def test_destination_stack_type(self):
-        self.view.destinationNameComboBox.setCurrentText(self.strict_dataset_name)
-        assert self.view.destination_dataset_name == self.strict_dataset_name
--- mantidimaging/gui/windows/move_stack_dialog/view.py
+++ mantidimaging/gui/windows/move_stack_dialog/view.py
@@ -1,15 +1,12 @@
 # Copyright (C) 2022 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 import uuid
-from typing import Dict
 
 from PyQt5.QtWidgets import QLabel, QComboBox
 from mantidimaging.gui.widgets.dataset_selector import DatasetSelectorWidgetView
 
 from mantidimaging.gui.mvp_base import BaseDialogView
 from mantidimaging.gui.windows.move_stack_dialog.presenter import MoveStackPresenter, Notification
-
-STRICT_DATASET_ATTRS = ["Sample", "Flat Before", "Flat After", "Dark Before", "Dark After", "Recon"]
 
 
 class MoveStackDialog(BaseDialogView):
@@ -20,54 +17,29 @@ class MoveStackDialog(BaseDialogView):
     destinationTypeComboBox: QComboBox
 
     def __init__(self, parent, origin_dataset_id: uuid.UUID, stack_id: uuid.UUID, origin_dataset_name: str,
-                 origin_data_type: str, is_dataset_strict: Dict[str, bool]):
+                 origin_data_type: str):
         super().__init__(parent, 'gui/ui/move_stack_dialog.ui')
 
         self.originDatasetName.setText(origin_dataset_name)
         self.originDataType.setText(origin_data_type)
         self._origin_dataset_id = origin_dataset_id
-        self.destination_dataset_info = is_dataset_strict
         self._stack_id = stack_id
 
         self.parent_view = parent
         self.presenter = MoveStackPresenter(self)
 
         self.datasetSelector = DatasetSelectorWidgetView(self)
-        self.datasetSelector.subscribe_to_main_window(parent)
         self.layout().addWidget(self.datasetSelector, 3, 1)
-        # self.destinationNameComboBox.addItems([name for name in is_dataset_strict.keys()])
-        # self._destination_dataset_is_strict = is_dataset_strict[self.destinationNameComboBox.currentText()]
 
-        # self.destinationNameComboBox.currentIndexChanged.connect(self._on_destination_dataset_changed)
-        # self._create_destination_type_options(self._destination_dataset_is_strict)
-
-    def _create_destination_type_options(self, destination_dataset_is_strict: bool):
-        """
-        Create optional data types for the destination depending on where the destination dataset is strict or mixed.
-        :param destination_dataset_is_strict: Whether the destination dataset is strict.
-        """
-        if destination_dataset_is_strict:
-            if self.destinationNameComboBox.currentText() == self.originDatasetName.text():
-                same_ds_list = STRICT_DATASET_ATTRS.copy()
-                same_ds_list.remove(self.originDataType.text())
-                self.destinationTypeComboBox.addItems(same_ds_list)
-            else:
-                self.destinationTypeComboBox.addItems(STRICT_DATASET_ATTRS)
-        else:
-            self.destinationTypeComboBox.addItems(["Images", "Recon"])
+        self.datasetSelector.currentIndexChanged.connect(self._on_destination_dataset_changed)
+        self.datasetSelector.subscribe_to_main_window(parent)
 
     def accept(self):
         self.presenter.notify(Notification.ACCEPTED)
         self.close()
 
     def _on_destination_dataset_changed(self):
-        """
-        Check if the new dataset selection is strict or mixed and change the data type accordingly.
-        """
-        selected_dataset_is_strict = self.destination_dataset_info[self.destinationNameComboBox.currentText()]
-        self.destinationTypeComboBox.clear()
-        self._create_destination_type_options(selected_dataset_is_strict)
-        self._destination_dataset_is_strict = selected_dataset_is_strict
+        self.presenter.notify(Notification.DATASET_CHANGED)
 
     @property
     def origin_dataset_id(self) -> uuid.UUID:
@@ -80,7 +52,3 @@ class MoveStackDialog(BaseDialogView):
     @property
     def destination_stack_type(self) -> str:
         return self.destinationTypeComboBox.currentText()
-
-    @property
-    def destination_dataset_name(self) -> str:
-        return self.destinationNameComboBox.currentText()
