1f74b58bc1b2d58d0654438a9c83420e14fd6152
Sam Tygier <sam.tygier@stfc.ac.uk>
2022-12-13 16:13:58 +0000

    CILRecon: approximate norm for ProjectionOperator
    Add a check that runs when debugging

--- mantidimaging/core/reconstruct/cil_recon.py
+++ mantidimaging/core/reconstruct/cil_recon.py
@@ -2,7 +2,7 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 
 import time
-from logging import getLogger
+from logging import getLogger, DEBUG
 from math import sqrt
 from threading import Lock
 from typing import List, Optional
@@ -34,6 +34,17 @@ class CILRecon(BaseRecon):
                                  recon_params: ReconstructionParameters):
         # Forward operator
         A2d = ProjectionOperator(image_geometry, acquisition_data.geometry, 'gpu')
+
+        # This is slow to calculate, this approximation is good to with in 5%
+        # When running in debug mode, check the approximation and raise an error if it is bad
+        approx_a2d_norm = sqrt(image_geometry.voxel_num_x * acquisition_data.geometry.num_projections)
+        if LOG.isEnabledFor(DEBUG):
+            num_a2d_norm = A2d.norm()
+            diff = abs(approx_a2d_norm - num_a2d_norm) / max(approx_a2d_norm, num_a2d_norm)
+            LOG.debug(f"ProjectionOperator approx norm: {diff=} {approx_a2d_norm=} {num_a2d_norm=}")
+            if diff > 0.05:
+                raise RuntimeError(f"Bad ProjectionOperator norm: {diff=} {approx_a2d_norm=} {num_a2d_norm=}\n")
+        A2d.set_norm(approx_a2d_norm)
 
         # Define Gradient Operator and BlockOperator
         alpha = recon_params.alpha
