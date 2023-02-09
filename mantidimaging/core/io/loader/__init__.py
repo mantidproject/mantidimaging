fbc1b5907d831360cbd56939c484770a6fd0c57c
Sam Tygier <sam.tygier@stfc.ac.uk>
2023-02-09 16:23:42 +0000

    Remove unused loader.oad_stack

--- mantidimaging/core/io/loader/__init__.py
+++ mantidimaging/core/io/loader/__init__.py
@@ -3,4 +3,4 @@
 from __future__ import annotations
 
 from .loader import (  # noqa: F401
-    load, load_stack, load_p, load_log, read_in_file_information, supported_formats, load_stack_from_group)
+    load, load_p, load_log, read_in_file_information, supported_formats, load_stack_from_group)
