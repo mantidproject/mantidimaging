44050cf8174d506810bbf9fc9ded79c61e17a48f
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2023-03-10 11:00:16 +0000

    test fix

--- mantidimaging/core/io/test/io_test.py
+++ mantidimaging/core/io/test/io_test.py
@@ -340,7 +340,8 @@ class IOTest(FileOutputtingTestCase):
             assert "process" in nexus_file["processed-data"]
             self.assertEqual(_decode_nexus_class(nexus_file[process_path]), "NXprocess")
             self.assertEqual(_nexus_dataset_to_string(nexus_file[process_path]["program"]), "Mantid Imaging")
-            self.assertEqual(_nexus_dataset_to_string(nexus_file[process_path]["version"]), CheckVersion.get_version())
+            self.assertEqual(_nexus_dataset_to_string(nexus_file[process_path]["version"]),
+                             CheckVersion().get_version())
             self.assertIn(str(datetime.date.today()), _nexus_dataset_to_string(nexus_file[process_path]["date"]))
 
     @mock.patch("mantidimaging.core.io.saver._save_recon_to_nexus")
