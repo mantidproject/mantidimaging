ec6a85e7d3bc927788ef2cb00e78e3f5b3d2d1e9
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2023-03-21 17:27:07 +0000

    shared variable in utility file

--- mantidimaging/core/io/saver.py
+++ mantidimaging/core/io/saver.py
@@ -13,7 +13,7 @@ from tifffile import tifffile
 from mantidimaging.core.operation_history.const import TIMESTAMP
 import astropy.io.fits as fits
 
-from .utility import DEFAULT_IO_FILE_FORMAT
+from .utility import DEFAULT_IO_FILE_FORMAT, NEXUS_PROCESSED_DATA_PATH
 from ..operations.rescale import RescaleFilter
 from ..utility.progress_reporting import Progress
 from ..utility.version_check import CheckVersion
@@ -247,7 +247,7 @@ def _nexus_save(nexus_file: h5py.File, dataset: StrictDataset, sample_name: str)
 
 def _save_processed_data_to_nexus(nexus_file: h5py.File, dataset: StrictDataset, rotation_angle: h5py.Dataset,
                                   image_key: h5py.Dataset):
-    data = nexus_file.create_group("processed-data")
+    data = nexus_file.create_group(NEXUS_PROCESSED_DATA_PATH)
     data["rotation_angle"] = rotation_angle
     data["image_key"] = image_key
     _set_nx_class(data, "NXdata")
