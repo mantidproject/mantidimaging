2fedd4ccb518442456683b54fb4a53fbb6633202
Sam Tygier <sam.tygier@stfc.ac.uk>
2023-03-30 09:52:12 +0100

    BaseReconTest parameterized test

--- mantidimaging/core/reconstruct/test/base_recon_test.py
+++ mantidimaging/core/reconstruct/test/base_recon_test.py
@@ -6,41 +6,26 @@ import unittest
 from unittest import mock
 
 import numpy as np
+from parameterized import parameterized
 
 from mantidimaging.core.reconstruct.base_recon import BaseRecon
 from mantidimaging.core.utility.data_containers import ReconstructionParameters
 
 
 class BaseReconTest(unittest.TestCase):
-    def test_prepare_recon_no_bhc(self):
-        data = np.ones([10, 10], dtype=np.float32)
+    @parameterized.expand([
+        (np.float32, None, 1, 0),
+        (np.float32, [1, 1, 1, 1], 2, -0.474886),
+        (np.float64, [1, 1, 1, 1], 2, -0.474886),
+        (np.float32, [1, 2, 3, 4], 2, -0.826249),
+    ])
+    def test_prepare_recon_bhc(self, dtype, coefs, input, output):
+        data = np.zeros([10, 10], dtype=dtype) + input
         recon_params = mock.create_autospec(ReconstructionParameters)
-        recon_params.beam_hardening_coefs = None
+        recon_params.beam_hardening_coefs = coefs
 
         result = BaseRecon.prepare_sinogram(data, recon_params)
 
         self.assertEqual(data.shape, result.shape)
         self.assertEqual(data.dtype, result.dtype)
-        self.assertEqual(0, result[0, 0])  # -log(1) == 0
-
-    def test_prepare_recon_bhc(self):
-        data = np.ones([10, 10], dtype=np.float32) * 2
-        recon_params = mock.create_autospec(ReconstructionParameters)
-        recon_params.beam_hardening_coefs = [1, 1, 1, 1]
-
-        result = BaseRecon.prepare_sinogram(data, recon_params)
-
-        self.assertEqual(data.shape, result.shape)
-        self.assertEqual(data.dtype, result.dtype)
-        self.assertAlmostEqual(-0.474886, result[0, 0], 4)
-
-    def test_prepare_recon_bhc_f64(self):
-        data = np.ones([10, 10], dtype=np.float64) * 2
-        recon_params = mock.create_autospec(ReconstructionParameters)
-        recon_params.beam_hardening_coefs = [1, 1, 1, 1]
-
-        result = BaseRecon.prepare_sinogram(data, recon_params)
-
-        self.assertEqual(data.shape, result.shape)
-        self.assertEqual(data.dtype, result.dtype)
-        self.assertAlmostEqual(-0.474886, result[0, 0], 4)
+        self.assertAlmostEqual(output, result[0, 0], 4)
