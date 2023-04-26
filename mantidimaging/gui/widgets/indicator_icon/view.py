68a8b3479eb17cfe6ee16e572d0d7c8cb40ab615
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2023-04-26 14:41:20 +0100

    returns for indicator icon view

--- mantidimaging/gui/widgets/indicator_icon/view.py
+++ mantidimaging/gui/widgets/indicator_icon/view.py
@@ -11,7 +11,7 @@ from PyQt5.QtWidgets import QGraphicsPixmapItem, QGraphicsSimpleTextItem, QMenu,
 from PyQt5.QtGui import QPixmap, QImage, QColor
 
 
-class IndicatorIconView(QGraphicsPixmapItem):
+class IndicatorIconView(QGraphicsPixmapItem):  # type: ignore
     def __init__(self,
                  parent: ViewBox,
                  icon_path: str,
@@ -49,7 +49,7 @@ class IndicatorIconView(QGraphicsPixmapItem):
 
         self.actions: List[QAction] = []
 
-    def set_icon(self, icon_path: str, color: Optional[List[int]] = None):
+    def set_icon(self, icon_path: str, color: Optional[List[int]] = None) -> None:
         if color is not None:
             im = Image.open(icon_path)
             image_data = np.array(im)
@@ -70,7 +70,7 @@ class IndicatorIconView(QGraphicsPixmapItem):
             image_pm = QPixmap(icon_path)
         self.setPixmap(image_pm)
 
-    def position_icon(self):
+    def position_icon(self) -> None:
         # The size of the imageview we are putting the icon ing
         scene_size = self.parent.size()
         # The position of the image within the scene
@@ -86,23 +86,23 @@ class IndicatorIconView(QGraphicsPixmapItem):
         label_width = self.label.boundingRect().width()
         self.label.setPos(corner_pos_x - label_width, icon_pos_y - self.icon_size[0])
 
-    def hoverEnterEvent(self, event):
+    def hoverEnterEvent(self, event) -> None:
         if self.connected_overlay is not None:
             self.connected_overlay.setVisible(True)
         self.label.setVisible(True)
 
-    def hoverLeaveEvent(self, event):
+    def hoverLeaveEvent(self, event) -> None:
         if self.connected_overlay is not None:
             self.connected_overlay.setVisible(False)
         self.label.setVisible(False)
 
-    def add_actions(self, actions: List[Tuple[str, Callable]]):
+    def add_actions(self, actions: List[Tuple[str, Callable]]) -> None:
         for text, method in actions:
             action = QAction(text)
             action.triggered.connect(method)
             self.actions.append(action)
 
-    def mouseClickEvent(self, event):
+    def mouseClickEvent(self, event) -> None:
         event.accept()
         if self.actions:
             qm = QMenu()
