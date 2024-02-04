5bfd1f5d5d09309360ac9149a58cc37e3e3d1c00
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-02-04 23:09:31 +0000

    Add key events for rotaty encoder

--- mantidimaging/gui/windows/main/view.py
+++ mantidimaging/gui/windows/main/view.py
@@ -14,7 +14,7 @@ import numpy as np
 from PyQt5.QtCore import Qt, pyqtSignal, QUrl, QPoint
 from PyQt5.QtGui import QIcon, QDragEnterEvent, QDropEvent, QDesktopServices
 from PyQt5.QtWidgets import QAction, QDialog, QLabel, QMessageBox, QMenu, QFileDialog, QSplitter, \
-    QTreeWidgetItem, QTreeWidget
+    QTreeWidgetItem, QTreeWidget, QDockWidget
 
 from mantidimaging.core.data import ImageStack
 from mantidimaging.core.data.dataset import StrictDataset
@@ -179,6 +179,27 @@ class MainWindowView(BaseMainWindowView):
         if perf_logger.isEnabledFor(1):
             perf_logger.info(f"Mantid Imaging ready in {time.monotonic() - process_start_time}")
         super()._window_ready()
+
+    def keyPressEvent(self, e):
+        # print(e.key(), e.modifiers())
+        if e.modifiers() & Qt.ControlModifier:
+            if e.key() == Qt.Key_F1:
+                #print("move CCW")
+                self.rotate(1)
+            if e.key() == Qt.Key_F2:
+                #print("move CW")
+                self.rotate(-1)
+
+    def rotate(self, steps: int):
+        ws = self.findChildren(QDockWidget)
+        for w in ws:
+            if not w.visibleRegion().isEmpty():
+                image_count = w.presenter.get_num_images()
+                step_size = max(1, image_count // 100)
+                current_image = w.image_view.currentIndex
+                #print(f"{image_count=} {step_size=} {current_image=}")
+                new_image = (current_image + step_size * steps) % image_count
+                w.image_view.set_selected_image(new_image)
 
     def setup_shortcuts(self):
         self.actionLoadDataset.triggered.connect(self.show_image_load_dialog)
