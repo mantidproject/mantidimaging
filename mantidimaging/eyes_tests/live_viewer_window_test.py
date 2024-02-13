8ca51efc6c26a2a8b1ce615bc4113abc9c9eb880
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-02-13 17:23:39 +0000

    Test for live view with data
    Co-authored-by: JackEAllen <jack.allen@stfc.ac.uk>
    Co-authored-by: Mike Sullivan <michael.sullivan@stfc.ac.uk>
    Co-authored-by: Ashley Meigh <ashleymeigh0@gmail.com>

--- mantidimaging/eyes_tests/live_viewer_window_test.py
+++ mantidimaging/eyes_tests/live_viewer_window_test.py
@@ -2,7 +2,12 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 from __future__ import annotations
 
+from unittest import mock
+
+import numpy as np
+
 from mantidimaging.core.operations.loader import load_filter_packages
+from mantidimaging.gui.windows.live_viewer.model import Image_Data
 from mantidimaging.test_helpers.unit_test_helper import FakeFSTestCase
 from pathlib import Path
 
@@ -17,7 +22,33 @@ class LiveViewerWindowTest(FakeFSTestCase, BaseEyesTest):
         super().setUp()
         self.fs.add_real_directory(Path(__file__).parent.parent)
 
+    def _generate_image(self):
+        image = np.zeros((10, 10))
+        image[5, :] = np.arange(10)
+        return image
+
+    def _make_simple_dir(self, directory: Path):
+        file_list = [directory / f"abc_{i:06d}.tif" for i in range(5)]
+        if not directory.exists():
+            self.fs.create_dir(directory)
+
+        for file in file_list:
+            self.fs.create_file(file)
+
+        return file_list
+
     def test_live_view_opens_without_data(self):
         self.fs.create_dir("/live_dir")
         self.imaging.show_live_viewer(Path("/live_dir"))
+        self.check_target(widget=self.imaging.live_viewer)
+
+    @mock.patch('mantidimaging.gui.windows.live_viewer.presenter.LiveViewerWindowPresenter.load_image')
+    @mock.patch('mantidimaging.gui.windows.live_viewer.model.ImageWatcher')
+    def test_live_view_opens_with_data(self, _mock_image_watcher, mock_load_image):
+        self.fs.create_dir("/live_dir")
+        file_list = self._make_simple_dir(Path("/live_dir"))
+        image_list = [Image_Data(path) for path in file_list]
+        mock_load_image.return_value = self._generate_image()
+        self.imaging.show_live_viewer(Path("/live_dir"))
+        self.imaging.live_viewer.presenter.model._handle_image_changed_in_list(image_list)
         self.check_target(widget=self.imaging.live_viewer)
