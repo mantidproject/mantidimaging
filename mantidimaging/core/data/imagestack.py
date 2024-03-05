0156d5a08eef3e02d325747f44a48409be1f13d4
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-03-05 10:21:36 +0000

    Typing: Use npt.DTypeLike for arguments

--- mantidimaging/core/data/imagestack.py
+++ mantidimaging/core/data/imagestack.py
@@ -20,6 +20,7 @@ from mantidimaging.core.utility.leak_tracker import leak_tracker
 
 if TYPE_CHECKING:
     from mantidimaging.core.io.instrument_log import InstrumentLog
+    import numpy.typing as npt
 
 
 class ImageStack:
@@ -274,11 +275,11 @@ class ImageStack:
         return self._shared_array.has_shared_memory
 
     @property
-    def dtype(self) -> np.typing.DTypeLike:
+    def dtype(self) -> np.dtype:
         return self.data.dtype
 
     @staticmethod
-    def create_empty_image_stack(shape: tuple[int, ...], dtype: np.dtype, metadata: dict[str, Any]) -> 'ImageStack':
+    def create_empty_image_stack(shape: tuple[int, ...], dtype: npt.DTypeLike, metadata: dict[str, Any]) -> ImageStack:
         arr = pu.create_array(shape, dtype)
         return ImageStack(arr, metadata=metadata)
 
