c2e33f5b8a50653c31e86a1744da0027c56b2db5
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2024-03-22 16:27:07 +0000

    Mostly working - switching to Image Number units needs fixing

---
+++ mantidimaging/core/utility/unit_conversion.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2024 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+from __future__ import annotations
+
+import numpy as np
+from typing import TYPE_CHECKING
+
+
+class UnitConversion:
+    # target_to_camera_dist = 56 m taken from https://scripts.iucr.org/cgi-bin/paper?S1600576719001730
+    neutron_mass = 1.674927211e-27  # [kg]
+    planck_h = 6.62606896e-34  # [JHz-1]
+    angstrom = 1e-10  # [m]
+    mega_electro_volt = 1.60217662e-19 / 1e6
+
+    def __init__(self, data_to_convert: np.ndarray, target_to_camera_dist: float = 56) -> None:
+        self.tof_data_to_convert = data_to_convert
+        self.target_to_camera_dist = target_to_camera_dist
+        self.velocity = self.target_to_camera_dist / self.tof_data_to_convert
+
+    def tof_seconds_to_wavelength(self) -> np.ndarray:
+        wavelength = self.planck_h / (self.neutron_mass * self.velocity)
+        wavelength_angstroms = wavelength / self.angstrom
+        return wavelength_angstroms
+
+    def tof_seconds_to_energy(self) -> np.ndarray:
+        energy = self.neutron_mass * self.velocity / 2
+        energy_evs = energy / self.mega_electro_volt
+        return energy_evs
+
