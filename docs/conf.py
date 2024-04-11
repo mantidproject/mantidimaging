1df405c11f706c3ebd144382204d8bf3070959f3
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-04-11 17:02:59 +0100

    Add version switcher

--- docs/conf.py
+++ docs/conf.py
@@ -110,7 +110,13 @@ html_sidebars = {'**': ['localtoc.html', 'relations.html', 'searchbox.html']}
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
+html_theme_options = {
+    "switcher": {
+        "json_url": "/mantidimaging/_static/version_switcher.json",
+        "version_match": release,
+    },
+    "navbar_end": ["version-switcher"],
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
