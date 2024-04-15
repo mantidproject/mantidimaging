dc4cf2fc5b6adb6cfa3174a62d3fa16c67aafd64
ashmeigh <ashleymeigh0@gmail.com>
2024-04-15 15:00:13 +0100

    Use the TestRunner class to simplify the main function, clearly separating configuration and execution phases.

--- scripts/operations_tests/operations_tests.py
+++ scripts/operations_tests/operations_tests.py
@@ -10,22 +10,12 @@ import sys
 import time
 from collections import defaultdict
 from dataclasses import dataclass
-from datetime import datetime
 from functools import cached_property
 from pathlib import Path
 from statistics import stdev
 from collections.abc import Callable
 
 import numpy as np
-import pandas as pd
-
-try:
-    from plotly import graph_objs as go
-    from plotly.subplots import make_subplots
-except ModuleNotFoundError:
-    print("Approval tests require plotly")
-    print("Try: mamba install plotly")
-    exit(1)
 
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 from mantidimaging.core.io.filenames import FilenameGroup  # noqa: E402
@@ -88,233 +78,215 @@ def process_params(param):
     return param
 
 
-def compare_mode():
-    for operation, test_case_info in TEST_CASES.items():
-        print(f"Running tests for {operation}:")
-        cases = test_case_info["cases"]
-        for test_number, case in enumerate(cases):
-            sub_test_name = case["test_name"]
-            test_name = f"{operation.lower()}_{sub_test_name}"
-            if args.match and args.match not in test_name:
-                continue
-            params = test_case_info["params"] | case["params"]
-            params = {k: process_params(v) for k, v in params.items()}
-            op_class = FILTERS[operation]
-            op_func = op_class.filter_func
-            test_case = TestCase(operation, test_name, sub_test_name, test_number, params, op_func)
-            run_test(test_case)
-        print("\n")
+class TestRunner:
 
-    print_compare_mode_results()
+    def __init__(self, args):
+        self.args = args
 
+    def configure(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-m",
+                            "--mode",
+                            type=str,
+                            choices=["compare", "time"],
+                            help="what mode to run in (compare or time)")
+        parser.add_argument("-r", "--runs", type=int, default=5, help="number of times to run each test case")
+        parser.add_argument("-v", "--verbose", action="store_true", help="print verbose output")
+        parser.add_argument("-g", "--graphs", action="store_true", help="print verbose output")
+        parser.add_argument("-k",
+                            dest="match",
+                            type=str,
+                            help="only run tests which match the given substring expression")
+        parser.add_argument("--gui", dest="gui", action="store_true", help="Show GUI comparison for differences")
 
-def print_compare_mode_results():
-    print(f"{'=' * 40}RESULTS{'=' * 40}")
-    failures = 0
-    passes = 0
-    new_baselines = 0
+        self.args = parser.parse_args()
 
-    for test_case in TEST_CASE_RESULTS:
-        if test_case.status == "fail":
-            failures += 1
-            print(f"[FAIL] {test_case.operation} test #{test_case.test_number:03d}"
-                  f", {test_case.test_name} -> {test_case.message}\n")
-        elif test_case.status == "pass":
-            passes += 1
-            if args.verbose:
-                print(f"[PASS] {test_case.operation} test #{test_case.test_number:03d},"
-                      f"{test_case.test_name} -> {test_case.duration}s\n")
-        elif test_case.status == "new baseline":
-            new_baselines += 1
-            if args.verbose:
-                print(f"[NEW] {test_case.operation} test #{test_case.test_number:03d}, {test_case.test_name}\n")
+    def run_tests(self):
+        if self.args.mode == "time":
+            self.time_mode(self.args.runs)
 
-    print(f"{failures} failed\n{passes} passed\n{new_baselines} new baseline(s)")
-    print(f"{'=' * 42}END{'=' * 42}")
+        if self.args.mode == "compare":
+            self.compare_mode()
 
+        if self.args.graphs:
+            self.create_plots()
 
-def time_mode(runs):
-    durations = defaultdict(list)
-    image_stack = load_image_stack()
-    for operation, test_case_info in TEST_CASES.items():
-        print(f"Running tests for {operation}:")
-        cases = test_case_info["cases"]
-        for case in cases:
-            sub_test_name = case["test_name"]
-            test_name = f"{operation.lower()}_{sub_test_name}"
-            params = case["params"] | test_case_info["params"]
-            op_func = FILTERS[operation].filter_func
-            for _ in range(runs):
-                image_stack2 = image_stack.copy()
-                duration = time_operation(image_stack2, op_func, params)[0]
-                durations[test_name].append(duration)
+    def compare_mode(self):
+        for operation, test_case_info in TEST_CASES.items():
+            print(f"Running tests for {operation}:")
+            cases = test_case_info["cases"]
+            for test_number, case in enumerate(cases):
+                sub_test_name = case["test_name"]
+                test_name = f"{operation.lower()}_{sub_test_name}"
+                if self.args.match and self.args.match not in test_name:
+                    continue
+                params = test_case_info["params"] | case["params"]
+                params = {k: process_params(v) for k, v in params.items()}
+                op_class = FILTERS[operation]
+                op_func = op_class.filter_func
+                test_case = TestCase(operation, test_name, sub_test_name, test_number, params, op_func)
+                self.run_test(test_case)
+            print("\n")
 
-    print_time_mode_results(durations)
+        self.print_compare_mode_results()
 
+    def print_compare_mode_results(self):
+        print(f"{'=' * 40}RESULTS{'=' * 40}")
+        failures = 0
+        passes = 0
+        new_baselines = 0
 
-def print_time_mode_results(durations):
-    print(f"\n{'=' * 40}RESULTS{'=' * 40}", end="")
-    with open("timings.csv", "a", newline="", encoding="utf_8") as csvfile:
-        writer = csv.writer(csvfile, delimiter=",")
-        if csvfile.tell() == 0:
-            writer.writerow(["test_name", "avg", "quickest", "slowest", "stdev", "version", "commit_date"])
-        for test_name, times in durations.items():
-            avg = sum(times) / len(times)
-            quickest = min(times)
-            slowest = max(times)
-            print(f"\n{test_name}:\n\tAvg: {avg}\n\tQuickest: {quickest}\n\tSlowest: {slowest}")
-            data = (
-                test_name,
-                avg,
-                quickest,
-                slowest,
-                stdev(times),
-                GIT_TOKEN,
-                COMMIT_DATE,
-            )
-            writer.writerow(data)
+        for test_case in TEST_CASE_RESULTS:
+            if test_case.status == "fail":
+                failures += 1
+                print(f"[FAIL] {test_case.operation} test #{test_case.test_number:03d}"
+                      f", {test_case.test_name} -> {test_case.message}\n")
+            elif test_case.status == "pass":
+                passes += 1
+                if self.args.verbose:
+                    print(f"[PASS] {test_case.operation} test #{test_case.test_number:03d},"
+                          f"{test_case.test_name} -> {test_case.duration}s\n")
+            elif test_case.status == "new baseline":
+                new_baselines += 1
+                if self.args.verbose:
+                    print(f"[NEW] {test_case.operation} test #{test_case.test_number:03d}, {test_case.test_name}\n")
 
-    print(f"{'=' * 42}END{'=' * 42}")
+        print(f"{failures} failed\n{passes} passed\n{new_baselines} new baseline(s)")
+        print(f"{'=' * 42}END{'=' * 42}")
 
+    def time_mode(self, runs):
+        durations = defaultdict(list)
+        image_stack = self.load_image_stack()
+        for operation, test_case_info in TEST_CASES.items():
+            print(f"Running tests for {operation}:")
+            cases = test_case_info["cases"]
+            for case in cases:
+                sub_test_name = case["test_name"]
+                test_name = f"{operation.lower()}_{sub_test_name}"
+                params = case["params"] | test_case_info["params"]
+                op_func = FILTERS[operation].filter_func
+                for _ in range(runs):
+                    image_stack2 = image_stack.copy()
+                    duration = self.time_operation(image_stack2, op_func, params)[0]
+                    durations[test_name].append(duration)
 
-def time_operation(image_stack, op_func, params):
-    start = time.perf_counter()
-    image_stack = run_operation(image_stack, op_func, params)
-    duration = time.perf_counter() - start
-    return duration, image_stack
+        self.print_time_mode_results(durations)
 
+    def print_time_mode_results(self, durations):
+        print(f"\n{'=' * 40}RESULTS{'=' * 40}", end="")
+        with open("timings.csv", "a", newline="", encoding="utf_8") as csvfile:
+            writer = csv.writer(csvfile, delimiter=",")
+            if csvfile.tell() == 0:
+                writer.writerow(["test_name", "avg", "quickest", "slowest", "stdev", "version", "commit_date"])
+            for test_name, times in durations.items():
+                avg = sum(times) / len(times)
+                quickest = min(times)
+                slowest = max(times)
+                print(f"\n{test_name}:\n\tAvg: {avg}\n\tQuickest: {quickest}\n\tSlowest: {slowest}")
+                data = (
+                    test_name,
+                    avg,
+                    quickest,
+                    slowest,
+                    stdev(times),
+                    GIT_TOKEN,
+                    COMMIT_DATE,
+                )
+                writer.writerow(data)
 
-def run_test(test_case):
-    image_stack = load_image_stack()
-    test_case.duration, new_image_stack = time_operation(image_stack, test_case.op_func, test_case.params)
-    file_name = config_manager.save_dir / (test_case.test_name + ".npz")
+        print(f"{'=' * 42}END{'=' * 42}")
 
-    if file_name.is_file():
-        baseline_image_stack = load_post_operation_image_stack(file_name)
-        compare_image_stacks(baseline_image_stack, new_image_stack.data, test_case)
+    def time_operation(self, image_stack, op_func, params):
+        start = time.perf_counter()
+        image_stack = self.run_operation(image_stack, op_func, params)
+        duration = time.perf_counter() - start
+        return duration, image_stack
 
-        if test_case.status == "pass":
-            print(".", end="")
-        elif test_case.status == "fail":
-            print("F", end="")
+    def run_test(self, test_case):
+        image_stack = self.load_image_stack()
+        test_case.duration, new_image_stack = self.time_operation(image_stack, test_case.op_func, test_case.params)
+        file_name = config_manager.save_dir / (test_case.test_name + ".npz")
+
+        if file_name.is_file():
+            baseline_image_stack = self.load_post_operation_image_stack(file_name)
+            self.compare_image_stacks(baseline_image_stack, new_image_stack.data, test_case)
+
+            if test_case.status == "pass":
+                print(".", end="")
+            elif test_case.status == "fail":
+                print("F", end="")
+            else:
+                print("?", end="")
+                test_case.status = "unknown"
         else:
-            print("?", end="")
-            test_case.status = "unknown"
-    else:
-        print("X", end="")
-        test_case.status = "new baseline"
-        save_image_stack(file_name, new_image_stack)
+            print("X", end="")
+            test_case.status = "new baseline"
+            self.save_image_stack(file_name, new_image_stack)
 
-    TEST_CASE_RESULTS.append(test_case)
+        TEST_CASE_RESULTS.append(test_case)
 
+    def run_operation(self, image_stack, op_func, params):
+        op_func(image_stack, **params)
+        return image_stack
 
-def run_operation(image_stack, op_func, params):
-    op_func(image_stack, **params)
-    return image_stack
+    def save_image_stack(self, filepath, image_stack):
+        np.savez(filepath, image_stack.data)
 
+    def load_post_operation_image_stack(self, filepath):
+        return np.load(filepath)["arr_0"]
 
-def save_image_stack(filepath, image_stack):
-    np.savez(filepath, image_stack.data)
+    def load_image_stack(self):
+        filename_group = FilenameGroup.from_file(config_manager.load_sample)
+        filename_group.find_all_files()
+        image_stack = loader.load(filename_group=filename_group)
+        return image_stack
 
+    def compare_image_stacks(self, baseline_image_stack, new_image_stack, test_case):
+        if not (isinstance(baseline_image_stack, np.ndarray) and isinstance(new_image_stack, np.ndarray)):
+            test_case.status = "fail"
+            test_case.message = "new image stack is not an array"
+        elif baseline_image_stack.shape != new_image_stack.shape:
+            test_case.status = "fail"
+            test_case.message = "new image stack is different shape to the baseline"
+        elif baseline_image_stack.dtype != new_image_stack.dtype:
+            test_case.status = "fail"
+            test_case.message = "new image stack is different dtype to the baseline"
+        elif not np.array_equal(baseline_image_stack, new_image_stack):
+            test_case.status = "fail"
+            test_case.message = "arrays are not equal"
+            if self.args.gui:
+                self.gui_compare_image_stacks(baseline_image_stack, new_image_stack)
+        else:
+            test_case.status = "pass"
+            test_case.message = "arrays are equal"
 
-def load_post_operation_image_stack(filepath):
-    return np.load(filepath)["arr_0"]
+    def gui_compare_image_stacks(self, baseline_image_stack, new_image_stack):
+        from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout
+        from mantidimaging.gui.widgets.mi_image_view.view import MIImageView
+        app = QApplication([])
+        win = QWidget()
+        win.resize(600, 900)
+        layout = QHBoxLayout()
+        win.setLayout(layout)
 
+        imvs = []
+        for name, data in (
+            ("Baseline", baseline_image_stack),
+            ("New", new_image_stack),
+            ("Diff", new_image_stack - baseline_image_stack),
+        ):
+            imv = MIImageView()
+            imv.name = name
+            imv.enable_nan_check(True)
+            imv.setImage(data)
+            layout.addWidget(imv)
+            imvs.append(imv)
 
-def load_image_stack():
-    filename_group = FilenameGroup.from_file(config_manager.load_sample)
-    filename_group.find_all_files()
-    image_stack = loader.load(filename_group=filename_group)
-    return image_stack
+        imvs[0].sigTimeChanged.connect(imvs[1].setCurrentIndex)
+        imvs[0].sigTimeChanged.connect(imvs[2].setCurrentIndex)
 
-
-def compare_image_stacks(baseline_image_stack, new_image_stack, test_case):
-    if not (isinstance(baseline_image_stack, np.ndarray) and isinstance(new_image_stack, np.ndarray)):
-        test_case.status = "fail"
-        test_case.message = "new image stack is not an array"
-    elif baseline_image_stack.shape != new_image_stack.shape:
-        test_case.status = "fail"
-        test_case.message = "new image stack is different shape to the baseline"
-    elif baseline_image_stack.dtype != new_image_stack.dtype:
-        test_case.status = "fail"
-        test_case.message = "new image stack is different dtype to the baseline"
-    elif not np.array_equal(baseline_image_stack, new_image_stack):
-        test_case.status = "fail"
-        test_case.message = "arrays are not equal"
-        if args.gui:
-            gui_compare_image_stacks(baseline_image_stack, new_image_stack)
-    else:
-        test_case.status = "pass"
-        test_case.message = "arrays are equal"
-
-
-def gui_compare_image_stacks(baseline_image_stack, new_image_stack):
-    from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout
-    from mantidimaging.gui.widgets.mi_image_view.view import MIImageView
-    app = QApplication([])
-    win = QWidget()
-    win.resize(600, 900)
-    layout = QHBoxLayout()
-    win.setLayout(layout)
-
-    imvs = []
-    for name, data in (
-        ("Baseline", baseline_image_stack),
-        ("New", new_image_stack),
-        ("Diff", new_image_stack - baseline_image_stack),
-    ):
-        imv = MIImageView()
-        imv.name = name
-        imv.enable_nan_check(True)
-        imv.setImage(data)
-        layout.addWidget(imv)
-        imvs.append(imv)
-
-    imvs[0].sigTimeChanged.connect(imvs[1].setCurrentIndex)
-    imvs[0].sigTimeChanged.connect(imvs[2].setCurrentIndex)
-
-    win.show()
-    app.exec()
-
-
-def create_plots():
-    df = pd.read_csv("timings.csv", parse_dates=["commit_date"])
-    traces = []
-    test_names = []
-
-    for i, (test_name, group) in enumerate(df.groupby("test_name"), start=1):
-        group.sort_values("commit_date", inplace=True)
-        traces.append((
-            i,
-            go.Scatter(
-                x=group["commit_date"],
-                y=group["avg"],
-                text=group["version"],
-                name=f"{test_name} Average",
-                error_y={
-                    'type': 'data',
-                    'array': group['stdev'],
-                    'visible': True
-                },
-            ),
-        ))
-        traces.append((
-            i,
-            go.Scatter(
-                x=group["commit_date"],
-                y=group["quickest"],
-                text=group["version"],
-                name=f"{test_name} Minimum",
-            ),
-        ))
-        test_names.append(test_name)
-
-    fig = make_subplots(rows=len(traces), cols=1, subplot_titles=test_names)
-    for i, trace in traces:
-        fig.add_trace(trace, row=i, col=1)
-
-    fig.update_layout(legend_tracegroupgap=238)
-    fig.write_html(f"{datetime.today().strftime('%Y-%m-%d')}.html", default_height="1500%")
+        win.show()
+        app.exec()
 
 
 def main():
@@ -330,17 +302,11 @@ def main():
     parser.add_argument("-k", dest="match", type=str, help="only run tests which match the given substring expression")
     parser.add_argument("--gui", dest="gui", action="store_true", help="Show GUI comparison for differences")
 
-    global args
     args = parser.parse_args()
 
-    if args.mode == "time":
-        time_mode(args.runs)
-
-    if args.mode == "compare":
-        compare_mode()
-
-    if args.graphs:
-        create_plots()
+    runner = TestRunner(args)
+    runner.configure()
+    runner.run_tests()
 
 
 if __name__ == "__main__":
