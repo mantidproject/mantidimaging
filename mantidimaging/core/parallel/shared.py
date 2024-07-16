4ddad2bbf14b35c5522d88bad0a50bbb0d70f2e2
ashmeigh <ashleymeigh0@gmail.com>
2024-07-16 10:00:38 +0100

    deleted unnecessary functions
    Signed-off-by: ashmeigh <ashleymeigh0@gmail.com>

--- mantidimaging/core/parallel/shared.py
+++ mantidimaging/core/parallel/shared.py
@@ -2,80 +2,11 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 from __future__ import annotations
 
-from functools import partial
-from typing import Any, TYPE_CHECKING
+from typing import Any
 from collections.abc import Callable
+from numpy import ndarray  # Import ndarray type from numpy
 
 from mantidimaging.core.parallel import utility as pu
-
-if TYPE_CHECKING:
-    from numpy import ndarray
-
-
-def inplace3(func, data: list[pu.SharedArray] | list[pu.SharedArrayProxy], i, **kwargs):
-    func(data[0].array[i], data[1].array[i], data[2].array, **kwargs)
-
-
-def inplace2(func, data: list[pu.SharedArray] | list[pu.SharedArrayProxy], i, **kwargs):
-    func(data[0].array[i], data[1].array[i], **kwargs)
-
-
-def inplace1(func, data: list[pu.SharedArray] | list[pu.SharedArrayProxy], i, **kwargs):
-    func(data[0].array[i], **kwargs)
-
-
-def return_to_self(func, data: list[pu.SharedArray] | list[pu.SharedArrayProxy], i, **kwargs):
-    data[0].array[i] = func(data[0].array[i], **kwargs)
-
-
-def inplace_second_2d(func, data: list[pu.SharedArray] | list[pu.SharedArrayProxy], i, **kwargs):
-    func(data[0].array[i], data[1].array, **kwargs)
-
-
-def return_to_second_at_i(func, data: list[pu.SharedArray] | list[pu.SharedArrayProxy], i, **kwargs):
-    data[1].array[i] = func(data[0].array[i], **kwargs)
-
-
-def create_partial(func, fwd_function, **kwargs):
-    """
-    Create a partial using functools.partial, to forward the kwargs to the
-    parallel execution of imap.
-
-    If you seem to be getting nans, check if the correct fwd_function is set!
-
-    :param func: Function that will be executed
-    :param fwd_function: The function will be forwarded through function.
-    :param kwargs: kwargs to forward to the function func that will be executed
-    :return: The decorated forwarded function, ready for further execution
-    """
-    return partial(fwd_function, func, **kwargs)
-
-
-def execute(partial_func: partial,
-            arrays: list[pu.SharedArray],
-            num_operations: int,
-            progress=None,
-            msg: str = '') -> None:
-    """
-    Executes a function a given number of times using the provided list of SharedArray objects.
-
-    If all the arrays in the list use shared memory then the execution is done in parallel, with each process
-    accessing the data in shared memory.
-    If any arrays in the list do not use shared memory then the execution will be performed synchronously.
-
-    :param partial_func: A function constructed using create_partial
-    :param arrays: The list of SharedArray objects that the operations should be performed on
-    :param num_operations: The expected number of operations - should match the number of images being processed
-                           Also used to set the number of progress steps
-    :param progress: Progress instance to use for progress reporting (optional)
-    :param msg: Message to be shown on the progress bar
-    :return:
-    """
-
-    all_data_in_shared_memory, data = _check_shared_mem_and_get_data(arrays)
-    partial_func = partial(partial_func, data)
-    pu.execute_impl(num_operations, partial_func, all_data_in_shared_memory, progress, msg)
-
 
 ComputeFuncType = (Callable[[int, list['ndarray'], dict[str, Any]], None]
                    | Callable[[int, 'ndarray', dict[str, Any]], None])
