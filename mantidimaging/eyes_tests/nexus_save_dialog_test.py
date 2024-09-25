8bdd7597430d7ca64a7b80ee71fcb4034a17cc0e
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-09-25 11:22:57 +0100

    Fix NexusSaveDialogTest for strict_dataset_list removal

--- mantidimaging/eyes_tests/nexus_save_dialog_test.py
+++ mantidimaging/eyes_tests/nexus_save_dialog_test.py
@@ -2,21 +2,18 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 from __future__ import annotations
 
-from typing import NamedTuple
 from unittest import mock
-from uuid import UUID, uuid4
 
+from mantidimaging.core.data.dataset import Dataset
 from mantidimaging.eyes_tests.base_eyes import BaseEyesTest
 
 
 class NexusSaveDialogTest(BaseEyesTest):
 
     def test_save_dialog_opens_with_dataset(self):
-        TestTuple = NamedTuple('TestTuple', [('id', UUID), ('name', str)])
-        dataset_list = [TestTuple(uuid4(), 'Test Dataset')]
-        with mock.patch("mantidimaging.gui.windows.main.MainWindowView.strict_dataset_list",
-                        new_callable=mock.PropertyMock) as mock_dataset_list:
-            mock_dataset_list.return_value = dataset_list
+        with mock.patch("mantidimaging.gui.windows.main.MainWindowPresenter.datasets",
+                        new_callable=mock.PropertyMock) as mock_datasets:
+            mock_datasets.return_value = [Dataset(name='Test Dataset')]
             self.imaging.actionSaveNeXusFile.trigger()
 
         self.check_target(widget=self.imaging.nexus_save_dialog)
