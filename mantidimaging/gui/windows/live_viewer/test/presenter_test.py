ba7348b2ed7c59e3ce01f2233d9a9c9d3ee6a64a
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-10-24 16:25:30 +0100

    Live view add method to load as a dataset
    And a test

---
+++ mantidimaging/gui/windows/live_viewer/test/presenter_test.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2024 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+from __future__ import annotations
+
+import unittest
+from pathlib import Path
+from unittest import mock
+
+from mantidimaging.gui.windows.live_viewer import LiveViewerWindowView, LiveViewerWindowModel, LiveViewerWindowPresenter
+from mantidimaging.gui.windows.live_viewer.model import Image_Data
+from mantidimaging.gui.windows.main import MainWindowView
+
+
+class MainWindowPresenterTest(unittest.TestCase):
+
+    def setUp(self):
+        self.view = mock.create_autospec(LiveViewerWindowView)
+        self.main_window = mock.create_autospec(MainWindowView)
+        self.model = mock.create_autospec(LiveViewerWindowModel)
+
+        with mock.patch("mantidimaging.gui.windows.live_viewer.presenter.LiveViewerWindowModel") as mock_model:
+            mock_model.return_value = self.model
+            self.presenter = LiveViewerWindowPresenter(self.view, self.main_window)
+
+    def test_load_as_dataset(self):
+        image_dir = Path("/path/to/dir")
+        image_paths = [image_dir / f"image_{i:03d}.tif" for i in range(5)]
+        self.model.images = [mock.create_autospec(Image_Data, image_path=p) for p in image_paths]
+
+        self.presenter.load_as_dataset()
+
+        self.main_window.show_image_load_dialog_with_path.assert_called_once_with(str(image_dir))
