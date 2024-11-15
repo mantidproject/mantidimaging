2c6238d5db1433231b2667b153b7b2511419e3bf
Sam Tygier <sam.tygier@stfc.ac.uk>
2024-11-15 15:46:17 +0000

    Remove StrictDataset from BaseEyesTest, SpectrumViewerWindowTest, DatasetSelectorWidgetPresenterTests, SpectrumViewerWindowPresenterTest

--- mantidimaging/gui/widgets/dataset_selector/test/presenter_test.py
+++ mantidimaging/gui/widgets/dataset_selector/test/presenter_test.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 import unittest
 from unittest import mock
 
-from mantidimaging.core.data.dataset import StrictDataset, Dataset
+from mantidimaging.core.data.dataset import Dataset
 from mantidimaging.gui.widgets.dataset_selector.presenter import DatasetSelectorWidgetPresenter, Notification
 from mantidimaging.gui.widgets.dataset_selector.view import DatasetSelectorWidgetView
 
@@ -29,9 +29,9 @@ class DatasetSelectorWidgetPresenterTests(unittest.TestCase):
         self.img3.name = "Image 3"
         self.img4 = mock.Mock(id="img4")
         self.img4.name = "Image 4"
-        self.ds1 = StrictDataset(sample=self.img1)
+        self.ds1 = Dataset(sample=self.img1)
         self.ds1.name = "Dataset 1"
-        self.ds2 = StrictDataset(sample=self.img2, flat_before=self.img3)
+        self.ds2 = Dataset(sample=self.img2, flat_before=self.img3)
         self.ds2.name = "Dataset 2"
         self.ds3 = Dataset(stacks=[self.img4])
         self.ds3.name = "Dataset 3"
