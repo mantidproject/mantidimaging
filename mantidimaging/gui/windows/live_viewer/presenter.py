a9e5c164385ad74c122ffdb37e0e95a02ff37341
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2024-11-27 11:47:19 +0000

    Mean buffer loading and prints removed

--- mantidimaging/gui/windows/live_viewer/presenter.py
+++ mantidimaging/gui/windows/live_viewer/presenter.py
@@ -69,19 +69,12 @@ class LiveViewerWindowPresenter(BasePresenter):
     def update_image_list(self, images_list: list[Image_Data]) -> None:
         """Update the image in the view."""
         if not images_list:
-            print("++++++++++++++++++++++++++++ presenter.update_image_list() 1 +++++++++++++++++++++++++++++++++")
             self.handle_deleted()
-            print("++++++++++++++++++++++++++++ presenter.update_image_list() 2 +++++++++++++++++++++++++++++++++")
             self.view.set_load_as_dataset_enabled(False)
-            print("++++++++++++++++++++++++++++ presenter.update_image_list() 3 +++++++++++++++++++++++++++++++++")
         else:
-            print("++++++++++++++++++++++++++++ presenter.update_image_list() 4 +++++++++++++++++++++++++++++++++")
             self.view.set_image_range((0, len(images_list) - 1))
-            print("++++++++++++++++++++++++++++ presenter.update_image_list() 5 +++++++++++++++++++++++++++++++++")
             self.view.set_image_index(len(images_list) - 1)
-            print("++++++++++++++++++++++++++++ presenter.update_image_list() 6 +++++++++++++++++++++++++++++++++")
             self.view.set_load_as_dataset_enabled(True)
-            print("++++++++++++++++++++++++++++ presenter.update_image_list() 7 +++++++++++++++++++++++++++++++++")
 
     def select_image(self, index: int) -> None:
         if not self.model.images:
@@ -104,6 +97,10 @@ class LiveViewerWindowPresenter(BasePresenter):
             self.view.remove_image()
             self.view.live_viewer.show_error(message)
             return
+        self.view.live_viewer.set_image_shape(image_data.shape)
+        if not self.view.live_viewer.roi_object and self.view.spectrum_action.isChecked():
+            self.view.live_viewer.add_roi()
+        self.model.image_stack.set_roi(self.view.live_viewer.get_roi())
         image_data = self.perform_operations(image_data)
         if image_data.size == 0:
             message = "reading image: {image_path}: Image has zero size"
