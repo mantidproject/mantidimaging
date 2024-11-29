e9199623ca851f20892678231a757f122c696cf3
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2024-11-29 16:32:19 +0000

    mean calculated with incoming data and displayed, base ImageCache class created

--- mantidimaging/gui/windows/live_viewer/model.py
+++ mantidimaging/gui/windows/live_viewer/model.py
@@ -6,13 +6,113 @@ import time
 from typing import TYPE_CHECKING
 from pathlib import Path
 from logging import getLogger
+
+import numpy as np
 from PyQt5.QtCore import QFileSystemWatcher, QObject, pyqtSignal, QTimer
+
+from mantidimaging.core.utility.sensible_roi import SensibleROI
 
 if TYPE_CHECKING:
     from os import stat_result
     from mantidimaging.gui.windows.live_viewer.view import LiveViewerWindowPresenter
 
 LOG = getLogger(__name__)
+
+
+class ImageCache:
+    """
+    An ImageCache class to be used as a decorator on image read functions to store recent images in memory
+    """
+    cache_dict: dict = {}
+    image_list: list[Image_Data]
+    image_paths: set[str] = set()
+    mean: np.ndarray = np.array([])
+    roi: SensibleROI | None = None
+    param_to_calc: list[str] = []
+    max_cache_size: int = 100
+    buffer_size: int = 10
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        #print(f"Arguments: {args}, Keyword Arguments: {kwargs}")
+        result = self.func(*args, **kwargs)
+        self.add_to_cache(args, result)
+        return result
+
+    def add_to_cache(self, args, image_array: np.ndarray):
+        if args not in self.cache_dict.keys():
+            self.cache_dict[args] = image_array
+
+    def remove_from_cache(self, image: Image_Data):
+        if image.image_path in self.cache_dict.keys():
+            del self.cache_dict[image.image_path]
+
+    # def update_param_calculations(self) -> None:
+    #     if 'mean' in self.param_to_calc:
+    #         if len(self.mean) == len(self.image_list) - 1:
+    #             self.add_last_mean()
+    #         else:
+    #             if self.roi:
+    #                 self.calc_mean_fully_roi()
+    #             else:
+    #                 self.calc_mean_fully()
+    #
+    # def add_last_mean(self) -> None:
+    #     if self.delayed_stack is not None:
+    #         if self.roi:
+    #             left, top, right, bottom = self.roi
+    #             mean_to_add = dask.optimize(dask.array.mean(self.delayed_stack[-1, top:bottom,
+    #                                                                            left:right]))[0].compute()
+    #         else:
+    #             mean_to_add = dask.optimize(dask.array.mean(self.delayed_stack[-1]))[0].compute()
+    #         self.mean = np.append(self.mean, mean_to_add)
+    #         self.calc_mean_buffer()
+
+    # def calc_mean_fully(self) -> None:
+    #     if self.delayed_stack is not None:
+    #         self.mean = dask.array.mean(self.delayed_stack, axis=(1, 2)).compute()
+    #
+    # def calc_mean_fully_roi(self):
+    #     if self.delayed_stack is not None and self.image_list:
+    #         left, top, right, bottom = self.roi
+    #         current_cache_size = len(self.)
+    #         self.mean = np.full(len(self.image_list), np.nan)
+    #         np.put(self.mean, range(-current_cache_size, 0), self.calc_mean_cached_images(left, top, right, bottom))
+    #
+    # def calc_mean_cached_images(self, left, top, right, bottom):
+    #     current_cache_size = self.get_computed_image.cache_info()[3]
+    #     cache_stack = [
+    #         self.get_computed_image(index)
+    #         for index in range(self.selected_index - current_cache_size + 1, self.selected_index + 1, 1)
+    #     ]
+    #     cache_stack_array = np.stack(cache_stack)
+    #     cache_stack_mean = np.mean(cache_stack_array[:, top:bottom, left:right], axis=(1, 2))
+    #     return cache_stack_mean
+    #
+    # def calc_mean_buffer(self):
+    #     nanInds = np.argwhere(np.isnan(self.mean))
+    #     left, top, right, bottom = self.roi
+    #     if nanInds.size > 0:
+    #         print(f"{self.mean=}")
+    #         if nanInds.size < self.buffer_size:
+    #             buffer_start = 0
+    #         else:
+    #             buffer_start = nanInds.size - self.buffer_size
+    #         dask_mean = dask.optimize(
+    #             dask.array.mean(self.delayed_stack[buffer_start:nanInds.size, top:bottom, left:right],
+    #                             axis=(1, 2)))[0].compute()
+    #         np.put(self.mean, range(buffer_start, nanInds.size), dask_mean)
+
+    def set_roi(self, roi: SensibleROI):
+        self.roi = roi
+
+    def delete_all_data(self):
+        pass
+
+    def add_param_to_calc(self, param_name: str):
+        self.param_to_calc.append(param_name)
 
 
 class Image_Data:
@@ -102,7 +202,10 @@ class LiveViewerWindowModel:
         self.presenter = presenter
         self._dataset_path: Path | None = None
         self.image_watcher: ImageWatcher | None = None
-        self.images: list[Image_Data] = []
+        self._images: list[Image_Data] = []
+        self.mean: np.array = np.array([])
+        self.mean_dict: dict[Path, float] = {}
+        self.roi: SensibleROI | None = None
 
     @property
     def path(self) -> Path | None:
@@ -116,6 +219,14 @@ class LiveViewerWindowModel:
         self.image_watcher.recent_image_changed.connect(self.handle_image_modified)
         self.image_watcher._handle_notified_of_directry_change(str(path))
 
+    @property
+    def images(self):
+        return self._images if self._images is not None else None
+
+    @images.setter
+    def images(self, images):
+        self._images = images
+
     def _handle_image_changed_in_list(self, image_files: list[Image_Data]) -> None:
         """
         Handle an image changed event. Update the image in the view.
@@ -125,6 +236,8 @@ class LiveViewerWindowModel:
         :param image_files: list of image files
         """
         self.images = image_files
+        # if dask_image_stack.image_list:
+        #     self.image_stack = dask_image_stack
         self.presenter.update_image_list(image_files)
 
     def handle_image_modified(self, image_path: Path):
@@ -136,6 +249,16 @@ class LiveViewerWindowModel:
             self.image_watcher.remove_path()
             self.image_watcher = None
         self.presenter = None  # type: ignore # Model instance to be destroyed -type can be inconsistent
+
+    def add_mean(self, image_data_obj: Image_Data, image_array: np.array) -> None:
+        if self.roi:
+            left, top, right, bottom = self.roi
+            mean_to_add = np.mean(image_array[top:bottom, left:right])
+        else:
+            mean_to_add = np.mean(image_array)
+        self.mean_dict[image_data_obj.image_path] = mean_to_add
+        self.mean = list(self.mean_dict.values())
+
 
 
 class ImageWatcher(QObject):
@@ -161,7 +284,9 @@ class ImageWatcher(QObject):
         Sort the images by modified time.
     """
     image_changed = pyqtSignal(list)  # Signal emitted when an image is added or removed
+    update_spectrum = pyqtSignal(np.ndarray)  # Signal emitted to update the Live Viewer Spectrum
     recent_image_changed = pyqtSignal(Path)
+    create_delayed_array: bool = False
 
     def __init__(self, directory: Path):
         """
