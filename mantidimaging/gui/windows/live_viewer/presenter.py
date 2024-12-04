ece71a2e6c9cdcb5d16ead8b8c360baccfa61614
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2024-12-04 13:27:15 +0000

    Store Image_Data obj in cache and added TODOs

--- mantidimaging/gui/windows/live_viewer/presenter.py
+++ mantidimaging/gui/windows/live_viewer/presenter.py
@@ -11,8 +11,7 @@ import numpy as np
 from imagecodecs._deflate import DeflateError
 
 from mantidimaging.gui.mvp_base import BasePresenter
-from mantidimaging.gui.windows.live_viewer.model import (LiveViewerWindowModel, Image_Data, load_image_from_path,
-                                                         ImageCache)
+from mantidimaging.gui.windows.live_viewer.model import LiveViewerWindowModel, Image_Data
 from mantidimaging.core.operations.loader import load_filter_packages
 from mantidimaging.core.data import ImageStack
 
@@ -67,6 +66,8 @@ class LiveViewerWindowPresenter(BasePresenter):
 
     def update_image_list(self, images_list: list[Image_Data]) -> None:
         """Update the image in the view."""
+        # TODO: put add_mean in here and check that if images have been deleted, these are compared against the mean_dict
+        # TODO: throw away and recalc the mean_dict if needed
         if not images_list:
             self.handle_deleted()
             self.view.set_load_as_dataset_enabled(False)
