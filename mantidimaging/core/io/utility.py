5540de3f65fbcf9aa5282b0ec7cdcf8673b76bd3
JackEAllen <jack.allen@stfc.ac.uk>
2025-01-06 10:59:10 +0000

    Add FITS Support to find_first_file_that_is_possibly_a_sample()

--- mantidimaging/core/io/utility.py
+++ mantidimaging/core/io/utility.py
@@ -10,18 +10,24 @@ from logging import getLogger
 log = getLogger(__name__)
 
 DEFAULT_IO_FILE_FORMAT = 'tif'
-NEXUS_PROCESSED_DATA_PATH = "processed-data"
+NEXUS_PROCESSED_DATA_PATH = 'processed-data'
 
 THRESHOLD_180 = np.radians(1)
 
 
 def find_first_file_that_is_possibly_a_sample(file_path: str) -> str | None:
-    # Grab all .tif or .tiff files
-    possible_files = glob.glob(os.path.join(file_path, "**/*.tif*"), recursive=True)
-
+    """
+    Finds the first file that is possibly a tif, .tiff, .fit or .fits sample file.
+    If files are found, the files are sorted and filtered based on name and returned.
+    """
+    file_types = ['tif', 'tiff', 'fit', 'fits']
+    for file_type in file_types:
+        possible_files = glob.glob(os.path.join(file_path, f'**/*.{file_type}'), recursive=True)
+        if possible_files:
+            break
     for possible_file in sorted(possible_files):
         lower_filename = os.path.basename(possible_file).lower()
-        if "flat" not in lower_filename and "dark" not in lower_filename and "180" not in lower_filename:
+        if 'flat' not in lower_filename and 'dark' not in lower_filename and '180' not in lower_filename:
             return possible_file
     return None
 
