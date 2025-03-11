51c679889d3423d14983bfd194eda885f6d011b5
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2025-03-11 14:30:05 +0000

    can switch between joined and scatter plots for the SV by changing a bool

--- mantidimaging/gui/windows/spectrum_viewer/view.py
+++ mantidimaging/gui/windows/spectrum_viewer/view.py
@@ -478,12 +478,16 @@ class SpectrumViewerWindowView(BaseMainWindowView):
         self.show_visible_spectrums()
 
     def show_visible_spectrums(self) -> None:
+        join_plot = True
         for key, value in self.spectrum_widget.spectrum_data_dict.items():
             if value is not None and key in self.spectrum_widget.roi_dict:
                 self.spectrum_widget.spectrum.plot(self.presenter.model.tof_data,
                                                    value,
                                                    name=key,
-                                                   pen=self.spectrum_widget.roi_dict[key].colour)
+                                                   pen=self.spectrum_widget.roi_dict[key].colour,
+                                                   connect=np.full(value.shape, join_plot))
+                if not join_plot:
+                    self.presenter.set_scatter_plot_style(key)
 
     def add_roi_table_row(self, name: str, colour: tuple[int, int, int, int]) -> None:
         """
