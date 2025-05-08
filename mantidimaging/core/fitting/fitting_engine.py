84a992626b38f3daa8a996b791bcf5050d0c8249
Sam Tygier <sam.tygier@stfc.ac.uk>
2025-05-08 13:19:02 +0100

    Add FittingEngine find_best_fit

--- mantidimaging/core/fitting/fitting_engine.py
+++ mantidimaging/core/fitting/fitting_engine.py
@@ -2,6 +2,9 @@
 # SPDX - License - Identifier: GPL-3.0-or-later
 from __future__ import annotations
 
+import numpy as np
+from scipy.optimize import minimize
+
 from mantidimaging.core.fitting.fitting_functions import BaseFittingFunction
 
 
@@ -15,3 +18,11 @@ class FittingEngine:
 
     def get_init_params_from_roi(self, region: tuple[float, float, float, float]) -> dict[str, float]:
         return self.model.get_init_params_from_roi(region)
+
+    def find_best_fit(self, xdata: np.ndarray, ydata: np.ndarray, initial_params: list[float]) -> dict[str, float]:
+
+        def f(params):
+            return ((self.model.evaluate(xdata, params) - ydata)**2).sum()
+
+        result = minimize(f, initial_params, method="Nelder-Mead")
+        return dict(zip(self.model.get_parameter_names(), result.x, strict=True))
