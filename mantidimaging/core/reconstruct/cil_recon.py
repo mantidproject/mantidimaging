7a036fc648597f51d38d37323bce586dd753d656
hussam-stfc <173286573+hussam-stfc@users.noreply.github.com>
2025-06-09 15:18:36 +0100

    Check for Imagestack.geometry object before attempting to use in CILRecon

--- mantidimaging/core/reconstruct/cil_recon.py
+++ mantidimaging/core/reconstruct/cil_recon.py
@@ -379,17 +379,15 @@ class CILRecon(BaseRecon):
                 raise ValueError("recon_params.tilt is not set")
             tilt = recon_params.tilt.value
 
-            if images.geometry is None:
-                raise ValueError("images.geometry is not set")
-            images.geometry.set_geometry_from_cor_tilt(cors[pixel_num_v // 2], tilt)
-
             if images.is_sinograms:
                 data_order = DataOrder.ASTRA_AG_LABELS
             else:
                 data_order = DataOrder.TIGRE_AG_LABELS
-            images.geometry.set_labels(data_order)
 
-            ig = images.geometry.get_ImageGeometry()
+            if images.geometry is not None:
+                images.geometry.set_geometry_from_cor_tilt(cors[pixel_num_v // 2], tilt)
+                images.geometry.set_labels(data_order)
+                ig = images.geometry.get_ImageGeometry()
 
             data = CILRecon.get_data(BaseRecon.prepare_sinogram(images.data, recon_params), images.geometry,
                                      recon_params, num_subsets)
