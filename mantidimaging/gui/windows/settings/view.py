954ff443766f5d7e34c913eb82c1d4f5dfba4d28
Ashley <ashley.meigh@stfc.ac.uk>
2025-06-16 09:42:11 +0100

    Add Settings Tab for Logging Configuration in Mantid Imaging

--- mantidimaging/gui/windows/settings/view.py
+++ mantidimaging/gui/windows/settings/view.py
@@ -5,7 +5,8 @@ from logging import getLogger
 from typing import TYPE_CHECKING
 
 from PyQt5.QtCore import QSettings, QSignalBlocker
-from PyQt5.QtWidgets import QTabWidget, QWidget, QComboBox, QLabel, QCheckBox, QSpinBox
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QLabel, QCheckBox, QLineEdit, QPushButton, QFileDialog,
+                             QComboBox, QWidget, QTabWidget, QSpinBox)
 
 from mantidimaging.gui.mvp_base import BaseMainWindowView
 
@@ -39,6 +40,7 @@ class SettingsWindowView(BaseMainWindowView, QtStyleTools):
         self.setWindowTitle('Settings')
         self.main_window = main_window
         self.presenter = SettingsWindowPresenter(self, main_window)
+        self._create_logging_tab()
 
         self.themeName.addItem('Fusion')
         self.themeName.addItems(list_themes())
@@ -67,6 +69,81 @@ class SettingsWindowView(BaseMainWindowView, QtStyleTools):
         self.processesSpinBox.setMaximum(128)
         self.processesSpinBox.setValue(settings.value("multiprocessing/process_count", 8, type=int))
         self.processesSpinBox.valueChanged.connect(self.presenter.set_processes_value)
+
+    def _create_logging_tab(self):
+        settings = QSettings()
+
+        self.loggingTab = QWidget()
+        self.settingsTabWidget.addTab(self.loggingTab, "Logging")
+
+        # Create main
+        mainLayout = QVBoxLayout()
+        self.loggingTab.setLayout(mainLayout)
+
+        # Enable Logging
+        self.enableLoggingCheckBox = QCheckBox("Enable Logging")
+        self.enableLoggingCheckBox.setChecked(settings.value("logging/enabled", True, type=bool))
+        mainLayout.addWidget(self.enableLoggingCheckBox)
+
+        # Log Directory
+        dirLayout = QHBoxLayout()
+        self.logDirectoryLabel = QLabel("Log Directory:")
+        self.logDirectoryLineEdit = QLineEdit(settings.value("logging/log_dir", "", type=str))
+        self.logDirectoryLineEdit.setFixedWidth(200)
+        self.logDirectoryButton = QPushButton("Browse")
+        self.logDirectoryButton.setFixedWidth(80)
+        self.logDirectoryButton.clicked.connect(self.select_log_directory)
+        dirLayout.addWidget(self.logDirectoryLabel)
+        dirLayout.addWidget(self.logDirectoryLineEdit)
+        dirLayout.addWidget(self.logDirectoryButton)
+        dirLayout.addStretch()
+        mainLayout.addLayout(dirLayout)
+
+        # Log Level
+        levelLayout = QHBoxLayout()
+        self.logLevelLabel = QLabel("Log Level:")
+        self.logLevelComboBox = QComboBox()
+        self.logLevelComboBox.addItems(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
+        self.logLevelComboBox.setCurrentText(settings.value("logging/log_level", "INFO"))
+        self.logLevelComboBox.setFixedWidth(150)
+        levelLayout.addWidget(self.logLevelLabel)
+        levelLayout.addWidget(self.logLevelComboBox)
+        levelLayout.addStretch()
+        mainLayout.addLayout(levelLayout)
+
+        # Retention (days)
+        retentionLayout = QHBoxLayout()
+        self.logRetentionLabel = QLabel("Retention (days):")
+        self.logRetentionSpinBox = QSpinBox()
+        self.logRetentionSpinBox.setMinimum(1)
+        self.logRetentionSpinBox.setMaximum(365)
+        self.logRetentionSpinBox.setValue(settings.value("logging/retention", 30, type=int))
+        self.logRetentionSpinBox.setFixedWidth(80)
+        retentionLayout.addWidget(self.logRetentionLabel)
+        retentionLayout.addWidget(self.logRetentionSpinBox)
+        retentionLayout.addStretch()
+        mainLayout.addLayout(retentionLayout)
+
+        # Performance
+        self.performanceLoggingCheckBox = QCheckBox("Performance Logging")
+        self.performanceLoggingCheckBox.setChecked(settings.value("logging/performance_log", False, type=bool))
+        mainLayout.addWidget(self.performanceLoggingCheckBox)
+
+        # align
+        mainLayout.addStretch()
+
+        # Connect signals
+        self.enableLoggingCheckBox.stateChanged.connect(self.presenter.set_logging_enabled)
+        self.logLevelComboBox.currentTextChanged.connect(self.presenter.set_log_level)
+        self.logRetentionSpinBox.valueChanged.connect(self.presenter.set_log_retention)
+        self.logDirectoryButton.clicked.connect(self.select_log_directory)
+        self.performanceLoggingCheckBox.stateChanged.connect(self.presenter.set_performance_logging)
+
+    def select_log_directory(self):
+        directory = QFileDialog.getExistingDirectory(self, "Select Log Directory")
+        if directory:
+            self.logDirectoryLineEdit.setText(directory)
+            self.presenter.set_log_directory(directory)
 
     @property
     def current_theme(self) -> str:
