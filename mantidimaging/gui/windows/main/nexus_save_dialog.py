99d1b61ce8a89b1a601c80888b5c48af23fcddc0
Ashley <ashley.meigh@stfc.ac.uk>
2025-07-07 16:59:44 +0100

    (Main) Refactor Codebase for Pathlib Compatibility and Type Safety

--- mantidimaging/gui/windows/main/nexus_save_dialog.py
+++ mantidimaging/gui/windows/main/nexus_save_dialog.py
@@ -1,9 +1,10 @@
 # Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
 from __future__ import annotations
-import os
+
 import uuid
 from collections.abc import Iterable
+from pathlib import Path
 
 from PyQt5.QtWidgets import QDialogButtonBox, QFileDialog, QRadioButton
 
@@ -63,9 +64,10 @@ class NexusSaveDialog(BaseDialogView):
         self._check_extension()
 
     def _check_extension(self) -> None:
-        path = self.save_path()
-        if os.path.splitext(path)[1] != NXS_EXT:
-            self.savePath.setText(path + NXS_EXT)
+        path = Path(self.save_path())
+        if path.suffix != NXS_EXT:
+            new_path = path.with_suffix(NXS_EXT)
+            self.savePath.setText(str(new_path))
 
     @property
     def save_as_float(self) -> bool:
