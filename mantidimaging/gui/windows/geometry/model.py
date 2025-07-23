03be7d7578aa11feb8dedce5a1a1bf04ec543470
hussam-stfc <173286573+hussam-stfc@users.noreply.github.com>
2025-07-23 15:29:44 +0100

    TEMP: Added boilerplate files for new Geometry Window

---
+++ mantidimaging/gui/windows/geometry/model.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
+# SPDX - License - Identifier: GPL-3.0-or-later
+
+
+class GeometryWindowModel:
+
+    def __init__(self):
+        self.temp = "TEMP"
