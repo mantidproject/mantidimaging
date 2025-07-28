c074b23e11a40f0ba54d5b9767dcab4b9ea210d0
hussam-stfc <173286573+hussam-stfc@users.noreply.github.com>
2025-07-28 10:46:36 +0100

    TEMP

--- mantidimaging/gui/windows/geometry/view.py
+++ mantidimaging/gui/windows/geometry/view.py
@@ -8,8 +8,27 @@ from typing import TYPE_CHECKING
 from mantidimaging.gui.mvp_base import BaseMainWindowView
 from mantidimaging.gui.windows.geometry.presenter import GeometryWindowPresenter
 
+## EXPERIMENTAL
+import matplotlib
+
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtCore, QtWidgets
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
+from matplotlib.figure import Figure
+from matplotlib import pyplot
+from cil.utilities.display import show_geometry
+## EXPERIMENTAL
+
 if TYPE_CHECKING:
     from mantidimaging.gui.windows.main import MainWindowView  # noqa:F401  # pragma: no cover
+
+
+class MplCanvas(FigureCanvasQTAgg):
+
+    def __init__(self, parent, figure, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        super().__init__(figure)
 
 
 class GeometryWindowView(BaseMainWindowView):
@@ -19,3 +38,19 @@ class GeometryWindowView(BaseMainWindowView):
 
         self.main_window = main_window
         self.presenter = GeometryWindowPresenter(self, main_window)
+
+        test_uuid = None
+        for u in self.main_window.stack_list:
+            if u.id is not None:
+                test_uuid = u.id
+                break
+
+        test_geometry = self.main_window.get_stack(test_uuid).geometry
+
+        test_plot: Figure = show_geometry(test_geometry)
+
+        # Create the maptlotlib FigureCanvas object,
+        # which defines a single set of axes as self.axes.
+        sc = MplCanvas(self, figure=test_plot, width=5, height=4, dpi=100)
+        sc.axes.plot([0, 1, 2, 3, 4], [10, 1, 20, 3, 40])
+        self.setCentralWidget(sc)
