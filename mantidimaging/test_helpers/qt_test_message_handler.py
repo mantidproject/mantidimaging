0fb2d8e1d94778d0902a874fed9fc5c819b18a1f
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2025-08-18 14:40:14 +0100

    use mode map in message handler

--- mantidimaging/test_helpers/qt_test_message_handler.py
+++ mantidimaging/test_helpers/qt_test_message_handler.py
@@ -4,17 +4,16 @@ from PyQt5 import QtCore
 
 
 def qt_message_handler(mode, context, message):
-    if mode == QtCore.QtInfoMsg:
-        mode = 'INFO'
-    elif mode == QtCore.QtWarningMsg:
-        mode = 'WARNING'
-        if 'propagateSizeHints' in message:
-            return
-    elif mode == QtCore.QtCriticalMsg:
-        mode = 'CRITICAL'
-    elif mode == QtCore.QtFatalMsg:
-        mode = 'FATAL'
-    else:
-        mode = 'DEBUG'
-    print(f'qt_message_handler: line: {context.line}, func: {context.function}, file: {context.file}')
-    print(f"{mode}: {message}\n")
+    mode_map = {
+        QtCore.QtInfoMsg: 'INFO',
+        QtCore.QtWarningMsg: 'WARNING',
+        QtCore.QtCriticalMsg: 'CRITICAL',
+        QtCore.QtFatalMsg: 'FATAL'
+    }
+
+    # Get the string label for mode, otherwise default to 'DEBUG'
+    mode_str = mode_map.get(mode, 'DEBUG')
+    if mode == QtCore.QtWarningMsg and 'propagateSizeHints' in message:
+        return
+    print(f"qt_message_handler: line: {context.line}, func: {context.function}, file: {context.file}")
+    print(f"{mode_str}: {message}\n")
