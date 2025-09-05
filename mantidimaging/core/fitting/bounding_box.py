b8e15b65c9a517cbc955128bb503fa1a6b4c23d5
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2025-09-05 14:45:05 +0100

    the bounding box neglects the first 10% of pixels to avoid image edge effects

--- mantidimaging/core/fitting/bounding_box.py
+++ mantidimaging/core/fitting/bounding_box.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
 # SPDX - License - Identifier: GPL-3.0-or-later
-import matplotlib.pyplot as plt
 import numpy as np
 
 from mantidimaging.core.data import ImageStack
@@ -18,55 +17,28 @@ def get_bounding_box(stack: ImageStack) -> SensibleROI:
     all_row_mean_flipped = np.flip(all_row_mean)
 
     def find_bound(data_temp: np.ndarray, flipped: bool = False, noise_threshold_factor: int = 25) -> int:
-        # find bound by looking where the data first goes outside of the initial noise level (standard deviation of first 5 pixels)
-        # could take the abs of the dataset and look for peaks so that it works for inversed datasets
-        # take the tol between the noise threshold and 6 * sigma, if no bounds are found, then set the bounds to the full dataset
-        data_temp = data_temp[50:round(len(data_temp) / 2)]
-        data_diff_norm = np.absolute(np.diff(data_temp) / data_temp[1:])
+        data_temp = data_temp[0:round(len(data_temp) / 2):2]
+        data_diff_norm = np.absolute(np.gradient(data_temp) / data_temp)
         noise_sigma = np.std(data_diff_norm[:5])
         noise_threshold = noise_threshold_factor * noise_sigma
-        for tol in np.linspace(noise_threshold, 6 * noise_sigma, 10, endpoint=True):
-            if not np.where(data_diff_norm > tol)[0].size == 0:
-                print(f"{np.where(data_diff_norm > tol)=}")
-                bound = int(np.where(data_diff_norm > tol)[0][0])
+
+        for tol in np.linspace(noise_threshold, 5 * noise_sigma, 20, endpoint=True):
+            above_threshold_ind = np.where(data_diff_norm > tol)[0]
+            #ignore the first 10% of the data to avoid catching edge effects
+            above_threshold_ind = above_threshold_ind[above_threshold_ind > stack.data.shape[1] / 10]
+            if not above_threshold_ind.size == 0:
+                bound = above_threshold_ind[0] * 2
                 if flipped:
-                    return stack.data.shape[1] - bound + buffer_pixels if buffer_pixels - bound < 0 else stack.data.shape[1]
+                    return stack.data.shape[1] - bound + buffer_pixels if buffer_pixels - bound < 0 \
+                        else stack.data.shape[1]
                 else:
                     return bound - buffer_pixels if bound > buffer_pixels else 0
-        # if no peak can be found above 6 sigma of the noise, then return with full image ROI
+        # if no peak can be found above 5 sigma of the noise, then return with full image ROI
         if flipped:
             return stack.data.shape[1]
         else:
             return 0
-    print(f"{[np.std(np.absolute(np.diff(all_row_mean) / all_row_mean[1:])[:round(depth)]) for depth in [stack.data.shape[1] * 0.01, stack.data.shape[1] * 0.05, stack.data.shape[1] * 0.1]]}")
 
-    # t = np.linspace(np.std(np.absolute(np.diff(all_row_mean) / all_row_mean[1:])[:5]) * 25, 6 * np.std(np.absolute(np.diff(all_row_mean) / all_row_mean[1:])[:5]), 5, endpoint=True)
-    # plt.plot(np.absolute(np.diff(all_row_mean) / all_row_mean[1:]), 'b')
-    # plt.plot(np.absolute(np.diff(all_col_mean) / all_col_mean[1:]), 'r')
-    # for thresh in t:
-    #     plt.axhline(y=thresh, color='g', linestyle='-')
-    # print(f"STANDARD DEVIATION: {np.std(np.absolute(np.diff(all_row_mean) / all_row_mean[1:])[:5]) * 25}")
-    plt.subplot(4, 1, 1)
-    plt.plot(all_row_mean, 'b')
-    plt.plot(all_col_mean, 'r')
-    plt.subplot(4, 1, 2)
-    plt.plot(np.gradient(all_row_mean), 'b')
-    plt.plot(np.gradient(all_col_mean), 'r')
-    plt.subplot(4, 1, 3)
-    t = np.linspace(np.std(np.absolute(np.diff(all_row_mean) / all_row_mean[1:])[:5]) * 25,
-                    6 * np.std(np.absolute(np.diff(all_row_mean) / all_row_mean[1:])[:5]), 5, endpoint=True)
-    plt.plot(np.absolute(np.diff(all_row_mean) / all_row_mean[1:]), 'b')
-    plt.plot(np.absolute(np.diff(all_col_mean) / all_col_mean[1:]), 'r')
-    for thresh in t:
-        plt.axhline(y=thresh, color='g', linestyle='-')
-    plt.show()
-    plt.subplot(4, 1, 4)
-    t = np.linspace(np.std(np.absolute(np.diff(all_col_mean_flipped) / all_col_mean_flipped[1:])[:5]) * 25,
-                    6 * np.std(np.absolute(np.diff(all_col_mean_flipped) / all_col_mean_flipped[1:])[:5]), 10, endpoint=True)
-    plt.plot(np.absolute(np.diff(all_col_mean_flipped) / all_col_mean_flipped[1:]), 'b')
-    for thresh in t:
-        plt.axhline(y=thresh, color='g', linestyle='-')
-    plt.show()
     # add a buffer around the found bounds to make sure all the sample is captured in the bounding box
 
     top_bound = find_bound(all_row_mean)
