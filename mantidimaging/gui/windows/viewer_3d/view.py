b4a0355e0cd362b1f99e20740f9d78cb52b7005f
Rasmia Kulan <rasmia.kulan@stfc.ac.uk>
2025-09-09 17:23:33 +0100

    Add MI 3D viewer window

---
+++ mantidimaging/gui/windows/viewer_3d/view.py
@@ -0,0 +1,40 @@
+from PyQt5.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+from ccpi.viewer.CILViewer import CILViewer
+
+
+class MI3DViewer(QMainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Set up the central widget and layout
+        central_widget = QWidget(self)
+        layout = QVBoxLayout(central_widget)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        # Set up the VTK render window interactor
+        self.vtk_widget = QVTKRenderWindowInteractor(central_widget)
+
+        # Set up the CIL Viewer
+        self.viewer = CILViewer(
+            renWin=self.vtk_widget.GetRenderWindow(),
+            iren=self.vtk_widget
+        )
+
+        # Add the VTK widget to the layout
+        layout.addWidget(self.vtk_widget)
+        self.setCentralWidget(central_widget)
+
+        # Initialize the VTK widget
+        self.vtk_widget.Initialize()
+
+        # Set window properties
+        self.setWindowTitle("Mantid Imaging - 3D Viewer")
+        self.resize(800, 600)
+        self.show()
+
+    def closeEvent(self, event):
+        # Clean up VTK resources
+        self.vtk_widget.Finalize()
+        super().closeEvent(event)
