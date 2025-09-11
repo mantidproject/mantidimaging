d7e7c1322a0f11f493ddb3fb8ee27bf774352129
Rasmia Kulan <rasmia.kulan@stfc.ac.uk>
2025-09-11 17:57:42 +0100

    add addWidget to init_layout and reorder init methods

--- mantidimaging/gui/windows/viewer_3d/view.py
+++ mantidimaging/gui/windows/viewer_3d/view.py
@@ -11,23 +11,23 @@ class MI3DViewer(QMainWindow):
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.init_layout()
         self.init_viewer()
+        self.init_layout()
         self.init_window()
 
-    # Set up the central widget and layout
+    # Set up the VTK render window interactor and the CIL Viewer
+    def init_viewer(self):
+        self.vtk_widget = QVTKRenderWindowInteractor()
+        self.viewer = CILViewer(renWin=self.vtk_widget.GetRenderWindow(), iren=self.vtk_widget)
+        self.vtk_widget.Initialize()
+
+    # Set up the central widget and layout, and add the viewer
     def init_layout(self):
         self.central_widget = QWidget(self)
         self.layout = QVBoxLayout(self.central_widget)
         self.layout.setContentsMargins(0, 0, 0, 0)
-        self.setCentralWidget(self.central_widget)
-
-    # Set up the VTK render window interactor and the CIL Viewer
-    def init_viewer(self):
-        self.vtk_widget = QVTKRenderWindowInteractor(self.central_widget)
-        self.viewer = CILViewer(renWin=self.vtk_widget.GetRenderWindow(), iren=self.vtk_widget)
         self.layout.addWidget(self.vtk_widget)
-        self.vtk_widget.Initialize()
+        self.setCentralWidget(self.central_widget)
 
     # Set window properties
     def init_window(self):
