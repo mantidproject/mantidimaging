1f105e426cea76f6ee013d1d568a788625121f61
Rasmia Kulan <rasmia.kulan@stfc.ac.uk>
2025-12-03 14:10:19 +0000

    Fix copy rights

--- mantidimaging/eyes_tests/mi_3d_viewer_test.py
+++ mantidimaging/eyes_tests/mi_3d_viewer_test.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2021 ISIS Rutherford Appleton Laboratory UKRI
-# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX - License - Identifier: GPL-3.0-or-later
 from __future__ import annotations
 
 import sys
