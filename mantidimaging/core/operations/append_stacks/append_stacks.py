2468c2cfb4ab3ac9afe4c41be7c69461d38fb1a6
Mike Sullivan <michael.sullivan@stfc.ac.uk>
2026-01-21 09:51:18 +0000

    trying

--- mantidimaging/core/operations/append_stacks/append_stacks.py
+++ mantidimaging/core/operations/append_stacks/append_stacks.py
@@ -6,8 +6,14 @@ from functools import partial
 from typing import TYPE_CHECKING, Any
 from PyQt5.QtWidgets import QComboBox
 
+import numpy as np
+
+from mantidimaging import helper as h
 from mantidimaging.core.operations.base_filter import BaseFilter
+from mantidimaging.core.parallel import utility as pu
+from mantidimaging.core.utility.data_containers import ProjectionAngles
 from mantidimaging.gui.utility.qt_helpers import Type
+from mantidimaging.core.utility.progress_reporting import Progress
 
 from mantidimaging.gui.widgets.dataset_selector import DatasetSelectorWidgetView
 
@@ -32,6 +38,46 @@ class AppendStacks(BaseFilter):
                     progress=None) -> ImageStack:
         """
         """
+
+        h.check_data_stack(images)
+
+        sample_data = images.data
+        sample_angles = images.projection_angles()
+        stack_to_append_data = stack_to_append.data
+        stack_to_append_angles = stack_to_append.projection_angles()
+
+        print(f"{sample_angles=}")
+        print(f"{stack_to_append_angles=}")
+
+
+        #out_angles = np.concatenate((sample_angles, stack_to_append_angles))
+        #print(f"{out_angles=}\n{out_angles.shape}")
+        #images.set_projection_angles()
+
+        # print(f"{sample_data=}")
+        # print(f"{sample_angles=}")
+        # print(f"{stack_to_append_data=}")
+        # print(f"{stack_to_append_angles=}")
+
+        out_shape = (sample_data.shape[0] + stack_to_append_data.shape[0], sample_data.shape[1], sample_data.shape[2])
+        out_angles_shape = (sample_angles.value.shape[0] + stack_to_append_angles.value.shape[0]) if sample_angles is not None else None
+
+        print(f"{out_shape=}")
+        print(f"{sample_data.shape=}")
+        print(f"{stack_to_append_data.shape=}")
+
+        output = pu.create_array(out_shape, images.dtype)
+        output_angles = np.empty(out_angles_shape, images.dtype)
+        # execute_single(sample_data, stack_to_append_data, sample_angles, stack_to_append_angles, progress, out=output.array)
+        execute_single(images, stack_to_append, progress, out=output.array, out_ang=output_angles)
+
+        print("++++++++++++++ AFTER EXECUTE +++++++++++++++++")
+        print(f"======================{output.array.shape=}=========================")
+        print(f"======================{output_angles=}=========================")
+
+        images.shared_array = output
+        images.set_projection_angles(ProjectionAngles(output_angles))
+
         return images
 
     @staticmethod
@@ -65,3 +111,49 @@ class AppendStacks(BaseFilter):
         return partial(AppendStacks.filter_func,
                        stack_to_append=stack_to_append_images,
                        append_type=type_widget.currentText())
+
+# def execute_single(data, data_to_append, data_angles, data_to_append_angles, progress=None, out=None):
+#     progress = Progress.ensure_instance(progress, task_name='Append Stacks')
+#     # progress.set_estimated_steps(data.shape[0] + data_to_append.shape[0])
+#     # prob_occupied = numpy.zeros_like(data, dtype=numpy.float32)
+#     progress.add_estimated_steps(1)
+#
+#     with progress:
+#         progress.update(msg=f"Appending Stacks")
+#
+#         output = out[:] if out is not None else data[:]
+#         output = np.concatenate((data, data_to_append))
+#
+#         if data_angles and data_to_append_angles:
+#             out_angles = np.concatenate((data_angles, data_to_append_angles))
+#
+#
+#     return output
+
+def execute_single(stack, stack_to_append, progress=None, out=None, out_ang=None):
+    progress = Progress.ensure_instance(progress, task_name='Append Stacks')
+    # progress.set_estimated_steps(data.shape[0] + data_to_append.shape[0])
+    # prob_occupied = numpy.zeros_like(data, dtype=numpy.float32)
+    progress.add_estimated_steps(1)
+    print(f"{out=}")
+    print(f"{out_ang=}")
+
+    # angles = stack.projection_angles().value
+    if angles := stack.projection_angles():
+        angles = angles.value
+    if angles_to_append := stack_to_append.projection_angles():
+        angles_to_append = angles_to_append.value
+
+    with progress:
+        progress.update(msg=f"Appending Stacks")
+
+        output = out[:] if out is not None else stack.data
+        output_angles = out_ang[:] if out_ang is not None else angles
+
+        if stack_to_append.data:
+            output[0] = np.concatenate((stack.data, stack_to_append.data))
+
+        if angles_to_append:
+            output_angles = np.concatenate((angles, angles_to_append))
+
+    return output, output_angles
