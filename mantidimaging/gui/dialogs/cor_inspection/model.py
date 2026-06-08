2c963d3a933b42f31f300015110c569db3058ed5
JackEAllen <jack.allen@stfc.ac.uk>
2026-06-08 18:18:48 +0100

    Fix CoR Inflation by Backcalculating CoR for Slice Index
    Instead of setting  directly from a slice-specific value. Since  is defined at slice 0,  called  adding the title gradient again causing it to double count it, back-calculate CoR for selected Idx by calling  instead of assigning directly to . When  now calls , it adds the gradient back, recovering the origional slice CoR without double counting tilt

--- mantidimaging/gui/dialogs/cor_inspection/model.py
+++ mantidimaging/gui/dialogs/cor_inspection/model.py
@@ -84,7 +84,7 @@ class CORInspectionDialogModel:
     def _recon_cor_preview(self, image: ImageType) -> np.ndarray:
         assert (self.images.geometry is not None)
         assert self.proj_angles is not None
-        self.images.geometry.cor = ScalarCoR(self.cor(image))
+        self.images.geometry.set_cor_at_slice_index(self.slice_idx, ScalarCoR(self.cor(image)))
         return self.reconstructor.single_sino(self.images, self.slice_idx, self.recon_params)
 
     def _recon_iters_preview(self, image: ImageType) -> np.ndarray:
