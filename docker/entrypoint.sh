1b2dea537b7a95490e8476fda3ec8833820b658f
Rasmia Kulan <rasmia.kulan@stfc.ac.uk>
2026-09-15 12:54:40 +0100

    migrate rocky and u18 testing workflows from mamba/conda to pixi

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,9 +5,6 @@
 # This is all in aid of ensuring that permissions of filesystem mapped volumes
 # function correctly and that the root user is not used within the container.
 
-eval "$(/opt/miniconda/bin/conda shell.bash hook)"
-conda activate mantidimaging-dev
-
 set -x
 CMD=${1:-"bash"}
 ARGS=${@:2}
